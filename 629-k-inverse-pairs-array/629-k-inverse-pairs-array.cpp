4d23df346f3d930bebda8497596301a1c272017c
Martin Yang <martin0327@gmail.com>
2022-07-17 16:25:20 +0900

    Time: 111 ms (26.92%), Space: 25.6 MB (8.33%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -1,28 +1,27 @@
-using ll = long long;
-using vi = vector<ll>;
+#define ll long long
+#define int ll
+
+using vi = vector<int>;
 using vvi = vector<vi>;
-using pii = pair<ll,ll>;
-using vp = vector<pii>;
-using vvp = vector<vp>;
-const ll mod = 1e9+7;
+const int mod = 1e9+7;
 
 class Solution {
 public:
     int kInversePairs(int n, int k) {
         vvi dp(n+1, vi(k+1));
-        dp[0][0] = 1;
+        for (int i=0; i<=n; i++) dp[i][0] = 1;
         for (int i=1; i<=n; i++) {
-            ll temp = 0;
-            for (int j=0; j<=k; j++) {
-                temp += dp[i-1][j];
-                temp %= mod;
+            for (int j=1; j<=k; j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                dp[i][j] %= mod;
                 if (j >= i) {
-                    temp += mod - dp[i-1][j-i];
-                    temp %= mod;
+                    dp[i][j] += mod - dp[i-1][j-i];
+                    dp[i][j] %= mod;
                 }
-                dp[i][j] = temp;
             }
         }
         return dp[n][k];
     }  
 };
+
+#define ll int
