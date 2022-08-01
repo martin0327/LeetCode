d2d03bbe1ed2d2c42dd68dca8c33aeefb3553d2c
Martin Yang <martin0327@gmail.com>
2022-08-01 11:59:53 +0900

    Time: 4 ms (28.94%), Space: 5.8 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,8 +5,7 @@ public:
         n--; m--;
         if (m<n) swap(n,m);
         long long ans = 1;
-        int i = m+1;
-        int j = 1;
+        int i = m+1, j = 1;
         while (i<=n+m || j<=n) {
             if (j <= n && ans % j == 0) ans /= j++;
             else ans *= i++;
