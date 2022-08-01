a252ba2e3ecb2a41e7d0e9f8e0b9f1da88a3c3d0
Martin Yang <martin0327@gmail.com>
2022-08-01 11:54:28 +0900

    Time: 0 ms (100.00%), Space: 6 MB (63.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,50 +2,16 @@ vector<int> primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73
 
 class Solution {
 public:
-    vector<int> cnt;
-    int int_pow(int base, int exp) {
-        int res = 1;
-        while (exp) {
-            if (exp & 1) res *= base;
-            exp >>= 1;
-            base *= base;
-        }
-        return res;
-    }
-    void f(int n) {
-        for (int i=2; i<=n; i++) {
-            int x = i;
-            for (auto p : primes) {
-                while (x % p == 0) {
-                    x /= p;
-                    cnt[p]++;
-                }
-                if (x==1) break;
-            }
-        }
-    }
-    void g(int n) {
-        for (int i=2; i<=n; i++) {
-            int x = i;
-            for (auto p : primes) {
-                while (x % p == 0) {
-                    x /= p;
-                    cnt[p]--;
-                }
-                if (x==1) break;
-            }
-        }
-    }
-    
     int uniquePaths(int m, int n) {
-        cnt = vector<int>(200);
-        f(n+m-2);
-        g(n-1);
-        g(m-1);
-        int ans = 1;
-        for (auto p : primes) {
-            int c = cnt[p];
-            if (c>0) ans *= int_pow(p,c);
+        if (n==1 || m==1) return 1;
+        n--; m--;
+        if (m<n) swap(n,m);
+        long long ans = 1;
+        int i = m+1;
+        int j = 1;
+        while (i<=n+m || j<=n) {
+            if (j <= n && ans % j == 0) ans /= j++;
+            else if (i<=n+m) ans *= i++;
         }
         return ans;
     }
