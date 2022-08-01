e4b3d0ed014fe0b6ca0eaaa411e32fbba5ee42f8
Martin Yang <martin0327@gmail.com>
2022-08-01 12:09:38 +0900

    Time: 0 ms (100.00%), Space: 5.8 MB (98.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,10 @@
-using ll = long long;
-
 class Solution {
 public:
     int uniquePaths(int m, int n) {
         if (n==1 || m==1) return 1;
         n--; m--;
         if (m<n) swap(n,m);
-        ll ans = 1;
+        long long ans = 1;
         int i = m+1, j = 1;
         while (i<=n+m) {
             ans *= i++;
