be2e52028a8a837703e099a6d77658870c6acaec
Martin Yang <martin0327@gmail.com>
2022-08-07 09:56:09 +0900

    Time: 6 ms (87.38%), Space: 6 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,27 +1,27 @@
+using ll = long long;
+const ll mod = 1e9+7;
+
 class Solution {
 public:
-    using ll = long long;
-    using vi = vector<ll>;
-    using vvi = vector<vi>;
-    const ll mod = 1e9+7;
-    
     int countVowelPermutation(int n) {
-        vvi dp(n, vi(5));
-        for (int j=0; j<5; j++) dp[0][j] = 1;
-        for (int i=1; i<n; i++) {
-            dp[i][0] = (dp[i][0] + dp[i-1][1]) % mod;
-            dp[i][1] = (dp[i][1] + dp[i-1][0] + dp[i-1][2]) % mod;
-            dp[i][3] = (dp[i][3] + dp[i-1][2] + dp[i-1][4]) % mod;
-            dp[i][4] = (dp[i][4] + dp[i-1][0]) % mod ;
-            for (int j=0; j<5; j++) 
-                dp[i][2] = (dp[i][2] + dp[i-1][j]) % mod;
-            dp[i][2] = (dp[i][2] + mod - dp[i-1][2]) % mod;
+        ll a,e,i,o,u;
+        a=e=i=o=u=1;
+        while (--n) {
+            int aa,ee,ii,oo,uu;
+            aa = e;
+            ee = (a+i)%mod;
+            ii = (a+e+o+u)%mod;
+            oo = (i+u)%mod;
+            uu = a;
+            
+            a = aa;
+            e = ee;
+            i = ii;
+            o = oo;
+            u = uu;
         }
-        ll ans = 0;
-        for (int j=0; j<5; j++) {
-            ans += dp[n-1][j];
-            ans %= mod;
-        }
+        
+        ll ans = (a+e+i+o+u)%mod;
         return ans;
     }
 };
