39826bc3e5ffefd226f8847cbdcd9eeae332cd07
Martin Yang <martin0327@gmail.com>
2022-08-08 11:56:48 +0900

    Time: 14 ms (85.19%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,13 +1,12 @@
-const int inf = 1e9;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& a) {
-        vector<int> dp = {-inf};
+        vector<int> dp = {a[0]};
         for (auto x : a) {
             if (x > dp.back()) dp.push_back(x);
             else *lower_bound(dp.begin(),dp.end(),x) = x;
         }
-        int ans = dp.size()-1;
+        int ans = dp.size();
         return ans;        
     }
 };
