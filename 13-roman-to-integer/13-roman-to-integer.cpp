302ec7e31fb42e26cdbd63f185cd8708b7ff7bdd
Martin Yang <martin0327@gmail.com>
2022-08-15 19:34:36 +0900

    Time: 37 ms (14.22%), Space: 8.6 MB (10.22%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        string a = "IVXLCDM";
+        vector<int> b = {1,5,10,50,100,500,1000};
+        map<char,int> mp;
+        for (int i=0; i<7; i++) mp[a[i]] = b[i];
+        int ans = 0;
+        int n = s.size();
+        for (int i=0; i<n; i++) {
+            int mult = 1;
+            if (i<n-1) {
+                int q = mp[s[i+1]] / mp[s[i]];
+                if (q==5 || q==10) mult = -1;
+            }
+            ans += mult*mp[s[i]];
+        }
+        return ans;
+    }
+};
