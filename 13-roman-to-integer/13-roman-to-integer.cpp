541d01c1551a195fa2b21abb59122d56927c6824
Martin Yang <martin0327@gmail.com>
2022-08-15 19:34:52 +0900

    Time: 21 ms (44.07%), Space: 8.4 MB (14.26%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -3,7 +3,7 @@ public:
     int romanToInt(string s) {
         string a = "IVXLCDM";
         vector<int> b = {1,5,10,50,100,500,1000};
-        map<char,int> mp;
+        unordered_map<char,int> mp;
         for (int i=0; i<7; i++) mp[a[i]] = b[i];
         int ans = 0;
         int n = s.size();
