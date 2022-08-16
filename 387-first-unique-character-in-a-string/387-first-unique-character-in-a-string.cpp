c4d01ff73c946ed81faeac0df93578e28ecc459b
Martin Yang <martin0327@gmail.com>
2022-08-16 09:26:31 +0900

    Time: 28 ms (87.01%), Space: 10.6 MB (91.21%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,19 @@
+const int inf = 1e9;
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int> idx(26,-1);
+        int idx[26];
+        memset(idx, -1, sizeof(int) * 26);
         for (int i=0; i<s.size(); i++) {
             int x = s[i] - 'a';
             if (idx[x] == -1) idx[x] = i;
             else if (idx[x] != -2) idx[x] = -2;
         }
-        int ans = 1e9;
+        int ans = inf;
         for (int i=0; i<26; i++) {
             if (idx[i] >= 0) ans = min(ans, idx[i]);
         }
-        if (ans==1e9) ans = -1;
+        if (ans==inf) ans = -1;
         return ans;
     }
 };
