381b64ded50f534e682ab39c45e9eb0c06c87983
Martin Yang <martin0327@gmail.com>
2022-08-18 10:07:31 +0900

    Time: 494 ms (16.68%), Space: 80.2 MB (31.31%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  int minSetSize(vector<int>& a) {
+    int n = a.size();
+    map<int,int> cnt;
+    for (auto x : a) cnt[x]++;
+    vector<int> b;
+    for (auto [x,c] : cnt) b.push_back(c);
+    sort(b.rbegin(), b.rend());
+    
+    int ans = 0;
+    int cum = 0;
+    int m = (n/2) + (n&1);
+    for (auto x : b) {
+      if (cum >= m) break;
+      ans++;
+      cum += x;
+    }
+    
+    return ans;
+  }
+};
