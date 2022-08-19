ef0203ca54b4fbebed61931d454b510e0c21dd7e
Martin Yang <martin0327@gmail.com>
2022-08-19 13:23:46 +0900

    Time: 271 ms (13.69%), Space: 73.6 MB (9.92%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -4,22 +4,21 @@ using min_pq = priority_queue<T, vector<T>, greater<T>>;
 class Solution {
 public:
   bool isPossible(vector<int>& a) {
-    unordered_map<int, deque<int>> mp;
+    unordered_map<int, min_pq<int>> mp;
     for (auto x : a) {
       if (mp[x-1].size()) {
-        int len = mp[x-1].front();
-        mp[x-1].pop_front();
-        mp[x].push_back(len+1);
+        int len = mp[x-1].top();
+        mp[x-1].pop();
+        mp[x].push(len+1);
       }
       else {
-        mp[x].push_front(1);
+        mp[x].push(1);
       }
     }
-    
     bool ret = true;
     for (auto [key,val] : mp) {
       if (val.size()) {
-        ret &= (val.front() >= 3);
+        ret &= (val.top() >= 3);
       }
     }
     return ret;
