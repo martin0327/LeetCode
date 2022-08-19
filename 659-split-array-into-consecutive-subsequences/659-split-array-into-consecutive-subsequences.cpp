21f4fcb66de5c23b90a925377481088fff822f4e
Martin Yang <martin0327@gmail.com>
2022-08-19 13:08:50 +0900

    Time: 417 ms (5.90%), Space: 75.4 MB (8.74%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,21 +1,24 @@
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+
 class Solution {
 public:
   bool isPossible(vector<int>& a) {
-    map<int, multiset<int>> mp;
+    map<int, min_pq<int>> mp;
     for (auto x : a) {
       if (mp[x-1].size()) {
-        int len = *mp[x-1].begin();
-        mp[x-1].erase(mp[x-1].begin());
-        mp[x].insert(len+1);
+        int len = mp[x-1].top();
+        mp[x-1].pop();
+        mp[x].push(len+1);
       }
       else {
-        mp[x].insert(1);
+        mp[x].push(1);
       }
     }
     bool ret = true;
     for (auto [key,val] : mp) {
       if (val.size()) {
-        ret &= (*val.begin() >= 3);
+        ret &= (val.top() >= 3);
       }
     }
     return ret;
