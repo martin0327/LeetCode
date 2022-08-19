8bdde434335e07ae81a3cf81c7e08c4016edc3b3
Martin Yang <martin0327@gmail.com>
2022-08-19 13:10:13 +0900

    Time: 230 ms (20.07%), Space: 73.6 MB (9.92%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -4,7 +4,7 @@ using min_pq = priority_queue<T, vector<T>, greater<T>>;
 class Solution {
 public:
   bool isPossible(vector<int>& a) {
-    map<int, min_pq<int>> mp;
+    unordered_map<int, min_pq<int>> mp;
     for (auto x : a) {
       if (mp[x-1].size()) {
         int len = mp[x-1].top();
