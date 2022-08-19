e1cc85d81909c3c05618036da3a2e4ee1bcaf014
Martin Yang <martin0327@gmail.com>
2022-08-19 13:07:13 +0900

    Time: 381 ms (7.44%), Space: 106.8 MB (5.31%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
   bool isPossible(vector<int>& a) {
-    vector<multiset<int>> b(2002);
-    for (auto &x : a) x += 1001;
+    map<int, multiset<int>> mp;
     for (auto x : a) {
-      if (b[x-1].size()) {
-        int len = *b[x-1].begin();
-        b[x-1].erase(b[x-1].begin());
-        b[x].insert(len+1);
+      if (mp[x-1].size()) {
+        int len = *mp[x-1].begin();
+        mp[x-1].erase(mp[x-1].begin());
+        mp[x].insert(len+1);
       }
       else {
-        b[x].insert(1);
+        mp[x].insert(1);
       }
     }
     bool ret = true;
-    for (int i=0; i<2001; i++) {
-      if (b[i].size()) {
-        ret &= (*b[i].begin()>=3);
+    for (auto [key,val] : mp) {
+      if (val.size()) {
+        ret &= (*val.begin() >= 3);
       }
     }
-
     return ret;
   }
 };
