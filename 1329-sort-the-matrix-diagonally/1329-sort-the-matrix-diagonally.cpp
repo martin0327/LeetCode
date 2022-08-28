e42732f7857e37aa2ff0c8f72d83b74d5072a780
Martin Yang <martin0327@gmail.com>
2022-08-28 09:42:19 +0900

    Time: 4 ms (99.40%), Space: 8.8 MB (88.57%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -5,24 +5,23 @@ public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& a) {
         int n = a.size();
         int m = a[0].size();
+        vi b(max(n,m));
         for (int k=0; k<n; k++) {
             int sz = min(n-k, m);
-            vi b(sz);
             for (int d=0; d<sz; d++) {
                 b[d] = a[k+d][d];
             }
-            sort(b.begin(), b.end());
+            sort(b.begin(), b.begin()+sz);
             for (int d=0; d<sz; d++) {
                 a[k+d][d] = b[d];
             }
         }
         for (int k=1; k<m; k++) {
             int sz = min(n, m-k);
-            vi b(sz);
             for (int d=0; d<sz; d++) {
                 b[d] = a[d][k+d];
             }
-            sort(b.begin(), b.end());
+            sort(b.begin(), b.begin()+sz);
             for (int d=0; d<sz; d++) {
                 a[d][k+d] = b[d];
             }
