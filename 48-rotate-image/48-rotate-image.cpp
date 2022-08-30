4f19a57fc541136bf2764852081f50c3a3028dd5
Martin Yang <martin0327@gmail.com>
2022-08-30 10:20:55 +0900

    Time: 4 ms (42.47%), Space: 7.1 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,21 +3,10 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& a) {
         int n = a.size();
-        for (int i=0; i<n/2; i++) {
-            int m = n-2*i-1;
-            vi b(4*m);
-            for (int j=0; j<m; j++) {
-                b[j] = a[i][i+j];
-                b[m+j] = a[i+j][m+i];
-                b[2*m+j] = a[m+i][m+i-j];
-                b[3*m+j] = a[m+i-j][i];
-            }
-            for (int j=0; j<m; j++) {
-                a[i][i+j] = b[3*m+j];
-                a[i+j][m+i] = b[j];
-                a[m+i][m+i-j] = b[m+j];
-                a[m+i-j][i] = b[2*m+j];
-            }
+        reverse(a.begin(), a.end());
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j)
+                swap(a[i][j], a[j][i]);
         }
     }
 };
