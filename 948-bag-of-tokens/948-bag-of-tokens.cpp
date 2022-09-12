515883e8ec569a57bf9181dd18e69af511af6abe
Martin Yang <martin0327@gmail.com>
2022-09-12 11:25:35 +0900

    Time: 12 ms (42.34%), Space: 10.5 MB (66.86%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& a, int p) {
+        int n = a.size();
         sort(a.begin(), a.end());
-        deque<int> dq;
-        for (auto x : a) dq.push_back(x);
-        int ans = 0;
-        int cur = 0;
-        while (dq.size()) {
-            if (p >= dq.front()) {
-                p -= dq.front();
-                dq.pop_front();
-                cur++;
-                ans = max(ans, cur);
+        int l = 0, r = n-1;
+        int ans = 0, cur = 0;
+        while (l<=r) {
+            if (p >= a[l]) {
+                p -= a[l++];
+                ans = max(ans, ++cur);
             }
             else if (cur > 0) {
                 cur--;
-                p += dq.back();
-                dq.pop_back();
+                p += a[r--];
             }
             else break;
         }
