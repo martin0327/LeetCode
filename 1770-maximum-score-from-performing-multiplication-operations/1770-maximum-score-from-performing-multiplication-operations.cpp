830579bfa0929d0d442f9b0780e6e13283f5baf2
Martin Yang <martin0327@gmail.com>
2022-09-16 09:47:21 +0900

    Time: 631 ms (62.62%), Space: 119.7 MB (72.63%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,25 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+class Solution {
+public:
+    
+    
+    int maximumScore(vector<int>& a, vector<int>& b) {
+        int n = b.size();
+        int m = a.size();
+        vvi dp(n+1, vi(n+1, -1e9));
+        dp[0][0] = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; i+j<n; j++) {
+              dp[i+1][j] = max(dp[i+1][j], dp[i][j] + a[i] * b[i+j]);
+              dp[i][j+1] = max(dp[i][j+1], dp[i][j] + a[m-1-j] * b[i+j]);
+            }
+        }
+        int ans = -1e9;
+        for (int i=0; i<=n; i++) {
+            ans = max(ans, dp[i][n-i]);
+        }    
+        return ans;
+    }
+}; 
