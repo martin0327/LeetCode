8a668effed9e70076166be519adc8faabc7adfdc
Martin Yang <martin0327@gmail.com>
2022-09-19 20:21:44 +0900

    Time: 123 ms (28.57%), Space: 15.4 MB (14.29%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -4,14 +4,11 @@ public:
         int n = s.size();
         int ans = 0;
         for (int i=0; i<n; i++) {
-            int x = s[i] - 'a';
             for (int j=i; j<i+26; j++) {
-                int y = s[j] - 'a';
-                if (y-x == j-i) ans = max(ans, j-i+1);
+                if (s[j]-s[i] == j-i) ans = max(ans, j-i+1);
                 else break;
             }
         }
         return ans;
-        
     }
 };
