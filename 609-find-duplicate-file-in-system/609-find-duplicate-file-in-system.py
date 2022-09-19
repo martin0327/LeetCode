790eb2fb2fa73abce703d42d6f8681e36163dcfe
Martin Yang <martin0327@gmail.com>
2022-09-19 13:32:47 +0900

    Time: 247 ms (11.69%), Space: 24.1 MB (24.41%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for s in paths:
+            a = s.split()
+            pre = a[0]
+            for fc in a[1:]:
+                f,c = fc.split('(')
+                c = c[:-1]
+                d[c].append(pre + '/' + f)
+        return [x for x in list(d.values()) if len(x) > 1]
