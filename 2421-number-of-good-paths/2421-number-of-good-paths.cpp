2fd38ef68a5676421929d7a026844a5ea9c11def
Martin Yang <martin0327@gmail.com>
2022-09-28 17:14:59 +0900

    Time: 694 ms (85.35%), Space: 190 MB (15.26%) - LeetHub

---
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -0,0 +1,73 @@
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+  private:
+    int _n;
+    std::vector<int> parent_or_size;
+};
+
+class Solution {
+public:
+  int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+    using ll = long long;
+    using vi = vector<int>;
+    using vvi =vector<vi>;
+    
+    int n = vals.size();
+    vvi adj(n);
+    for (auto uv : edges) {
+      int u = uv[0];
+      int v = uv[1];
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+    }
+    map<int,vi> mp;
+    for (int i=0; i<n; i++) {
+      mp[vals[i]].push_back(i);
+    }
+
+    ll ans = n;
+    dsu d(n);
+    for (auto &[val, a] : mp) {
+      for (auto u : a) {
+        for (auto v : adj[u]) {
+          if (vals[v] > vals[u]) continue;
+          d.merge(u,v);
+        }
+      }
+      map<int,ll> cnt;
+      for (auto u : a) {
+        cnt[d.leader(u)]++;
+      }
+      for (auto [k,v] : cnt) {
+        ans += v * (v-1) / 2;
+      }
+    }
+    return ans;
+  }
+};
