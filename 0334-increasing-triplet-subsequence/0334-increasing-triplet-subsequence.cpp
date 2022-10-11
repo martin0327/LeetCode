65fa99aef278f4c94ed50476cfa919832e18bcbc
Martin Yang <martin0327@gmail.com>
2022-10-11 14:38:15 +0900

    Time: 219 ms (5.05%), Space: 63.4 MB (13.66%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
   bool increasingTriplet(vector<int>& a) {
     map<int,int> pre, suf;
-    for (auto x : a) suf[x]++;
-    for (auto x : a) {
+    for (auto &x : a) suf[x]++;
+    for (auto &x : a) {
       if (--suf[x] == 0) suf.erase(x);
-      auto it1 = pre.lower_bound(x);
-      auto it2 = suf.upper_bound(x);
-      if (it1 != pre.begin() && it2 != suf.end()) {
-        return true;
+      if (pre.lower_bound(x) != pre.begin()) {
+        if (suf.upper_bound(x) != suf.end()) return true;
       }
       pre[x]++;
     }
