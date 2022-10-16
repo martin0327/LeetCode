7eae7026c9ba70e88c6b055710ead1f5cb6b4a94
Martin Yang <martin0327@gmail.com>
2022-10-16 10:50:09 +0900

    Time: 3 ms (48.38%), Space: 6.2 MB (44.95%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,8 +3,7 @@ public:
     string breakPalindrome(string s) {
       int n = s.size();
       if (n==1) return "";
-      for (int i=0; i<n; i++) {
-        if (n&1 && i==n/2) continue;
+      for (int i=0; i<n/2; i++) {
         if (s[i]!='a') {
           s[i] = 'a';
           return s;
