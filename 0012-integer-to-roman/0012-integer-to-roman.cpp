5059c5a5ad6d471c756dd7e587091e1fc4426d91
Martin Yang <martin0327@gmail.com>
2022-10-20 09:46:16 +0900

    Time: 12 ms (65.58%), Space: 7.1 MB (33.56%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  string intToRoman(int x) {
+    vector<string> a = {"M??", "CDM", "XLC", "IVX"};
+    vector<int> b = {1000,100,10,1};
+    string ret;
+    for (int i=0; i<4; i++) {
+      int q = x / b[i];
+      if (q == 9) {
+        ret += a[i][0];
+        ret += a[i][2];
+      }
+      else if (q==4) {
+        ret += a[i][0];
+        ret += a[i][1];
+      }
+      else{
+        if (q/5 > 0) ret += a[i][1];
+        for (int j=0; j<q%5; j++) ret += a[i][0];
+      }
+      x %= b[i];
+    }
+    return ret;
+  }
+};
