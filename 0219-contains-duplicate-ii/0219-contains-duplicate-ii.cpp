7cb5edcab3754b98326da1cdc054e225bea8c9b0
Martin Yang <martin0327@gmail.com>
2022-10-21 09:30:32 +0900

    Time: 272 ms (73.47%), Space: 80.3 MB (23.51%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  bool containsNearbyDuplicate(vector<int>& a, int k) {
+    int n = a.size();
+    map<int,int> mp;
+    k = min(n, k+1);
+    for (int i=0; i<k; i++) if (++mp[a[i]] > 1) return true;
+    
+    for (int i=k; i<n; i++) {
+      mp[a[i-k]]--;
+      if (++mp[a[i]] > 1) return true;
+    }
+    return false;
+  }
+};
