8a7cde3b16ee5254d6f6786424356226b36cb45e
Martin Yang <martin0327@gmail.com>
2022-10-22 14:41:31 +0900

    Time: 57 ms (28.86%), Space: 16.6 MB (11.82%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+      int n = s.size();
+      map<int,int> mp1, mp2;
+      for (auto c : t) mp2[c]++;
+      int sz = mp2.size();
+      int i=0, j=0, cnt=0;
+      int min_len = 1e9;
+      string ans;
+      while (i<n) {
+        if (cnt<sz && j<n) {
+          if (mp2.count(s[j]) && ++mp1[s[j]] == mp2[s[j]]) cnt++;
+          j++;
+        }
+        else {
+          if (mp2.count(s[i]) && --mp1[s[i]] < mp2[s[i]]) cnt--;
+          i++;
+        }
+        if (cnt==sz) {
+          if (j-i < min_len) {
+            min_len = j-i;
+            ans = s.substr(i,j-i);
+          }
+        }
+      }
+      return ans;
+    }
+};
