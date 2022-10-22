974f7470aeff7ad4b1ca5227ca15be0189e53c56
Martin Yang <martin0327@gmail.com>
2022-10-22 14:54:13 +0900

    Time: 64 ms (24.54%), Space: 7.9 MB (48.70%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -5,7 +5,7 @@ public:
       map<int,int> mp1, mp2;
       for (auto c : t) mp2[c]++;
       int sz = mp2.size();
-      int i=0, j=0, cnt=0;
+      int i=0, j=0, cnt=0, ii=-1, jj=-1;;
       int min_len = 1e9;
       string ans;
       while (i<n) {
@@ -20,10 +20,12 @@ public:
         if (cnt==sz) {
           if (j-i < min_len) {
             min_len = j-i;
-            ans = s.substr(i,j-i);
+            ii = i;
+            jj = j;
           }
         }
       }
+      if (ii != -1) ans = s.substr(ii,jj-ii);
       return ans;
     }
-};  
+};
