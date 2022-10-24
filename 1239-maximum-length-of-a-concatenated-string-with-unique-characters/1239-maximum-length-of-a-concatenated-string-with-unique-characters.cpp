f521ff1a07579ac3bc95278a79121812169686fb
Martin Yang <martin0327@gmail.com>
2022-10-24 09:38:28 +0900

    Time: 1340 ms (7.96%), Space: 186 MB (13.44%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -3,7 +3,7 @@ public:
   int maxLength(vector<string>& a) {
     int n = a.size();
     int ret = 0;
-    for (int mask=0; mask < 1<<n; mask++) {
+    for (int mask=0; mask<(1<<n); mask++) {
       vector<int> cnt(26);
       bool ok = true;
       int s = 0;
