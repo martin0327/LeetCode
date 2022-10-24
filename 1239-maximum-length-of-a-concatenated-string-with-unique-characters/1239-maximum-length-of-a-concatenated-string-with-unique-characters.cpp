7328a47a909e04bf9e2fc1d26b55e00e348f193d
Martin Yang <martin0327@gmail.com>
2022-10-24 14:21:35 +0900

    Time: 699 ms (14.50%), Space: 185.8 MB (14.06%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -8,11 +8,10 @@ public:
       bool ok = true;
       int s = 0;
       for (int i=0; i<n; i++) {
+        if (!ok) break;
         if (mask>>i&1) {
-          for (auto c : a[i]) {
-            if (cnt[c - 'a']++) ok = false;;
-            s++;
-          }
+          for (auto c : a[i]) if (cnt[c - 'a']++) ok = false;
+          s += a[i].size();
         }
       }      
       if (ok) ret = max(ret, s);
