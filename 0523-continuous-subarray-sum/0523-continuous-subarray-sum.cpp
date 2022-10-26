e0c633373176c1ae43ba89d14d475ce2a25c830e
Martin Yang <martin0327@gmail.com>
2022-10-26 10:23:20 +0900

    Time: 603 ms (14.17%), Space: 119.3 MB (5.05%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -4,8 +4,6 @@ public:
     int n = a.size();
     vector<int> pre(n+1);
     for (int i=1; i<=n; i++) pre[i] = (pre[i-1] + a[i-1]) % k;
-    // for (auto x : pre) cout << x << " ";
-    // cout << endl;
     set<int> s = {0};
     for (int i=2; i<=n; i++) {
       if (s.count(pre[i])) return true;
