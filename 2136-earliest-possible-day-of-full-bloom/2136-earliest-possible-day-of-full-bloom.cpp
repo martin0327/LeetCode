afeacfe82c18de8310b41f0a950f01ce9d74a29b
Martin Yang <martin0327@gmail.com>
2022-10-29 10:37:53 +0900

    Time: 471 ms (78.31%), Space: 158.7 MB (64.26%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  int earliestFullBloom(vector<int>& a, vector<int>& b) {
+    int n = a.size();
+    vector<pair<int,int>> c(n);
+    for (int i=0; i<n; i++) {
+      c[i] = {b[i],a[i]};
+    }
+    sort(c.rbegin(), c.rend());
+    int cur = 0, ans = 0;
+    for (auto [x,y] : c) {
+      ans = max(ans, cur + x + y);
+      cur += y;
+    }
+    return ans;
+  }
+};
