793747fda3d8201bbfdcac9936ce27ca9b17068f
Martin Yang <martin0327@gmail.com>
2022-11-01 11:49:41 +0900

    Time: 68 ms (16.94%), Space: 13.2 MB (52.18%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+  vector<int> findBall(vector<vector<int>>& a) {
+    int n = a.size();
+    int m = a[0].size();
+    vector<int> ret(m);
+    for (int k=0; k<m; k++) {
+      int p = k;
+      for (int i=0; i<n; i++) {
+        if (a[i][p] == 1) {
+          if (p < m-1 && a[i][p+1] == 1) p++;
+          else {
+            p = -1;
+            break;
+          }
+        }
+        else {
+          if (p > 0 && a[i][p-1] == -1) p--;
+          else {
+            p = -1;
+            break;
+          }
+        }
+      }
+      ret[k] = p;
+    }
+    return ret;
+  }
+};
