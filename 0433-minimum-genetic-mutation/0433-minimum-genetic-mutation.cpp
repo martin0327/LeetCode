c0fe8d65d20218776439bb938d89fef705dea503
Martin Yang <martin0327@gmail.com>
2022-11-02 10:22:06 +0900

    Time: 3 ms (45.67%), Space: 6.7 MB (42.79%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  int minMutation(string start, string end, vector<string>& b) {
+    map<string,int> dist;
+    dist[start] = 0;
+    queue<string> q;
+    q.push(start);
+    while (q.size()) {
+      auto u = q.front(); q.pop();
+      for (auto v : b) {
+        if (dist.count(v)) continue;
+        int cnt = 0;
+        for (int i=0; i<8; i++) {
+          if (u[i] != v[i]) cnt++;
+        }
+        if (cnt==1) {
+          dist[v] = dist[u] + 1;
+          q.push(v);
+        }
+      }
+    }
+    if (dist.count(end)) return dist[end];
+    else return -1;
+  }
+};
