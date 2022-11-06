b6d45b64463cfa59bc4e2b67ae039101e5a1fba4
Martin Yang <martin0327@gmail.com>
2022-11-07 01:16:24 +0900

    Time: 9 ms (44.83%), Space: 11.8 MB (17.82%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,22 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+class Solution {
+public:
+  string orderlyQueue(string s, int k) {
+    int n = s.size();
+    if (k>1) {
+      sort(s.begin(), s.end());
+      return s;
+    }
+    
+    string ans = s;
+    for (int i=0; i<n; i++) {
+      s += s.front();
+      s = s.substr(1, s.size()-1);
+      ans = min(ans, s);
+    }
+    return ans;
+    
+  }
+};
