9a9c504a56d8f6a33823b7955763ed005cce0964
Martin Yang <martin0327@gmail.com>
2022-11-10 09:45:53 +0900

    Time: 58 ms (51.54%), Space: 11 MB (66.38%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+  string removeDuplicates(string s) {
+    string t;
+    for (auto c : s) {
+      if (t.size() && t.back()==c) t.pop_back();
+      else t += c;
+    }
+    return t;
+  }
+};
