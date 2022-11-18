3bf77e301bbcbdf5e730338683fbc395d39b3a7a
Martin Yang <martin0327@gmail.com>
2022-11-18 09:58:45 +0900

    Time: 3 ms (65.96%), Space: 5.9 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
   bool isUgly(int n) {
     if (n<=0) return false;
-    while (!(n%2)) n /= 2;
-    while (!(n%3)) n /= 3;
-    while (!(n%5)) n /= 5;
+    for (int i=2; i<=5; i++) {
+      while (!(n%i)) n /= i;
+    }
     return n==1;
   }
 };
