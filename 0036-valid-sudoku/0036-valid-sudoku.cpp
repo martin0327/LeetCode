c27064f12558e78b549979df37f1b30558ff394e
Martin Yang <martin0327@gmail.com>
2022-11-23 09:18:52 +0900

    Time: 21 ms (94.55%), Space: 18.7 MB (57.65%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -4,23 +4,13 @@ public:
     bool isValidSudoku(vector<vector<char>>& a) {
         int n = 9;
         for (int i=0; i<n; i++) {
-            vi cnt(10);
-            for (int j=0; j<n; j++) {
-                if (a[i][j]!='.' && cnt[a[i][j]-'0']++) return false;
-            }
-        }
-        for (int j=0; j<n; j++) {
-            vi cnt(10);
-            for (int i=0; i<n; i++) {
-                if (a[i][j]!='.' && cnt[a[i][j]-'0']++) return false;
-            }
-        }
-        for (int i=0; i<n; i++) {
-            vi cnt(10);
+            vi c1(10), c2(10), c3(10);
             for (int j=0; j<n; j++) {
                 int r = 3*(i/3) + j/3;
                 int c = 3*(i%3) + j%3;
-                if (a[r][c]!='.' && cnt[a[r][c]-'0']++) return false;
+                if (a[i][j]!='.' && c1[a[i][j]-'0']++) return false;
+                if (a[j][i]!='.' && c2[a[j][i]-'0']++) return false;
+                if (a[r][c]!='.' && c3[a[r][c]-'0']++) return false;
             }
         }
         return true;
