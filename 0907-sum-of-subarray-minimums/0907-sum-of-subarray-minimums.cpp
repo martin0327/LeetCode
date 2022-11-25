221680b59c486ef4dda812cd3b567da4dc260234
Martin Yang <martin0327@gmail.com>
2022-11-25 09:41:23 +0900

    Time: 573 ms (7.33%), Space: 69.2 MB (5.03%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,27 @@
+using ll = long long;
+using pii = pair<ll,ll>;
+const int mod = 1e9+7;
+
+class Solution {
+public:
+  int sumSubarrayMins(vector<int>& a) {
+    int n = a.size();
+    vector<pii> b(n);
+    for (int i=0; i<n; i++) b[i] = {a[i],i};
+    sort(b.begin(), b.end());
+    set<int> s;
+    ll ans = 0;
+    for (auto [x,i] : b) {
+      auto it = s.lower_bound(i);
+      int l,r;
+      if (it==s.end()) r = n;
+      else r = *it;
+      if (it==s.begin()) l = -1;
+      else l = *prev(it);
+      ans += x * (r-i) * (i-l);
+      ans %= mod;
+      s.insert(i);
+    }
+    return ans;
+  }
+};
