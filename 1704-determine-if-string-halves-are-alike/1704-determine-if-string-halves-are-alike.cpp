17b009b179ae5a96a55ba77afeb20974370b8e95
Martin Yang <martin0327@gmail.com>
2022-12-01 09:27:05 +0900

    Time: 0 ms (100.00%), Space: 6.7 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -5,8 +5,8 @@ public:
     set<char> sc(t.begin(), t.end());
     int x=0, y=0;
     int n = s.size();
-    for (int i=0; i<n/2; i++) if (sc.count(s[i])) x++;
-    for (int i=n/2; i<n; i++) if (sc.count(s[i])) y++;
+    for (int i=0; i<n/2; i++) x += sc.count(s[i]);
+    for (int i=n/2; i<n; i++) y += sc.count(s[i]);
     return x==y;
   }
 };  
