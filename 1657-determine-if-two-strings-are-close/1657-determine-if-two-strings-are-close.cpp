ebb4ea7090f9d22096f71a3e5b0af6b128c8694e
Martin Yang <martin0327@gmail.com>
2022-12-02 09:56:31 +0900

    Time: 75 ms (94.91%), Space: 20.6 MB (64.37%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     bool closeStrings(string s, string t) {
         if (s.size() != t.size()) return false;
-        vector<int> cnt1(26), cnt2(26);
-        for (auto c : s) cnt1[c-'a']++;
-        for (auto c : t) cnt2[c-'a']++;
-        vector<int> a,b;
+        vector<int> a(26), b(26);
         int c = 0, d = 0;
+        for (auto c : s) a[c-'a']++;
+        for (auto c : t) b[c-'a']++;
         for (int i=0; i<26; i++) {
-            a.push_back(cnt1[i]);
-            b.push_back(cnt2[i]);
-            if (cnt1[i]) c |= 1<<i;
-            if (cnt2[i]) d |= 1<<i;
+            if (a[i]) c |= 1<<i;
+            if (b[i]) d |= 1<<i;
         }
         sort(a.begin(), a.end());
         sort(b.begin(), b.end());
