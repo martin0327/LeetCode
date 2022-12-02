11213e0624b4bfd4a7ef36d9a813a72594969f64
Martin Yang <martin0327@gmail.com>
2022-12-02 09:51:38 +0900

    Time: 253 ms (46.11%), Space: 20.7 MB (32.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool closeStrings(string s, string t) {
-        map<char,int> mp1, mp2;
-        for (auto c : s) mp1[c]++;
-        for (auto c : t) mp2[c]++;
+        vector<int> cnt1(26), cnt2(26);
+        for (auto c : s) cnt1[c-'a']++;
+        for (auto c : t) cnt2[c-'a']++;
         vector<int> a,b;
         int c = 0, d = 0;
-        for (auto &[k,v] : mp1) {
-            a.push_back(v);
-            c |= (1<<(k-'a'));
-        }
-        for (auto &[k,v] : mp2) {
-            b.push_back(v);
-            d |= (1<<(k-'a'));
+        for (int i=0; i<26; i++) {
+            a.push_back(cnt1[i]);
+            b.push_back(cnt2[i]);
+            if (cnt1[i]) c |= 1<<i;
+            if (cnt2[i]) d |= 1<<i;
         }
         sort(a.begin(), a.end());
         sort(b.begin(), b.end());
