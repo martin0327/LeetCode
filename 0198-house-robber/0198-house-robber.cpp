6d0a3ca4e87a5775f33e4d52b8184a2b12a78e27
Martin Yang <martin0327@gmail.com>
2022-12-14 09:38:54 +0900

    Time: 3 ms (56.21%), Space: 7.8 MB (50.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int rob(vector<int>& a) {
+    int n = a.size();
+    vector<int> dp(n);
+    dp[0] = a[0];
+    for (int i=1; i<n; i++) {
+      dp[i] = max(dp[i-1], a[i] + (i>1 ? dp[i-2] : 0));
+    }
+    return dp[n-1];
+  }
+};
