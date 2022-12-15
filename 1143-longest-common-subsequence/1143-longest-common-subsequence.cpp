94e2badfd3a688ced6b7640723c76651af71e127
Martin Yang <martin0327@gmail.com>
2022-12-15 09:49:36 +0900

    Time: 33 ms (64.49%), Space: 10.6 MB (79.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,7 +3,6 @@ public:
   int dp[1001][1001];
   int longestCommonSubsequence(string s, string t) {
     int n = s.size(), m = t.size();
-    int ans = 0;
     for (int i=1; i<=n; i++) {
       for (int j=1; j<=m; j++) {
         dp[i][j] = max({dp[i-1][j], dp[i][j-1], s[i-1]==t[j-1] ? dp[i-1][j-1]+1 : 0});
@@ -11,4 +10,4 @@ public:
     }
     return dp[n][m];
   }
-};  
+};
