4942dbc684c90def92f16ffc79a377cd0490f37f
Martin Yang <martin0327@gmail.com>
2022-12-19 09:44:10 +0900

    Time: 684 ms (88.28%), Space: 126.6 MB (88.28%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,43 @@
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+  private:
+    int _n;
+    std::vector<int> parent_or_size;
+};
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int s, int t) {
+        dsu d(n);
+        for (auto e : edges) {
+            d.merge(e[0],e[1]);
+        }
+        return d.same(s,t);
+    }
+};
