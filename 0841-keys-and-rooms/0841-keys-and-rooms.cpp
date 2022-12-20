3f001325dd78741590f5546df937690140c1f388
Martin Yang <martin0327@gmail.com>
2022-12-20 09:06:09 +0900

    Time: 27 ms (18.25%), Space: 10.5 MB (35.11%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,78 @@
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    std::vector<int> parent_or_size;
+};
+
+
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& a) {
+        int n = a.size();
+        queue<int> q;
+        q.push(0);
+        dsu d(n);
+        while (q.size()) {
+            int u = q.front();
+            q.pop();
+            for (auto v : a[u]) {
+                if (d.same(0,v)) continue;
+                d.merge(0,v);
+                q.push(v);
+            }
+        }
+        return d.size(0) == n;
+    }
+};
