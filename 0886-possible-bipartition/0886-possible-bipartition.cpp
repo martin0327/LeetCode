3fd318652191e807087a5f2a9e7d5ab69ded081f
Martin Yang <martin0327@gmail.com>
2022-12-21 09:32:49 +0900

    Time: 492 ms (33.45%), Space: 71.9 MB (25.05%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,34 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dis) {
+        vvi adj(n);
+        for (auto uv : dis) {
+            int u = --uv[0];
+            int v = --uv[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        queue<int> q;
+        vi check(n, -1);
+        bool ans = true;
+        for (int i=0; i<n; i++) {
+            if (check[i] != -1) continue;
+            check[i] = 0;
+            q.push(i);
+            while (q.size()) {
+                int u = q.front();
+                q.pop();
+                for (auto v : adj[u]) {
+                    if (check[v] == -1) {
+                        check[v] = check[u] ^ 1;
+                        q.push(v);
+                    }
+                    else if (check[v] == check[u]) ans = false;
+                }
+            }
+        }
+        return ans;
+    }
+};
