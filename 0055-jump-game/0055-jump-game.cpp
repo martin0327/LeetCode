0e3728fe269e5e2c3193409af1043ee441da4462
Martin Yang <martin0327@gmail.com>
2022-12-26 10:06:47 +0900

    Time: 86 ms (76.13%), Space: 48.5 MB (48.80%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool canJump(vector<int>& a) {
-        int n = a.size();
-        int sup = 0;
-        for (int i=0; i<n; i++) {
-            if (i <= sup) {
-                sup = max(sup, i + a[i]);
+        int mx = 0;
+        for (int i=0; i<a.size(); i++) {
+            if (i <= mx) {
+                mx = max(mx, i + a[i]);
             }
             else return false;
         }
