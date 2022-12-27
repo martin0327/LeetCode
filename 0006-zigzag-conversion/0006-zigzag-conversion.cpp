c349502236a8b11810ee35c2ea7a1622847c6e8a
Martin Yang <martin0327@gmail.com>
2022-12-27 10:17:21 +0900

    Time: 120 ms (11.67%), Space: 39.8 MB (11.13%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,30 @@
+using vc = vector<char>;
+using vvc = vector<vc>;
+class Solution {
+public:
+    string convert(string s, int k) {
+        if (k==1) return s;
+        int n = s.size();
+        vvc a(k, vc(n));
+        int i = 0, j = 0;
+        for (auto c : s) {
+            a[i][j] = c;
+            if (j % (k-1) == 0) {
+                if (i == k-1) {
+                    i--; j++;
+                }
+                else i++;
+            }
+            else {
+                i--; j++;
+            }
+        }
+        string ans;
+        for (int i=0; i<k; i++) {
+            for (int j=0; j<n; j++) {
+                if (a[i][j]) ans += a[i][j];
+            }
+        }
+        return ans;
+    }
+};
