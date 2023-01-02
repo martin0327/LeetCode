85e26a7c2c9ecda7965ecc719ad73cb5869432f1
tuttelikz <s.askaruly@unist.ac.kr>
2023-01-03 00:43:02 +0900

    Time: 1814 ms (42.74%), Space: 26.2 MB (27.29%) - LeetHub

--- 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
@@ -4,21 +4,17 @@ class Solution:
         n = len(price)
         ans = -1
         price.sort()
-
         while lo <= hi:
             mid = (lo + hi) // 2
             curr = price[0]
             cnt = 1
-
             for i in range(1, n):
                 if (price[i] - curr) >= mid:
                     curr = price[i]
                     cnt += 1
-            
             if cnt >= k:
                 ans = mid
                 lo = mid + 1
             else:
                 hi = mid - 1
-            
         return int(ans)
