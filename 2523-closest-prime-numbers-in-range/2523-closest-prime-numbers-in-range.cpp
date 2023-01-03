303c6a1b80ae9b3e86216246ebbaddf07840f8c3
Martin Yang <martin0327@gmail.com>
2023-01-03 19:19:31 +0900

    Time: 1661 ms (24.59%), Space: 180.3 MB (9.74%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,36 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+class Solution {
+public:    
+  vi primes;
+  vi is_prime;
+  void init_primes(int n) {
+    is_prime.assign(n+1, 1);
+    is_prime[0] = is_prime[1] = 0;
+    for (int i=2; i<=n; i++) {
+      if (is_prime[i]) {
+        primes.push_back(i);
+        for (int j=2*i; j<=n; j+=i) {
+          is_prime[j] = 0;
+        }
+      }
+    }
+  }
+  vector<int> closestPrimes(int left, int right) {
+    init_primes(right+1);
+    int n = primes.size();
+    vi ans = {-1,-1};
+    int min_diff = 1e9;
+    for (int i=1; i<n; i++) {
+      int x = primes[i-1];
+      int y = primes[i];
+      if (left <= x && y <= right) {
+        if (y-x < min_diff) {
+          ans = {x,y};
+          min_diff = y-x;
+        }
+      }
+    }
+    return ans;
+  }
+};
