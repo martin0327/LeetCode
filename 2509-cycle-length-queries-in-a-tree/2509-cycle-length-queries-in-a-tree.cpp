5f5b4df0d75bc95c9ef34dbed83dadde75b4bdd6
Martin Yang <martin0327@gmail.com>
2023-01-04 01:55:50 +0900

    Time: 525 ms (52.77%), Space: 74 MB (41.15%) - LeetHub

--- 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
@@ -7,37 +7,35 @@ public:
       int u = uv[0];
       int v = uv[1];
 
-      vi a,b;
-      for (int i=0; i<30; i++) {
-        if (u>>i&1) a.push_back(1);
-        else a.push_back(0);
-        if (v>>i&1) b.push_back(1);
-        else b.push_back(0);
+      if (u > v) swap(u,v);
+      int x = 0, y = 0;
+      {
+        int t = u;
+        while (t) {
+          t >>= 1;
+          x++;
+        }
       }
       
-      while (a.size()) {
-        if (a.back()) break;
-        else a.pop_back();
+      {
+        int t = v;
+        while (t) {
+          t >>= 1;
+          y++;
+        }
       }
-      while (b.size()) {
-        if (b.back()) break;
-        else b.pop_back();
+      int cnt = y - x;
+      while (y > x) {
+        y--;
+        v >>= 1;        
       }
-      reverse(a.begin(), a.end());
-      reverse(b.begin(), b.end());
-      
-      if (a.size() > b.size()) swap(a,b);
-      int cnt = 0;
-      while (a.size() < b.size()) {
-        cnt++;
-        b.pop_back();
-      }
-      while (a.size()) {
-        if (a == b) break;
+
+      while (u) {
+        if (u==v) break;
         else {
-          a.pop_back();
-          b.pop_back();
-          cnt+=2;
+          u >>= 1;
+          v >>= 1;
+          cnt += 2;
         }
       }
       ans.push_back(cnt+1);
