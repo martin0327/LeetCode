7d8c2c1353e5b3237ffdec4f17a4bf2bb39e0412
Martin Yang <martin0327@gmail.com>
2023-01-11 14:45:43 +0900

    Time: 196 ms (80.20%), Space: 65 MB (37.58%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -5,20 +5,22 @@ class Solution {
 public:
   int n;
   vvi adj;
+  vector<bool> a;
   int ans = 0;
-  bool f(int u, int p, vector<bool> &a) {
+  bool f(int u, int p) {
     bool ret = false;
     for (auto v : adj[u]) {
       if (v==p) continue;
-      if (f(v,u,a)) {
+      if (f(v,u)) {
         ans += 2;
         ret = true;
       }
     }
     return ret | a[u];
   }
-  int minTime(int _n, vector<vector<int>>& edges, vector<bool>& a) {
+  int minTime(int _n, vector<vector<int>>& edges, vector<bool>& hasApple) {
     n = _n;
+    a = hasApple;
     adj = vvi(n);
     for (auto uv : edges) {
       int u = uv[0];
@@ -26,7 +28,7 @@ public:
       adj[u].push_back(v);
       adj[v].push_back(u);
     }
-    f(0,-1,a);
+    f(0,-1);
     return ans;
   }
-};  
+};
