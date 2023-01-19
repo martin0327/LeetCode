90272a1a75afd4876351971e8c9ca53673c6917c
Martin Yang <martin0327@gmail.com>
2023-01-19 20:27:09 +0900

    Time: 105 ms (98.83%), Space: 15.3 MB (21.31%) - LeetHub

--- 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.py
+++ 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.py
@@ -2,13 +2,11 @@ class Solution:
     def isItPossible(self, s: str, t: str) -> bool:
         c1 = Counter(s)
         c2 = Counter(t)
-        l1 = len(c1)
-        l2 = len(c2)
         for x, y in c1.items():
           for z, w in c2.items():
-            t1 = l1
-            t2 = l2
-            t1 += (z not in c1) - (y==1 and x != z)
-            t2 += (x not in c2) - (w==1 and x != z)
-            if t1 == t2: return True
+            l1 = len(c1)
+            l2 = len(c2)
+            l1 += (z not in c1) - (y==1 and x != z)
+            l2 += (x not in c2) - (w==1 and x != z)
+            if l1 == l2: return True
         return False
