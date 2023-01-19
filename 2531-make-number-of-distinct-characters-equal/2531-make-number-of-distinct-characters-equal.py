8ffbd57a9159ffcfd8e9e6b84c0c39103c7b078b
Martin Yang <martin0327@gmail.com>
2023-01-19 20:26:06 +0900

    Time: 116 ms (91.76%), Space: 15.4 MB (21.31%) - LeetHub

--- 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.py
+++ 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.py
@@ -6,12 +6,9 @@ class Solution:
         l2 = len(c2)
         for x, y in c1.items():
           for z, w in c2.items():
-            if x == z:
-              if l1 == l2: return True
-            else:
-              t1 = l1
-              t2 = l2
-              t1 += (z not in c1) - (y==1)
-              t2 += (x not in c2) - (w==1)
-              if t1 == t2: return True
+            t1 = l1
+            t2 = l2
+            t1 += (z not in c1) - (y==1 and x != z)
+            t2 += (x not in c2) - (w==1 and x != z)
+            if t1 == t2: return True
         return False
