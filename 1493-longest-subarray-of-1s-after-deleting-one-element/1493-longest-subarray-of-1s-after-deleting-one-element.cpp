a6087563757dd686541839faff81e931308dbce2
Martin Yang <martin0327@gmail.com>
2023-01-20 16:40:18 +0900

    Time: 51 ms (52.87%), Space: 36.5 MB (51.63%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,18 +3,11 @@ class Solution {
 public:
   int longestSubarray(vector<int>& a) {
     int n = a.size();
-    a.insert(a.begin(), 0);
-    a.push_back(0);
-    vi pre = a, suf = a;
-    for (int i=1; i<=n; i++) {
-      if (pre[i]) pre[i] += pre[i-1];
-    }
-    for (int i=n; i>=1; i--) {
-      if (suf[i]) suf[i] += suf[i+1];
-    }
-    int ans = 0;
-    for (int i=1; i<=n; i++) {
-      ans = max(ans, pre[i-1] + suf[i+1]);
+    int i=0, j=0, cnt=0, ans=0;
+    while (i<n) {
+      if (cnt < 2 && j < n) cnt += (a[j++]==0);
+      else cnt -= (a[i++]==0);
+      if (cnt < 2) ans = max(ans, j-i-1); 
     }
     return ans;
   }
