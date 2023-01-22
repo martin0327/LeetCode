0d895fbbc6fe6380351bb023731ee6b7753d8ba2
Martin Yang <martin0327@gmail.com>
2023-01-22 12:52:11 +0900

    Time: 211 ms (43.25%), Space: 118.5 MB (23.86%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,23 +1,19 @@
 using vs = vector<string>;
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        int n = s.size()-1;
-        vector<vs> ret;
-        for (int mask=0; mask<1<<n; mask++) {
-            vs a = {string(1,s[0])};
-            for (int i=0; i<n; i++) {
-                if (mask>>i&1) a.push_back(string(1,s[i+1]));
-                else a.back().push_back(s[i+1]);
-            }
-            bool ok = true;
-            for (auto x : a) {
-                string y = x;
-                reverse(y.begin(), y.end());
-                ok &= (x==y);
-            }
-            if (ok) ret.push_back(a);
-        }
-        return ret;
+  vector<vector<string>> partition(string s) {
+    int n = s.size()-1;
+    vector<vs> ret;
+    for (int mask=0; mask<1<<n; mask++) {
+      vs a = {string(1,s[0])};
+      for (int i=0; i<n; i++) {
+        if (mask>>i&1) a.push_back(string(1,s[i+1]));
+        else a.back().push_back(s[i+1]);
+      }
+      bool ok = true;
+      for (auto x : a) ok &= equal(x.begin(), x.end(), x.rbegin());
+      if (ok) ret.push_back(a);
     }
-};         
+    return ret;
+  }
+};
