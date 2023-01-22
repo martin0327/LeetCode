25b50f4540605ff21d7b2ea67a530151a17b6782
Martin Yang <martin0327@gmail.com>
2023-01-22 10:11:25 +0900

    Time: 216 ms (41.85%), Space: 118.5 MB (23.86%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,23 @@
+using vs = vector<string>;
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        int n = s.size()-1;
+        vector<vs> ret;
+        for (int mask=0; mask<1<<n; mask++) {
+            vs a = {string(1,s[0])};
+            for (int i=0; i<n; i++) {
+                if (mask>>i&1) a.push_back(string(1,s[i+1]));
+                else a.back().push_back(s[i+1]);
+            }
+            bool ok = true;
+            for (auto x : a) {
+                string y = x;
+                reverse(y.begin(), y.end());
+                if (x!=y) ok = false;
+            }
+            if (ok) ret.push_back(a);
+        }
+        return ret;
+    }
+};
