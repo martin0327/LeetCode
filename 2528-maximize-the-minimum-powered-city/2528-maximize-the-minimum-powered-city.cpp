c9a3c3da4b7a6d14d4fbddd4eab70632c801ccf0
Martin Yang <martin0327@gmail.com>
2023-01-27 21:40:12 +0900

    Time: 324 ms (64.48%), Space: 242.2 MB (47.23%) - LeetHub

--- 2528-maximize-the-minimum-powered-city/2528-maximize-the-minimum-powered-city.cpp
+++ 2528-maximize-the-minimum-powered-city/2528-maximize-the-minimum-powered-city.cpp
@@ -1,55 +1,27 @@
-template <class T> struct fenwick_tree {
-  public:
-    fenwick_tree() : _n(0) {}
-    fenwick_tree(int n) : _n(n), data(n) {}
-
-    void add(int p, T x) {
-        assert(0 <= p && p < _n);
-        p++;
-        while (p <= _n) {
-            data[p - 1] += T(x);
-            p += p & -p;
-        }
-    }
-
-    T sum(int l, int r) {
-        assert(0 <= l && l <= r && r <= _n);
-        return sum(r) - sum(l);
-    }
-
-  private:
-    int _n;
-    std::vector<T> data;
-
-    T sum(int r) {
-        T s = 0;
-        while (r > 0) {
-            s += data[r - 1];
-            r -= r & -r;
-        }
-        return s;
-    }
-};
-
 using ll = long long;
 
 class Solution {
 public:
   long long maxPower(vector<int>& a, int r, int k) {
-    ll n = a.size();    
-    ll lo = 0, hi = 2e10, ans = -1;
+    ll n = a.size(), lo = 0, hi = 2e10, ans = -1, ss = 0;
+    for (int i=0; i<r; i++) ss += a[i];
 
     while (lo <= hi) {
       ll mid = (lo + hi) / 2;
-      fenwick_tree<ll> fen(n);
-      for (ll i=0; i<n; i++) fen.add(i, a[i]);
+      ll llim = 0, rlim = r-1;
+      ll s = ss;
       ll cnt = 0;
+      vector<ll> b(n);
       for (ll i=0; i<n; i++) {
-        ll llim = max(0ll, i - r);
-        ll rlim = min(n-1, i + r);
-        ll s = fen.sum(llim,rlim+1);
+        if (llim < i-r) {
+          s -= a[llim];
+          s -= b[llim++];
+        }
+        if (rlim < n-1) s += a[++rlim];
+        
         ll d = max(0ll, mid - s);
-        fen.add(rlim,d);
+        b[rlim] += d;
+        s += d;
         cnt += d;
         if (cnt > k) break;
       }
@@ -59,6 +31,7 @@ public:
       }
       else hi = mid - 1;
     }
+
     return ans;
   }
 };  
