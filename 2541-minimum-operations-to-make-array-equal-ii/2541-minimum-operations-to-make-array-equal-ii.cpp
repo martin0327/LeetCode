94d79bea4f1bc8186d4705015a9d32ae34f20479
Martin Yang <martin0327@gmail.com>
2023-01-28 00:40:09 +0900

    Time: 215 ms (37.29%), Space: 73 MB (25.21%) - LeetHub

--- 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
@@ -4,8 +4,7 @@ class Solution {
 public:
   long long minOperations(vector<int>& a, vector<int>& b, int k) {
     if (k==0) return a==b?0:-1;
-    ll n = a.size();
-    ll cnt = 0, ans = 0;
+    ll n = a.size(), cnt = 0, ans = 0;
     for (int i=0; i<n; i++) {
       if (a[i]%k != b[i]%k) return -1;
       ll x = a[i]/k - b[i]/k;
