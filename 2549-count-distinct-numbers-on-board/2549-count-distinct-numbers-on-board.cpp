9fee23707369f69a9ce82fecffee759d6f0a1e5e
Martin Yang <martin0327@gmail.com>
2023-02-03 15:27:14 +0900

    Time: 0 ms (100.00%), Space: 6 MB (32.69%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
@@ -2,22 +2,5 @@ class Solution {
 public:
     int distinctIntegers(int n) {
         return n==1 ? 1 : n-1;
-        set<int> s;
-        queue<int> q;
-        q.push(n);
-        s.insert(n);
-        while (q.size()) {
-            int x = q.front();
-            q.pop();
-            for (int i=1; i<=n; i++) {
-                if (s.count(i)) continue;
-                if (x % i == 1) {
-                    q.push(i);
-                    s.insert(i);
-                }
-            }
-        }
-        int ans = s.size();
-        return ans;
     }
 };
