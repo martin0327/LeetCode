2883637b959fe558bae790f644e45fc789019c8e
Martin Yang <martin0327@gmail.com>
2023-02-03 15:19:04 +0900

    Time: 33 ms (71.59%), Space: 13.2 MB (40.66%) - LeetHub

--- 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.cpp
+++ 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     bool makeStringsEqual(string s, string t) {
-        int n = s.size();
-        int cnt = 0, cnt2 = 0;
-        for (auto c : s) cnt += c == '1';
-        for (auto c : t) cnt2 += c == '1';
-        if (cnt) {
-            if (cnt2) return true;
-            else return false;
-        }
-        else {
-            if (cnt2) return false;
-            else return true;
-        }
-        return true;
+        int x = 0, y = 0;
+        for (auto c : s) x += c == '1';
+        for (auto c : t) y += c == '1';
+        return !(x>0 ^ y>0);
     }
 };
