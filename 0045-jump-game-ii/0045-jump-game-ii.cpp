e4f0abd31534b172a789e9d2bb1045c5a50dfd24
Martin Yang <martin0327@gmail.com>
2023-02-08 10:35:10 +0900

    Time: 20 ms (66.62%), Space: 16.7 MB (43.97%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
   int jump(vector<int>& a) {
     int n = a.size();
-    vector<int> b = {0}; // b[i] = i번 안에 갈 수 있는 가장 큰 index
+    vector<int> b = {0};
     for (int i=0; i<n; i++) {
       auto it = lower_bound(b.begin(), b.end(), i);
       int cnt = it - b.begin();
@@ -13,13 +13,7 @@ public:
       }
       else b[cnt+1] = max(b[cnt+1], min(i+a[i],n-1));
     }
-    int ans;
-    for (int i=0; i<b.size(); i++) {
-      if (b[i]==n-1) {
-        ans = i;
-        break;
-      } 
-    }
-    return ans;
+    for (int i=0; i<b.size(); i++) if (b[i]==n-1) return i; 
+    return 0;
   }
-};
+}; 
