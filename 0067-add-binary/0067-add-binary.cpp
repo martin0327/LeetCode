45e32972a91a3256d7e12b55020bd9b625d92942
Martin Yang <martin0327@gmail.com>
2023-02-14 09:15:26 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (61.65%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+  string addBinary(string a, string b) {
+    string ret;
+    int x = 0;
+    while (a.size() && b.size()) {
+      int y = a.back() - '0'; 
+      a.pop_back();
+      int z = b.back() - '0'; 
+      b.pop_back();
+      int s = x+y+z;
+      ret += (s&1) + '0';
+      x = s / 2;
+    }
+    while (a.size()) {
+      int s = x + a.back() - '0'; a.pop_back();
+      ret += (s&1) + '0';
+      x = s / 2;
+    }
+    while (b.size()) {
+      int s = x + b.back() - '0';
+      b.pop_back();
+      ret += (s&1) + '0';
+      x = s / 2;
+    }
+    if (x) ret += '1';
+    reverse(ret.begin(), ret.end());
+    return ret;
+  }
+};
