82b302de0e5b2c16592b8c168654f77209e26d78
Martin Yang <martin0327@gmail.com>
2023-02-24 09:15:46 +0900

    Time: 1744 ms (5.08%), Space: 282.2 MB (5.93%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,18 +1,16 @@
-using ll = long long;
 class Solution {
 public:
   int minimumDeviation(vector<int>& a) {
     int n = a.size();
-    multiset<ll> s;
+    multiset<int> s;
     for (int i=0; i<n; i++) {
       if (a[i]&1) s.insert(2*a[i]);
       else s.insert(a[i]);
     }
-    ll ans = 2e9, cur_max = 2e9;
+    int ans = 2e9, cur_max = 2e9;
     bool done = false;
     while (s.size()>1) {
-      // debug(ms);
-      ll hi = *prev(s.end()), lo = *s.begin();
+      int hi = *prev(s.end()), lo = *s.begin();
       if (!done) cur_max = min(cur_max, hi);
       ans = min(ans, cur_max - lo);
       if (hi%2==0) {
@@ -26,4 +24,4 @@ public:
     }
     return ans;    
   }
-};
+}; 
