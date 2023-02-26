5e8005ebc7453bd83b4aca506165ea171fc9893f
Martin Yang <martin0327@gmail.com>
2023-02-27 01:12:56 +0900

    Time: 22 ms (35.49%), Space: 9.1 MB (42.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -9,13 +9,11 @@ public:
     const int inf = 1e9;
     vvi dp(n+1, vi(m+1,inf));
     dp[0][0] = 0;
-    s = ' ' + s;
-    t = ' ' + t;
     for (int i=0; i<=n; i++) {
       for (int j=0; j<=m; j++) {
         if (i) chmin(dp[i][j], dp[i-1][j]+1);
         if (j) chmin(dp[i][j], dp[i][j-1]+1);
-        if (i&&j) chmin(dp[i][j], dp[i-1][j-1]+(s[i]!=t[j]));
+        if (i&&j) chmin(dp[i][j], dp[i-1][j-1]+(s[i-1]!=t[j-1]));
       }
     }
     return dp[n][m];
