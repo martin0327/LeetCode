b522307a9ee14f4430cf580f80a4f2a7493cda20
Martin Yang <martin0327@gmail.com>
2023-02-27 11:19:58 +0900

    Time: 97 ms (5.07%), Space: 27.4 MB (5.44%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -0,0 +1,90 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+class Solution {
+public:
+  vvi a;
+  int n;
+  
+  
+  Node* f(int r1, int r2, int c1, int c2) {
+    if (r2-r1==1) {
+      Node* ret = new Node(a[r1][c1], true);
+      return ret;
+    }
+    int rm = (r1+r2)/2;
+    int cm = (c1+c2)/2;
+    vvi dr = {{r1,rm},{rm,r2}};
+    vvi dc = {{c1,cm},{cm,c2}};
+    vector<Node*> vv;
+    for (auto r : dr) {
+      for (auto c : dc) {
+        vv.push_back(f(r[0],r[1],c[0],c[1]));
+      }
+    }
+    bool leaf = true;
+    int cnt = 0;
+    for (auto v : vv) {
+      if (!v->isLeaf) leaf = false;
+      cnt += v->val;
+    }
+    leaf &= (cnt==0 || cnt==4);
+    Node* ret = new Node();
+    if (leaf) {
+      ret->isLeaf = true;
+      ret->val = (cnt==4);
+    }
+    else {
+      ret->topLeft = vv[0];
+      ret->topRight = vv[1];
+      ret->bottomLeft = vv[2];
+      ret->bottomRight = vv[3];
+    }
+    return ret;
+  }
+  Node* construct(vector<vector<int>>& grid) {
+    a = grid;
+    n = a.size();
+    Node* ret = f(0,n,0,n);
+    return ret;        
+  }
+};
