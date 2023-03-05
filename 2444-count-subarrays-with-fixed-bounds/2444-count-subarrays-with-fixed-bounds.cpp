c62be9ddbc2a5bc91dbd9592bac5e1c515820377
Martin Yang <martin0327@gmail.com>
2023-03-05 14:24:30 +0900

    Time: 119 ms (73.75%), Space: 70.5 MB (57.92%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,59 +1,20 @@
-using ll = long long;
-using vi = vector<ll>;
-
 class Solution {
 public:
-  long long countSubarrays(vector<int>& a, int minK, int maxK) {
-
-    ll n = a.size(), ans = 0;
-    set<ll> mn, mx, blk; 
-    if (minK > maxK) return 0;
-    else if (minK == maxK) {
-      mx = blk = {n};
-      for (int i=0; i<n; i++) {
-        if (a[i]==maxK) mx.insert(i);
-        else if (a[i] < minK || a[i] > maxK) blk.insert(i);
-      }
-      for (int i=0; i<n; i++) {
-        if (blk.count(i)) continue;
-        if (a[i] == maxK) ans += *blk.upper_bound(i) - i;
-        else {
-          ll r = *mx.upper_bound(i);
-          ll lim = *blk.upper_bound(i);
-          if (r < lim) ans += lim - r;
+    long long countSubarrays(vector<int>& a, int lo, int hi) {
+        if (lo > hi) return 0;
+        int n = a.size();
+        int mn = -1, mx = -1, blk = -1;
+        long long ans = 0;
+        for (int i=0; i<n; i++) {
+            if (a[i] < lo || a[i] > hi) {
+                blk = i;
+                continue;
+            }
+            if (a[i] == lo) mn = i;
+            if (a[i] == hi) mx = i;
+            int lim = min(mn,mx);
+            if (lim > blk) ans += lim - blk;
         }
-      }
+        return ans;
     }
-    else {
-      mn = mx = blk = {n};
-      for (int i=0; i<n; i++) {
-        if (a[i] == minK) mn.insert(i);
-        else if (a[i] == maxK) mx.insert(i);
-        else if (a[i] < minK || a[i] > maxK) blk.insert(i); 
-      }
-
-      for (int i=0; i<n; i++) {
-        if (blk.count(i)) continue;
-        if (a[i] == minK) {
-          ll r = *mx.upper_bound(i);
-          ll lim = *blk.upper_bound(i);
-          if (r < lim) ans += lim - r;
-        }
-        else if (a[i] == maxK) {
-          ll r = *mn.upper_bound(i);
-          ll lim = *blk.upper_bound(i);
-          if (r < lim) ans += lim - r;
-        }
-        else {
-          ll l = *mn.upper_bound(i);
-          ll r = *mx.upper_bound(i);
-          if (l > r) swap(l,r);
-          ll lim = *blk.upper_bound(i);
-          if (r < lim) ans += lim - r;
-        }
-      }
-    }
-    return ans;
-
-  }
 };
