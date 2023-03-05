7400399014785825290f3ec3f540ef60eb4723fd
Martin Yang <martin0327@gmail.com>
2023-03-05 14:40:43 +0900

    Time: 120 ms (72.08%), Space: 70.5 MB (57.92%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -3,14 +3,14 @@ public:
     long long countSubarrays(vector<int>& a, int lo, int hi) {
         if (lo > hi) return 0;
         int n = a.size();
-        int mn = -1, mx = -1, blk = -1;
+        int mn = -1, mx = -1, l = -1;
         long long ans = 0;
         for (int i=0; i<n; i++) {
-            if (a[i] < lo || a[i] > hi) blk = i;
+            if (a[i] < lo || a[i] > hi) l = i;
             if (a[i] == lo) mn = i;
             if (a[i] == hi) mx = i;
-            int lim = min(mn,mx);
-            if (lim > blk) ans += lim - blk;
+            int r = min(mn,mx);
+            if (r > l) ans += r - l;
         }
         return ans;
     }
