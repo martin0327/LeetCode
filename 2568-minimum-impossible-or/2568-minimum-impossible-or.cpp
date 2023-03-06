1cddcb00e1da5efb6d1bac49510303c8d55bfdd2
Martin Yang <martin0327@gmail.com>
2023-03-06 21:14:37 +0900

    Time: 89 ms (99.58%), Space: 47.7 MB (78.59%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
   int minImpossibleOR(vector<int>& a) {
+    int y = 0;
+    for (auto x : a) {
+      if ((x & (x - 1)) == 0) y |= x;
+    }
     for (int i=0; i<30; i++) {
-      bool found = false;
-      for (auto x : a) if (x==(1<<i)) found = true;
-      if (!found) return (1<<i);
+      if ((y>>i&1)==0) return 1<<i; 
     }
     return 1<<30;
   }
