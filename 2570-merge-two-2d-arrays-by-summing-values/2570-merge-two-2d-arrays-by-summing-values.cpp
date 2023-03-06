457cb4800a80e559650688995f749992140cc781
Martin Yang <martin0327@gmail.com>
2023-03-06 19:46:23 +0900

    Time: 7 ms (93.52%), Space: 10.5 MB (99.50%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
   vector<vector<int>> mergeArrays(vector<vector<int>>& a, vector<vector<int>>& b) {
     vector<vector<int>> ret;
-    int i=0, j=0, n=a.size(), m=b.size();
-    while (i<n || j<m) {
+    int n=a.size(), m=b.size();
+    for (int i=0,j=0; i<n || j<m;) {
       if (i<n && j<m) {
         if (a[i][0] == b[j][0]) {
           ret.push_back({a[i][0], a[i++][1] + b[j++][1]});
