23b5ada96b00f2b963c25ef8c1d2d801a5fb3f31
Martin Yang <martin0327@gmail.com>
2023-03-13 20:42:21 +0900

    Time: 41 ms (98.99%), Space: 13.8 MB (93.14%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -1,8 +1,8 @@
 class Solution:
     def finalValueAfterOperations(self, a: List[str]) -> int:
-        # return sum(1 if '+' in x else -1 for x in a)
-        ret = 0
-        for x in a:
-            if '+' in x: ret += 1
-            else: ret -= 1
-        return ret
+        return sum(1 if '+' in x else -1 for x in a)
+        # ret = 0
+        # for x in a:
+        #     if '+' in x: ret += 1
+        #     else: ret -= 1
+        # return ret
