445bd6dea1d7a827fad18e60ff9778d8da06d3e9
Martin Yang <martin0327@gmail.com>
2023-03-13 19:14:44 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (49.24%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -5,7 +5,7 @@ public:
     int ans = 0;
     for (int i=0; i<n; i++) {
       if (s[i] == 'X') {
-        ans++;
+        ++ans;
         for (int j=i+1; j<min(n,i+3); j++) {
           s[j] = 'O';
         }
