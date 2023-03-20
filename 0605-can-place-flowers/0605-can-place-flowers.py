8010a37f265d7de57f966bd28dcdcb4581562f34
Martin Yang <martin0327@gmail.com>
2023-03-20 11:06:49 +0900

    Time: 220 ms (8.42%), Space: 14.5 MB (20.94%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,15 +1,12 @@
 class Solution:
   def canPlaceFlowers(self, a: List[int], k: int) -> bool:
-    cnt = 0
     n = len(a)
     a = [0] + a + [0]
-
-
     for i in range(1,n+1):  
-      if reduce(lambda x,y : x|y, [a[i+j] for j in range(-1,2)]) == 0:
+      if sum(a[i+j] for j in range(-1,2)) == 0:
         a[i] = 1
-        cnt += 1
-    return cnt >= k
+        k -= 1
+    return k <= 0
           
                    
           
