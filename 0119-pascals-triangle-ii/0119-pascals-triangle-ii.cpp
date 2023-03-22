c4ab70c3fa2a889e73b53dd13e2247c813c0f77e
Martin Yang <martin0327@gmail.com>
2023-03-22 21:39:56 +0900

    Time: 4 ms (34.85%), Space: 6.7 MB (15.80%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,15 +1,15 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
 class Solution {
 public:
     vector<int> getRow(int n) {
-        vector<int> a = {1};
-        for (int i=0;i<n; i++) {
-            vector<int> b = {1};
-            for (int i=1; i<a.size(); i++) {
-                b.push_back(a[i]+a[i-1]);
+        vvi dp(n+1, vi(n+1));
+        dp[0][0] = 1;
+        for (int i=1; i<=n; i++) {
+            for (int j=0; j<=i; j++) {
+                dp[i][j] = dp[i-1][j] + ((j>0) ? dp[i-1][j-1] : 0);
             }
-            b.push_back(1);
-            a = b;
         }
-        return a;
+        return dp[n];
     }
 };
