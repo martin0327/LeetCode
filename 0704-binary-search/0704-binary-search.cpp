26156775143d7cf9ca6e1c2bd3aff743dd8fc05e
Martin Yang <martin0327@gmail.com>
2023-04-01 22:44:48 +0900

    Time: 35 ms (83.54%), Space: 27.7 MB (18.34%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int search(vector<int>& a, int t) {
-        int n = a.size();
-        for (int i=0; i<n; i++) {
-            if (a[i] == t) return i;
-        }
-        return -1;
+        auto it = lower_bound(a.begin(), a.end(),t);
+        if (it != a.end() && *it==t) return (int) (it - a.begin());
+        else return -1;
     }
 };
