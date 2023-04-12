7741ad77736cf20a6299726134a30bc46c2c8fbd
Martin Yang <martin0327@gmail.com>
2023-04-12 17:37:07 +0900

    Time: 1155 ms (76.65%), Space: 0B (100.00%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,15 @@
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM
+    products p
+JOIN
+    orders o
+ON
+    p.product_id = o.product_id
+WHERE
+    o.order_date BETWEEN "2020-02-01" AND "2020-02-29"
+GROUP BY
+    p.product_id
+HAVING
+    unit >= 100
