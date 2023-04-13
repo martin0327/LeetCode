4e91641925a31eb21e42295c4460500120fb3146
Martin Yang <martin0327@gmail.com>
2023-04-14 06:58:18 +0900

    Time: 1708 ms (48.46%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,22 +1,13 @@
-WITH
-  t AS (
-    SELECT
-      st.student_id,
-      st.student_name,
-      sb.subject_name
-    FROM
-      students st
-      JOIN subjects sb
-  )
 SELECT
-  t.student_id,
-  t.student_name,
-  t.subject_name,
+  st.student_id,
+  st.student_name,
+  sb.subject_name,
   COUNT(e.subject_name) AS attended_exams
 FROM
-  t
-  LEFT JOIN examinations e ON t.student_id=e.student_id
-  AND t.subject_name=e.subject_name
+  students st
+  JOIN subjects sb
+  LEFT JOIN examinations e ON st.student_id=e.student_id
+  AND sb.subject_name=e.subject_name
 GROUP BY
   student_id,
   subject_name
