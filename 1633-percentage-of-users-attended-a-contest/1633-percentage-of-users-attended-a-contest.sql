ee732177ed7458ec35ceb3aee6cd5e3726cc8b34
Martin Yang <martin0327@gmail.com>
2023-04-14 16:20:44 +0900

    Time: 1680 ms (72.84%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,18 +1,26 @@
+WITH
+  t1 AS (
+    SELECT
+      COUNT(*) AS tot
+    FROM
+      users
+  ),
+  t2 AS (
+    SELECT
+      r.contest_id,
+      COUNT(u.user_id) AS cnt
+    FROM
+      register r
+      JOIN users u ON u.user_id=r.user_id
+    GROUP BY
+      contest_id
+  )
 SELECT
-  contest_id,
-  ROUND(
-    100*COUNT(DISTINCT user_id)/(
-      SELECT
-        COUNT(*)
-      FROM
-        users
-    ),
-    2
-  ) AS percentage
+  t2.contest_id,
+  ROUND(100*t2.cnt/t1.tot, 2) AS percentage
 FROM
-  register
-GROUP BY
-  contest_id
+  t2
+  JOIN t1
 ORDER BY
   percentage desc,
-  contest_id asc  
+  contest_id asc
