0242a22d010b811fc49ade01b7ed3080946a5baf
Martin Yang <martin0327@gmail.com>
2023-04-14 21:38:42 +0900

    Time: 775 ms (50.53%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,14 +1,13 @@
 WITH
   t AS (
     SELECT
-      num,
-      COUNT(num) AS cnt
+      num
     FROM
       mynumbers
     GROUP BY
       num
     HAVING
-      cnt=1
+      COUNT(*)=1
   )
 SELECT
   MAX(num) AS num
