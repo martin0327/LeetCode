99acb92df0ff4febd5e86a3a987d8dd0ed0abbc8
Martin Yang <martin0327@gmail.com>
2023-04-15 01:54:23 +0900

    Time: 1136 ms (35.53%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,16 +1,13 @@
 WITH
   t AS (
     SELECT
-      id,
-      country,
-      state,
-      amount,
-      date_format (trans_date, '%Y-%m') AS MONTH
+      *,
+      date_format (trans_date, '%Y-%m') AS month
     FROM
       transactions
   )
 SELECT
-  MONTH,
+  month,
   country,
   COUNT(*) AS trans_count,
   SUM(if (state='approved', 1, 0)) AS approved_count,
@@ -19,5 +16,5 @@ SELECT
 FROM
   t
 GROUP BY
-  MONTH,
+  month,
   country
