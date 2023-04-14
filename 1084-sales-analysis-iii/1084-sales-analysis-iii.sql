a5c561f15d1a5eba17391b35ac83f4e5256f98a1
Martin Yang <martin0327@gmail.com>
2023-04-14 23:32:44 +0900

    Time: 2507 ms (8.07%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,11 @@
+SELECT
+  p.product_id,
+  p.product_name
+FROM
+  product p
+  JOIN sales s ON p.product_id=s.product_id
+GROUP BY
+  p.product_id
+HAVING
+  MAX(s.sale_date)<="2019-03-31"
+  AND MIN(s.sale_date)>="2019-01-01"
