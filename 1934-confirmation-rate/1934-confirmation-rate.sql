214b3e78e1b8bd5589448e103cebe3e2b5bd15a3
Martin Yang <martin0327@gmail.com>
2023-04-15 14:38:49 +0900

    Time: 1023 ms (98.74%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,8 +1,8 @@
-select
+SELECT
     s.user_id,
-    round(avg(if(action = 'confirmed', 1, 0)), 2) as confirmation_rate
-from
+    ROUND(AVG(IF(ACTION = 'confirmed', 1, 0)), 2) AS confirmation_rate
+FROM
     signups s
-    left join confirmations c on s.user_id = c.user_id
-group by
+    LEFT JOIN confirmations c ON s.user_id = c.user_id
+GROUP BY
     s.user_id
