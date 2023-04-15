0242dfbdf7b65fcf1aa8acb42e713358cb5e610e
Martin Yang <martin0327@gmail.com>
2023-04-15 16:11:05 +0900

    Time: 609 ms (42.46%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,19 @@
+SELECT
+    (
+        CASE
+            WHEN id = (
+                SELECT
+                    MAX(id)
+                FROM
+                    seat
+            )
+            AND id % 2 = 1 THEN id
+            WHEN id % 2 = 0 THEN id - 1
+            ELSE id + 1
+        END
+    ) AS id,
+    student
+FROM
+    seat
+ORDER BY
+    id
