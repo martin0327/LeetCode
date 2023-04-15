f890e838c49843b74bf97fdabd6d2280a3628a4a
Martin Yang <martin0327@gmail.com>
2023-04-15 14:36:52 +0900

    Time: 1117 ms (89.99%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,37 +1,8 @@
-with
-    t1 as (
-        select
-            user_id,
-            count(*) as cnt
-        from
-            confirmations
-        where
-            action = 'confirmed'
-        group by
-            user_id
-    ),
-    t2 as (
-        select
-            user_id,
-            count(*) as cnt
-        from
-            confirmations
-        group by
-            user_id
-    )
 select
     s.user_id,
-    round(
-        if(
-            t1.cnt is null
-            or t2.cnt is null
-            or t2.cnt = 0,
-            0,
-            t1.cnt / t2.cnt
-        ),
-        2
-    ) as confirmation_rate
+    round(avg(if(action = 'confirmed', 1, 0)), 2) as confirmation_rate
 from
-    signups as s
-    left join t1 on s.user_id = t1.user_id
-    left join t2 on s.user_id = t2.user_id
+    signups s
+    left join confirmations c on s.user_id = c.user_id
+group by
+    s.user_id
