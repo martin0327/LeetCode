f0a85f1a09df4024db88988407228d2857bd94b0
Martin Yang <martin0327@gmail.com>
2023-04-16 10:28:40 +0900

    Time: 257 ms (65.09%), Space: 88.1 MB (6.90%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,28 +1,32 @@
 using ll = long long;
 using vi = vector<ll>;
 using vvi = vector<vi>;
-const int mod = 1e9+7;
+const ll mod = 1e9+7;
 
 class Solution {
 public:
-    int numWays(vector<string>& words, string a) {
-        int n = a.size();
+    int numWays(vector<string>& words, string target) {
+        int n = target.size();
+        vi a(n);
+        for (int i=0; i<n; i++) a[i] = target[i] - 'a';
+        
         int m = words[0].size();
-
         vvi b(m, vi(26));
         for (auto x : words) {
             for (int i=0; i<m; i++) {
                 b[i][x[i]-'a']++;
             }
         }
+        
         vvi dp(n+1, vi(m+1));
         dp[0] = vi(m+1, 1);
         for (int i=0; i<n; i++) {
             for (int j=i; j<m; j++) {
-                dp[i+1][j+1] = dp[i][j] * b[j][a[i]-'a'] + dp[i+1][j];
+                dp[i+1][j+1] = dp[i][j] * b[j][a[i]] + dp[i+1][j];
                 dp[i+1][j+1] %= mod;
             }
         }
+        
         return dp[n][m];
     }
 };
