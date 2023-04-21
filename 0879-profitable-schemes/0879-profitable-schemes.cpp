f85748acead8f624ec2b05e0768c9f595948ffad
Martin Yang <martin0327@gmail.com>
2023-04-21 09:56:31 +0900

    Time: 118 ms (91.04%), Space: 9.7 MB (77.83%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -0,0 +1,27 @@
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll mod = 1e9+7;
+
+class Solution {
+public:
+    int profitableSchemes(int n, int m, vector<int>& a, vector<int>& b) {
+        vvi dp(n+1, vi(m+1));
+        dp[0][0] = 1;
+        int sz = a.size();
+        for (int k=0; k<sz; k++) {
+            for (int i=n-1; i>=0; i--) {
+                int ni = i + a[k];
+                if (ni > n) continue;
+                for (int j=m; j>=0; j--) {
+                    int nj = min(m,j+b[k]);
+                    dp[ni][nj] += dp[i][j];
+                    dp[ni][nj] %= mod; 
+                }
+            }
+        }
+        ll ans = 0;
+        for (int i=0; i<=n; i++) ans += dp[i][m];
+        return ans % mod;
+    }
+};
