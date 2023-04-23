ee3c896aeb1a71673086f4eac9ce2475247e6c28
Martin Yang <martin0327@gmail.com>
2023-04-23 09:56:44 +0900

    Time: 71 ms (60.46%), Space: 14.9 MB (60.46%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -0,0 +1,30 @@
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll mod = 1e9+7;
+
+class Solution {
+public:
+    int numberOfArrays(string s, int k) {
+        int n = s.size();
+        s = " " + s;
+        vi dp(n+1);
+        dp[0] = 1;
+        for (int i=1; i<=n; i++) {
+            ll cur = 0;
+            ll prod = 1;
+            for (int j=0; j<10; j++) {
+                if (i-j < 1) break;
+                ll x = s[i-j]-'0';
+                cur += prod*x;
+                prod *= 10;
+                if (cur <= k && x > 0) {
+                    dp[i] += dp[i-j-1];
+                    dp[i] %= mod;
+                }
+                else if (cur > k) break;
+            }
+        }
+        return dp[n];
+    }
+};
