77a879e6dba67b0facd2ac69e4a43d5ff518f5ab
Martin Yang <martin0327@gmail.com>
2023-05-09 09:17:33 +0900

    Time: 0 ms (100.00%), Space: 6.8 MB (91.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -4,21 +4,18 @@ const int inf = 1e9;
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& a) {
-        int n = a.size(), m = a[0].size();
         vector<int> ret;
-        int r=0, c=0, d=0;
+        int n = a.size(), m = a[0].size(), r=0, c=0, d=0;
         while (ret.size() < n*m) {
             ret.push_back(a[r][c]);
             a[r][c] = inf;
             int nr = r + dr[d];
             int nc = c + dc[d];
-            if (nr < 0 || nr >=n || nc < 0 || nc >= m || a[nr][nc] == inf) {
+            if (nr*nc < 0 || nr >=n || nc >= m || a[nr][nc] == inf) {
                 d = (d+1)%4;
                 r = r + dr[d], c = c + dc[d];
             }
-            else {
-                r = nr, c = nc;
-            }
+            else r = nr, c = nc;
         }
         return ret;
     }
