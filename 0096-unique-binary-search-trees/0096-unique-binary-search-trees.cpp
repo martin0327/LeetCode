426f19f37a872a07563ebed13258e4a01b31a782
Martin Yang <martin0327@gmail.com>
2023-05-10 13:14:19 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (13.88%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,21 +1,16 @@
 using ll = long long;
 using vi = vector<ll>;
-using vvi = vector<vi>;
 
 class Solution {
 public:
     int numTrees(int n) {
-        vi a(n+1);
-        // vvi b(n+1);
-        // for (int i=1; i<=n; i++) {
-        //     b[i] = vi(i);
-        // }
-        a[0] = a[1] = 1;
+        vi dp(n+1);
+        dp[0] = dp[1] = 1;
         for (int i=2; i<=n; i++) {
             for (int j=0; j<i; j++) {
-                a[i] += a[j] * a[i-1-j]; 
+                dp[i] += dp[j] * dp[i-1-j]; 
             }
         }
-        return a[n];
+        return dp[n];
     }
 };
