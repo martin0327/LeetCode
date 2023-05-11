b0b6d658b7155d3a184f44f9befb0c0d5fdb3184
Martin Yang <martin0327@gmail.com>
2023-05-11 09:53:11 +0900

    Time: 24 ms (49.12%), Space: 15.2 MB (6.70%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -8,8 +8,7 @@ public:
         vvi dp(n+1, vi(m+1));
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=m; j++) {
-                dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
-                dp[i][j] = max(dp[i][j],dp[i-1][j-1]+(a[i-1]==b[j-1]));
+                dp[i][j] = max({dp[i][j-1],dp[i-1][j],dp[i][j],dp[i-1][j-1]+(a[i-1]==b[j-1])});
             }
         }
         return dp[n][m];
