cdc5fd93aaeae5a7fadeecdeffc3785861b598e3
Martin Yang <martin0327@gmail.com>
2023-05-12 10:46:02 +0900

    Time: 412 ms (14.38%), Space: 113.6 MB (96.18%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,13 +1,13 @@
 using ll = long long;
-using vi = vector<ll>;
 void chmax(ll &x, ll y) {x = max(x,y);}
+const int sz = 2e5;
 
 class Solution {
 public:
+    ll dp[sz+1];
     long long mostPoints(vector<vector<int>>& a) {
         int n = a.size();
         ll ans = 0;
-        vi dp(n);
         for (int i=0; i<n; i++) {
             int x = a[i][0], y = a[i][1];
             if (i+y+1 < n) chmax(dp[i+y+1],dp[i]+x);
