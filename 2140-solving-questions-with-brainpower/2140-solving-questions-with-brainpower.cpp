a943bed0138e0a9f5aad41c4ffefda044c512c08
Martin Yang <martin0327@gmail.com>
2023-05-12 10:44:40 +0900

    Time: 406 ms (17.98%), Space: 115.2 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -7,11 +7,11 @@ public:
     long long mostPoints(vector<vector<int>>& a) {
         int n = a.size();
         ll ans = 0;
-        vi dp(2e5+1);
+        vi dp(n);
         for (int i=0; i<n; i++) {
             int x = a[i][0], y = a[i][1];
-            chmax(dp[i+y+1],dp[i]+x);
-            chmax(dp[i+1],dp[i]);
+            if (i+y+1 < n) chmax(dp[i+y+1],dp[i]+x);
+            if (i+1 < n) chmax(dp[i+1],dp[i]);
             chmax(ans,dp[i]+x);
         }
         return ans;        
