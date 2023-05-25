6313ef9f6c369fde456bedfa003dcf445c194c1b
Martin Yang <martin0327@gmail.com>
2023-05-25 16:03:53 +0900

    Time: 16 ms (5.65%), Space: 12.2 MB (6.45%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.cpp
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -1,15 +1,14 @@
+using ld = long double;
 class Solution {
 public:
     double new21Game(int n, int k, int lim) {
-        using ld = long double;
         vector<ld> dp(n+2);
         dp[0] = 1;
         for (int i=0; i<=n+1; i++) {
             if (i>1) dp[i] += dp[i-1];
             if (i < k) {
                 dp[i+1] += dp[i] / lim;
-                int rlim = min(i+lim+1,n+1);
-                dp[rlim] -= dp[i] / lim;
+                dp[min(i+lim+1,n+1)] -= dp[i] / lim;
             }
         }
         ld ans = 0;
