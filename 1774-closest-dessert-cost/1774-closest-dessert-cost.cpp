7d8045e55783cc1d46ee0926483ef50d40ab1d06
Martin Yang <martin0327@gmail.com>
2023-05-30 21:28:22 +0900

    Time: 144 ms (20.07%), Space: 96.1 MB (6.06%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.cpp
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.cpp
@@ -1,42 +1,48 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+
 class Solution {
 public:
     int closestCost(vector<int>& a, vector<int>& b, int t) {
         int n = a.size(), m = b.size();
         for (int i=0; i<m; i++) b.push_back(b[i]);
         m = b.size();
-        set<int> c;
-        for (int mask=0; mask<(1<<m); mask++) {
-            int s = 0;
-            for (int i=0; i<m; i++) {
-                if (mask>>i&1) s += b[i];
+        int mx = 2e4;
+        vvi dp(m+1, vi(mx+1)); // 20 *2e4 = 4e5
+        dp[0][0] = 1; // when I have used 0 topping, cost 0 is the only achievable cost
+    
+        for (int i=0; i<m; i++) {
+            dp[i+1] = dp[i];
+            for (int j=0; j<mx; j++) {
+                if (dp[i][j]==0) continue;
+                if (j+b[i] <= mx) dp[i+1][j+b[i]] = 1;
             }
-            c.insert(s);
+            // print(dp[i]);
         }
-        int d = 1e9;
-        int ans = 1e9;
+        const int inf = 1e9;
+        int d = inf, ans = inf;
         for (auto x : a) {
-            auto it = c.lower_bound(t-x);
-            if (it != c.end()) {
-                int y = x + *it;
-                if (abs(y-t) < d) {
-                    d = abs(y-t);
-                    ans = y; 
-                }
-                else if (abs(y-t) == d) {
-                    ans = min(ans, y);
-                }
-            }
-            if (it != c.begin()) {
-                int y = x + *prev(it);
-                if (abs(y-t) < d) {
-                    d = abs(y-t);
-                    ans = y; 
-                }
-                else if (abs(y-t) == d) {
-                    ans = min(ans, y);
+            for (int j=0; j<=mx; j++) {
+                if (dp[m][j]) {
+                    int c = x + j;
+                    if (abs(c-t) < d) {
+                        d = abs(c-t);
+                        ans = c;
+                    }
+                    else if (abs(c-t) == d) {
+                        ans = min(ans, c);
+                    }
                 }
             }
         }
         return ans;
     }
 };
+
+
+
+
+
+
+
+
