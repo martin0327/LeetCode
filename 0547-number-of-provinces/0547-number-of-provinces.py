c369111d6cf5f02cc6213b9c37692fb2376226a7
Martin Yang <martin0327@gmail.com>
2023-06-04 09:31:54 +0900

    Time: 220 ms (16.97%), Space: 16.9 MB (24.93%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,49 @@
+class dsu:
+    def __init__(self, n):
+        self._n = n
+        self.parent_or_size = [-1] * n
+
+    def merge(self, a, b):
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+        x, y = self.leader(a), self.leader(b)
+        if x == y: return x
+        if -self.parent_or_size[x] < -self.parent_or_size[y]: x, y = y, x
+        self.parent_or_size[x] += self.parent_or_size[y]
+        self.parent_or_size[y] = x
+        return x
+
+    def same(self, a, b):
+        assert 0 <= a < self._n
+        assert 0 <= b < self._n
+        return self.leader(a) == self.leader(b)
+
+    def leader(self, a):
+        assert 0 <= a < self._n
+        if self.parent_or_size[a] < 0: return a
+        self.parent_or_size[a] = self.leader(self.parent_or_size[a])
+        return self.parent_or_size[a]
+
+    def size(self, a):
+        assert 0 <= a < self._n
+        return -self.parent_or_size[self.leader(a)]
+
+    def groups(self):
+        leader_buf = [self.leader(i) for i in range(self._n)]
+        result = [[] for _ in range(self._n)]
+        for i in range(self._n): result[leader_buf[i]].append(i)
+        return [r for r in result if r != []]
+
+class Solution:
+    def findCircleNum(self, adj: List[List[int]]) -> int:
+        n = len(adj)
+        d = dsu(n)
+        for i in range(n):
+            for j in range(n):
+                if adj[i][j]:
+                    d.merge(i,j)
+        return len(d.groups())
+        
+        
+        
+        
