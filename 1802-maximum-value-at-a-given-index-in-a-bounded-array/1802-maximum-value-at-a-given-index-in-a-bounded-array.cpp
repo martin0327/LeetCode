f79e9b49c8a0fbe6bb93e1a9c15e1ac1a220613a
Martin Yang <martin0327@gmail.com>
2023-06-10 10:31:54 +0900

    Time: 0 ms (100.00%), Space: 5.8 MB (92.49%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -0,0 +1,22 @@
+using ll = long long;
+
+class Solution {
+public:
+    int maxValue(int n, int idx, int mx) {
+        ll lo = 1, hi = 1e9, ans = -1;
+        auto f = [&](ll n, ll x) {
+            if (n >= x) return x*(x+1)/2 + (n-x);
+            else return x*(x+1)/2 - (x-n)*(x-n+1)/2;
+        };
+        while (lo <= hi) {
+            ll mid = (lo+hi)/2;
+            ll s = mid + f(idx,mid-1) + f(n-1-idx,mid-1);            
+            if (s <= mx) {
+                ans = mid;
+                lo = mid + 1;
+            }
+            else hi = mid - 1;
+        }
+        return ans;
+    }
+};
