161a188d8ac3febf3e975b8fc12d5168688c039c
Martin Yang <martin0327@gmail.com>
2023-06-17 10:24:48 +0900

    Time: 294 ms (58.14%), Space: 108.6 MB (5.81%) - LeetHub

---
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -0,0 +1,38 @@
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+void chmin(ll &x, ll y) {x = min(x,y);}
+
+class Solution {
+public:
+    int makeArrayIncreasing(vector<int>& a, vector<int>& b) {
+        ll n = a.size();
+
+        map<ll,ll> mp;
+        for (auto x : a) mp[x];
+        for (auto x : b) mp[x];
+        ll sz = 1;
+        for (auto &[k,v] : mp) v = sz++;
+        for (auto &x : a) x = mp[x];
+        for (auto &x : b) x = mp[x];
+        sort(b.begin(), b.end());
+        vi c(sz);
+        for (auto x : b) c[x] = 1;
+
+        const ll inf = 1e9;
+        vvi dp(n+1, vi(sz,inf));
+        dp[0][0] = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<sz; j++) {
+                if (dp[i][j] >= inf) continue;
+                if (a[i] > j) chmin(dp[i+1][a[i]], dp[i][j]);
+                auto it = upper_bound(b.begin(), b.end(), j);
+                if (it != b.end()) chmin(dp[i+1][*it], dp[i][j] + 1);
+            }
+        }
+        ll ans = *min_element(dp[n].begin(), dp[n].end());
+        if (ans == inf) ans = -1;
+        return ans;
+    }
+};
