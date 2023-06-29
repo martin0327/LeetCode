8e39a8d564ac9af2da06d56142564b4c68b31c1f
Martin Yang <martin0327@gmail.com>
2023-06-29 10:45:10 +0900

    Time: 100 ms (53.92%), Space: 22 MB (55.97%) - LeetHub

---
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.cpp
@@ -0,0 +1,61 @@
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+const ll inf = 1e9;
+vi dr = {0,0,-1,1};
+vi dc = {-1,1,0,0};
+
+class Solution {
+public:
+    int shortestPathAllKeys(vector<string>& a) {
+        int k = 0, sr, sc, n = a.size(), m = a[0].size();
+        map<char,int> mp;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                char c = a[i][j];
+                if ('a' <= c && c <= 'z') mp[c] = k++;
+                if (c == '@') {
+                    sr = i;
+                    sc = j;
+                }
+            }
+        }
+        queue<tuple<ll,ll,ll>> q;
+        vector dist(n, vvi(m, vi(1<<k,inf)));
+        dist[sr][sc][0] = 0;
+        q.push({sr,sc,0});
+        while (q.size()) {
+            auto [r,c,mask] = q.front();
+            q.pop();
+            for (int i=0; i<4; i++) {
+                ll nr = r + dr[i];
+                ll nc = c + dc[i];
+                if (nr < 0 || nr >= n) continue;
+                if (nc < 0 || nc >= m) continue;
+                if (a[nr][nc] == '#') continue;
+                char x = a[nr][nc];
+                if ('A'<=x && x<='Z') {
+                    if (!((mask>>(mp[x-'A'+'a']))&1)) continue;
+                }
+                ll nmask = mask;
+                
+                if ('a' <= x && x <= 'z') {
+                    nmask |= (1<<(mp[x]));
+                }
+                
+                if (dist[nr][nc][nmask] > dist[r][c][mask] + 1) {
+                    dist[nr][nc][nmask] = dist[r][c][mask] + 1;
+                    q.emplace(nr,nc,nmask);
+                }
+            }
+        }
+        ll ans = inf;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                ans = min(ans, dist[i][j][(1<<k)-1]);
+            }
+        }
+        if (ans==inf) ans = -1;
+        return ans;
+    }
+};
