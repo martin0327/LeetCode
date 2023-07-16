58956c1e7568671e8f47c806106650e311ef5efb
Martin Yang <martin0327@gmail.com>
2023-07-16 11:05:08 +0900

    Time: 165 ms (49.68%), Space: 10.7 MB (74.84%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
@@ -1,40 +1,37 @@
-using ll = int;
-using ti3 = tuple<ll,ll,ll>;
-using vi = vector<ll>;
-const ll inf = 1e9;
+using vi = vector<int>;
+using ti3 = tuple<int,int,int>;
+const int inf = 1e9;
 
 class Solution {
 public:
-    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
-        map<string,ll> mp;
-        ll m = 0;
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {    
+        int m = 0;
+        map<string,int> mp;
         for (auto s : req_skills) {
-            if (!mp.count(s)) mp[s] = m++;
+            mp[s] = m++;
         }
-        ll n = people.size();
+        int n = people.size();
         vi a(n);
         for (int i=0; i<n; i++) {
             for (auto s : people[i]) {
-                a[i] |= (1ll<<mp[s]);
+                a[i] |= (1<<mp[s]);
             }
         }
         vector<ti3> dp(1<<m, {inf,-1,-1});
         dp[0] = {0,-1,-1};
         for (int i=0; i<n; i++) {
-            ll x = a[i];
-            for (ll mask=(1ll<<m)-1; mask>=0; mask--) {
-                auto [y,pidx,pmask] = dp[mask];
+            for (int mask=(1<<m)-1; mask>=0; mask--) {
+                auto [y,pi,pm] = dp[mask];
                 if (y==inf) continue;
-                ll nmask = mask | x;
-                auto [z,nidx,_] = dp[nmask];
+                int nmask = mask | a[i];
+                auto [z,ni,nm] = dp[nmask];
                 if (y+1 < z) {
                     dp[nmask] = {y+1,i,mask};
                 }
             }
         }
-        auto [val,idx,mask] = dp[(1ll<<m)-1];
-        vector<int> ans;
-        ans.push_back(idx);
+        auto [val,i,mask] = dp[(1<<m)-1];
+        vi ans = {i};
         while (mask > 0) {
             auto [v,i,nmask] = dp[mask];
             ans.push_back(i);
