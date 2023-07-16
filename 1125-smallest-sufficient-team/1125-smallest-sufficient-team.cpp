0b99c33058e87bd03a9ed53243f8902d5081767b
Martin Yang <martin0327@gmail.com>
2023-07-16 10:50:28 +0900

    Time: 147 ms (51.61%), Space: 12.5 MB (74.52%) - LeetHub

---
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
@@ -0,0 +1,48 @@
+using ll = long long;
+using ti3 = tuple<ll,ll,ll>;
+using vi = vector<ll>;
+const ll inf = 1e9;
+
+class Solution {
+public:
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+        map<string,ll> mp;
+        ll m = 0;
+        for (auto s : req_skills) {
+            if (!mp.count(s)) mp[s] = m++;
+        }
+        ll n = people.size();
+        vi a(n);
+        for (int i=0; i<n; i++) {
+            for (auto s : people[i]) {
+                a[i] |= (1ll<<mp[s]);
+            }
+        }
+        vector<ti3> dp(1<<m, {inf,-1,-1});
+        dp[0] = {0,-1,-1};
+        for (int i=0; i<n; i++) {
+            ll x = a[i];
+            for (ll mask=(1ll<<m)-1; mask>=0; mask--) {
+                auto [y,pidx,pmask] = dp[mask];
+                if (y==inf) continue;
+                ll nmask = mask | x;
+                auto [z,nidx,_] = dp[nmask];
+                if (y+1 < z) {
+                    dp[nmask] = {y+1,i,mask};
+                }
+            }
+        }
+        auto [val,idx,mask] = dp[(1ll<<m)-1];
+        vector<int> ans;
+        ans.push_back(idx);
+        while (mask > 0) {
+            auto [v,i,nmask] = dp[mask];
+            ans.push_back(i);
+            mask = nmask;
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+        
+   
+    }
+};
