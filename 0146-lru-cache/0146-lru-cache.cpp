b3e584716a8f41ed265ad98ff9340d692f220e1b
Martin Yang <martin0327@gmail.com>
2023-07-18 13:03:32 +0900

    Time: 435 ms (98.76%), Space: 182.1 MB (8.00%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,28 +1,29 @@
-const int mx = 2e5;
 using vi = vector<int>;
 class LRUCache {
-    public:
-    int i=0, j=0, sz=0, cap;
+public:
+    int i=0, j=0, cap;
     vi r2k, k2r, k2v;
     LRUCache(int capacity) {
         cap = capacity;
-        r2k = k2r = k2v = vi(mx+1,-1);
+        r2k = vi(2e5+1,-1);
+        k2r = k2v = vi(1e4+1,-1);
     }
     
     int get(int key) {
-        if (k2v[key] == -1) return -1;
-        r2k[j] = key;
-        k2r[key] = j++;
+        if (k2v[key] != -1) {
+            r2k[j] = key;
+            k2r[key] = j++;
+        }
         return k2v[key];
     }
     
     void put(int key, int value) {
         if (k2v[key] == -1) {
-            if (sz==cap) {
+            if (cap) cap--;
+            else {
                 while (k2r[r2k[i]] != i) i++;
                 k2v[r2k[i++]] = -1; 
             }
-            else sz++;
         }
         k2v[key] = value;
         r2k[j] = key;
