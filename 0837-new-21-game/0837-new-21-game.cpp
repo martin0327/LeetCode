7d69a09438af1185d775387a37195a5870e03a69
Martin Yang <martin0327@gmail.com>
2023-07-18 22:02:01 +0900

    Time: 9 ms (22.68%), Space: 15.2 MB (6.04%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.cpp
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -1,18 +1,22 @@
-using ld = long double;
 class Solution {
 public:
-    double new21Game(int n, int k, int lim) {
-        vector<ld> dp(n+2);
-        dp[0] = 1;
-        for (int i=0; i<=n; i++) {
-            if (i>1) dp[i] += dp[i-1];
-            if (i < k) {
-                dp[i+1] += dp[i] / lim;
-                dp[min(i+lim+1,n+1)] -= dp[i] / lim;
-            }
+    double new21Game(int n, int k, int m) {
+        using ld = long double;
+        int sz = max(n,k+m);
+
+        vector<ld> dp(sz+1);
+        for (int i=k; i<=sz; i++) {
+            if (i <= n) dp[i] = 1; // the probability that you get yes if you start at score i
         }
-        ld ans = 0;
-        for (int i=k; i<=n; i++) ans += dp[i];
-        return ans;
+        ld cur = 0;
+        for (int i=k; i<min(k+m,sz+1); i++) {
+            cur += dp[i];
+        }
+        for (int i=k-1; i>=0; i--) {
+            dp[i] = cur / (ld) m;
+            if (i+m <=sz ) cur -= dp[i+m];
+            cur += dp[i];
+        }
+        return dp[0];
     }
 };
