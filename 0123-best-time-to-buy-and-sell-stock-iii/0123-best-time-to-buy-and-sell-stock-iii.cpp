314ae7927cab7fba00d4f28363f74074751a2324
Martin Yang <martin0327@gmail.com>
2023-07-20 10:29:48 +0900

    Time: 179 ms (85.40%), Space: 81.5 MB (72.03%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,31 @@
+using vi = vector<int>;
+const int inf = 1e9;
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int n = a.size();
+        a.insert(a.begin(),0);
+        vi pre(n+1,-inf), suf(n+2,-inf);
+        {
+            int cur = inf;
+            for (int i=1; i<=n; i++) {
+                pre[i] = max(pre[i-1], a[i] - cur);
+                cur = min(cur, a[i]);
+            }
+        }
+        {
+            int cur = -inf;
+            for (int i=n; i>=1; i--) {
+                suf[i] = max(suf[i+1], cur - a[i]);
+                cur = max(cur, a[i]);
+            }
+        }
+        int ans = 0;
+        for (int i=1; i<n; i++) {
+            ans = max(ans, pre[i]+suf[i+1]);
+        }
+        ans = max(ans, pre[n]);
+        ans = max(ans, suf[1]);
+        return ans;
+    }
+};
