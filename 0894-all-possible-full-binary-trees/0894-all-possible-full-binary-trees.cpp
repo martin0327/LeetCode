253c783bbb9f1785310c72928b1dcfa5e5670327
Martin Yang <martin0327@gmail.com>
2023-07-23 15:57:01 +0900

    Time: 623 ms (5.05%), Space: 126.7 MB (6.04%) - LeetHub

---
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void debug(set<int> s) {
+    for (auto x : s) {
+        cout << x << " ";
+    }   cout << endl;
+}
+
+TreeNode* f(set<int> &s) {
+    map<int,TreeNode*> mp;
+    for (auto x : s) mp[x] = new TreeNode();
+    for (auto x : s) {
+        if (s.count(2*x)) {
+            mp[x]->left = mp[2*x];
+            mp[x]->right = mp[2*x+1];
+        }
+    }
+    return mp[1];
+}
+
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        if (!(n&1)) return {};        
+        set<set<int>> ss = {{1}};
+        for (int k=0; k<n/2; k++) {
+            set<set<int>> ns;
+            for (auto s : ss) {
+                for (auto x : s) {
+                    if (!s.count(2*x)) {
+                        set<int> t = s;
+                        t.insert(2*x);
+                        t.insert(2*x+1);
+                        if (!ns.count(t)) ns.insert(t);
+                    }
+                }
+            }
+            ss = ns;
+        }        
+        vector<TreeNode*> ret;
+        for (auto s : ss) {
+            ret.push_back(f(s));
+        }
+        return ret;
+    }
+};
