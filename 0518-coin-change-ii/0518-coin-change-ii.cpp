93494737eb935e77e4bc71fffabfa9a6dc201a88
Martin Yang <martin0327@gmail.com>
2023-08-12 14:03:32 +0900

    Time: 13 ms (76.71%), Space: 7.1 MB (90.21%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,14 @@
-using ll = long long;
-using vi = vector<int>;
 class Solution {
 public:
     int change(int k, vector<int>& c) {
-        ll n = c.size();
-        vector dp(n+1, vi(k+1));
-        dp[0][0] = 1;
+        int n = c.size();
+        vector<int> dp(k+1);
+        dp[0] = 1;
         for (int i=0; i<n; i++) {
-            dp[i+1] = dp[i];
             for (int j=0; j+c[i]<=k; j++) {
-                dp[i+1][j+c[i]] += dp[i+1][j];
+                dp[j+c[i]] += dp[j];
             }
         }
-        return dp[n][k];        
+        return dp[k];        
     }
 };
