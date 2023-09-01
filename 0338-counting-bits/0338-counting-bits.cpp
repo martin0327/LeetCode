f11cb8d664405eb7e3741ea069c1b5607282a86d
Martin Yang <martin0327@gmail.com>
2023-09-01 23:59:15 +0900

    Time: 3 ms (89.82%), Space: 7.9 MB (51.17%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        for (int i=1; i<=n; i++) ans[i] = ans[i - (i & -i)] + 1;
+        return ans;
+    }
+};
