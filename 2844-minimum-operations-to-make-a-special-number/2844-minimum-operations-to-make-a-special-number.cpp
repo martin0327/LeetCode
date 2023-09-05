fd3a513446a9c893c8d3ad5ca68ce67af307a717
Martin Yang <martin0327@gmail.com>
2023-09-05 20:49:56 +0900

    Time: 19 ms (8.01%), Space: 11 MB (5.05%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
@@ -1,14 +1,20 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+const int inf = 1e9;
 class Solution {
 public:
     int minimumOperations(string s) {
-        int n = s.size();
-        vector<bool> a(10);
-        for (int i=n-1; i>=0; i--) {
+        int n = s.size(), m = 25;
+        vvi dp(n+1, vi(m,inf));
+        dp[0][0] = 0;
+        for (int i=0; i<n; i++) {
             int x = s[i] - '0';
-            if ((x==7 || x==2) && a[5]) return n-i-2;
-            if ((x==5 || x==0) && a[0]) return n-i-2;
-            a[x] = true;
+            for (int j=0; j<m; j++) {
+                dp[i+1][j] = min(dp[i+1][j], dp[i][j]+1);
+                int nj = ((10*j)+x)%m;
+                dp[i+1][nj] = min(dp[i+1][nj], dp[i][j]);
+            }
         }
-        return n-a[0];        
+        return dp[n][0];         
     }
 };
