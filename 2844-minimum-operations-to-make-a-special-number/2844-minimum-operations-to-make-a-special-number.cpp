c240081f698c6444036eb00427196371d85e3e9d
Martin Yang <martin0327@gmail.com>
2023-09-05 20:28:12 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (28.29%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
@@ -2,23 +2,13 @@ class Solution {
 public:
     int minimumOperations(string s) {
         int n = s.size();
-        set<string> ss = {"00"};
-        for (int i=25; i<=75; i+=25) ss.insert(to_string(i));
-        
-        bool zero = false;
+        vector<bool> a(10);
         for (int i=n-1; i>=0; i--) {
-            if (s[i]=='0') zero = true;
-            for (int j=n-1; j>i; j--) {
-                string t;
-                t += s[i];
-                t += s[j];
-                if (ss.count(t)) {
-                    return (n-i-2);
-                }
-            }
+            int x = s[i] - '0';
+            if ((x==7 || x==2) && a[5]) return n-i-2;
+            if ((x==5 || x==0) && a[0]) return n-i-2;
+            a[x] = true;
         }
-        if (zero) return n-1;
-        return n;
-        
+        return n-a[0];        
     }
 };
