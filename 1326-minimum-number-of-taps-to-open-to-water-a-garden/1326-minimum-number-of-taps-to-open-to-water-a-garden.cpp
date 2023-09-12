d4d84ab1450f8f64fe013a6c6de30248383d3ad4
Martin Yang <martin0327@gmail.com>
2023-09-12 13:50:03 +0900

    Time: 48 ms (16.27%), Space: 20 MB (8.35%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,16 +1,15 @@
-using ll = long long;
-using vi = vector<ll>;
+using vi = vector<int>;
 using vvi = vector<vi>;
-const ll inf = 1e18;
+const int inf = 1e9;
 
 class Solution {
 public:
     int minTaps(int n, vector<int>& rg) {
         vvi a(n+1);
         for (int i=0; i<=n; i++) {
-            ll l = i - rg[i], r = i + rg[i];
-            l = max(l,0ll);
-            r = min(r,(ll)n);
+            int l = i - rg[i], r = i + rg[i];
+            l = max(l,0);
+            r = min(r,n);
             a[r].push_back(l);
         }
         vi dp(n+1, inf);
@@ -22,7 +21,7 @@ public:
                 }
             }
         }
-        ll ans = dp[n];
+        int ans = dp[n];
         if (ans==inf) ans = -1;
         return ans;
     }
