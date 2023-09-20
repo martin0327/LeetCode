517be3c28d9bc5df26bb93d83fec6f8083b4b460
Martin Yang <martin0327@gmail.com>
2023-09-20 10:55:01 +0900

    Time: 13 ms (24.76%), Space: 23.5 MB (25.80%) - LeetHub

--- 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.cpp
+++ 2855-minimum-right-shifts-to-sort-the-array/2855-minimum-right-shifts-to-sort-the-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int minimumRightShifts(vector<int>& a) {
+        auto b = a;
+        sort(b.begin(), b.end());
         for (int i=0; i<a.size(); i++) {
-            auto b = a;
-            sort(b.begin(), b.end());
             if (a==b) return i;
             a.insert(a.begin(), a.back());
             a.pop_back();
