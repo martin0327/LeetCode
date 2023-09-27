a1fd6bd22cb37c194fa77ae2d1c2989142cbdb5b
Martin Yang <martin0327@gmail.com>
2023-09-28 08:50:36 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (14.13%) - LeetHub

--- 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    string decodeAtIndex(string S, int K) {
-        int n = S.length();
-        long long size = 0;
-        
-        for(char ch:S) { // finding size of input string
-            if(isdigit(ch)) {//isdigit is function in c/c++, which used to check if the passed character is a digit or not.
-                size = size*(ch-'0');
-            } else {
-                size++;// if it is not digit then simplly size will be inc. by 1
+    string decodeAtIndex(string s, int k) {
+        using ll = long long;
+        ll sz = 0;
+        for (auto c : s) {
+            if (isdigit(c)) sz *= c - '0';
+            else sz++;
+        }
+        reverse(s.begin(), s.end());
+        for (auto c : s) {
+            if (sz==k) {
+                if (isdigit(c)) {
+                    int x = c - '0';
+                    sz /= x;
+                    k /= x;
+                }
+                else {
+                    return string(1,c);
+                }
+            }
+            else {
+                if (isdigit(c)) {
+                    int x = c - '0';
+                    sz /= x;
+                    if (k > sz) k %= sz;
+                    if (k==0) k += sz;
+                }
+                else {
+                    sz--;
+                }
             }
         }
-        
-        for(int i = n-1; i>=0; i--) { // we will traverse from last that will be help to decode effectively
-            K = K%size;
-            if(K == 0  && isalpha(S[i]))// isalpha is used to check if the passed character is an alphabet or not.
-                return string(1, S[i]);
-            
-            if(isalpha(S[i]))
-                size--;
-            else
-                size = size/(S[i]-'0');
-        }
-        
         return "";
+        
     }
 };
