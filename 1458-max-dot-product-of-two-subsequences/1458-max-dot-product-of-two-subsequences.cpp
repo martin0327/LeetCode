4afbca3b2fb43e5b964dc66d044009ca9bb03858
Martin Yang <martin0327@gmail.com>
2023-10-09 08:56:20 +0900

    Time: 41 ms (41.19%), Space: 13.4 MB (39.90%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -5,19 +5,18 @@ class Solution {
 public:
     int maxDotProduct(vector<int>& a, vector<int>& b) {
         int n = a.size(), m = b.size();
-        vvi dp(n, vi(m,-inf));
-        dp[0][0] = a[0]*b[0];
-        for (int i=1; i<n; i++) dp[i][0] = max(dp[i-1][0],a[i]*b[0]);
-        for (int j=1; j<m; j++) dp[0][j] = max(dp[0][j-1],a[0]*b[j]);
-        for (int i=1; i<n; i++) {
-            for (int j=1; j<m; j++) {
-                dp[i][j] = max(dp[i][j], dp[i-1][j-1]);
-                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + a[i]*b[j]);
-                dp[i][j] = max(dp[i][j], dp[i-1][j]);
-                dp[i][j] = max(dp[i][j], dp[i][j-1]);
-                dp[i][j] = max(dp[i][j], a[i]*b[j]);
+        vvi dp(n+1, vi(m+1,-inf));
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                dp[i][j] = max({
+                    a[i-1]*b[j-1],
+                    dp[i-1][j-1] + a[i-1]*b[j-1],
+                    dp[i-1][j-1],
+                    dp[i-1][j],
+                    dp[i][j-1]
+                });
             }
         }
-        return dp[n-1][m-1];        
+        return dp[n][m];        
     }
 };
