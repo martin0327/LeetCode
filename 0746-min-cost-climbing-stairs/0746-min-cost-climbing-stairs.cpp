d512b16339cc1fefd36733604fb36973c2a85edd
Martin Yang <martin0327@gmail.com>
2023-10-13 09:23:19 +0900

    Time: 3 ms (84.96%), Space: 14.1 MB (16.21%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& a) {
+        int n = a.size();
+        a.push_back(0);
+        vector<int> dp(n+1);
+        dp[0] = a[0];
+        dp[1] = a[1];
+        for (int i=2; i<=n; i++) {
+            dp[i] = min(dp[i-1],dp[i-2]) + a[i];
+        }
+        return dp[n];        
+    }
+};
