174c08181f43a8af2e7c3d6d3fe3ff6cff0087f9
Martin Yang <martin0327@gmail.com>
2023-10-17 22:01:38 +0900

    Time: 209 ms (84.04%), Space: 134.8 MB (76.78%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -7,34 +7,24 @@ class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& a) {
         int n = a.size(), m = a[0].size();
-        vvi pre(n+2, vi(m+2,1)), suf(n+2, vi(m+2,1));
-
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=m; j++) {
-                pre[i][j] = pre[i][j-1] * a[i-1][j-1];
-                pre[i][j] %= mod;
-            }
-            for (int j=m; j>=1; j--) {
-                suf[i][j] = suf[i][j+1] * a[i-1][j-1]; 
-                suf[i][j] %= mod;
-            }
+        int k = n*m;
+        
+        vi pre(k+2,1), suf(k+2,1);
+        for (int i=1; i<=k; i++) {
+            int r = (i-1) / m, c = (i-1) % m;
+            pre[i] = pre[i-1] * a[r][c];
+            pre[i] %= mod;
         }
-        vi pre2(n+2,1), suf2(n+2,1);
-        for (int i=1; i<=n; i++) {
-            pre2[i] = pre2[i-1] * pre[i][m];
-            pre2[i] %= mod;
+        for (int i=k; i>=1; i--) {
+            int r = (i-1) / m, c = (i-1) % m;
+            suf[i] = suf[i+1] * a[r][c];
+            suf[i] %= mod;
         }
-        for (int i=n; i>=1; i--) {
-            suf2[i] = suf2[i+1] * pre[i][m];
-            suf2[i] %= mod;
-        }        
-
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=m; j++) {
-                ll x = pre2[i-1] * suf2[i+1];
-                x *= pre[i][j-1] * suf[i][j+1];
-                x %= mod;
-                a[i-1][j-1] = x;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                int l = i * m + j + 1;
+                a[i][j] = pre[l-1] * suf[l+1];
+                a[i][j] %= mod;
             }
         }
         return a;
