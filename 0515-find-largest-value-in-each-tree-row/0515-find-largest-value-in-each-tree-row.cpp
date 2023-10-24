77c212c0cf58e6808ced74f940dbc85d29c36946
Martin Yang <martin0327@gmail.com>
2023-10-24 13:28:28 +0900

    Time: 4 ms (91.88%), Space: 22.5 MB (50.91%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -15,7 +15,7 @@ public:
         vector<int> ret;
         function<void(TreeNode*,int)> f = [&] (TreeNode* u, int d) {
             if (!u) return;
-            if (ret.size()==d) ret.push_back(u->val);
+            if (ret.size()==d) ret.emplace_back(u->val);
             else ret[d] = max(ret[d], u->val);
             f(u->left, d+1); f(u->right, d+1);
         };
