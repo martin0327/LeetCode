5509f7fff59e012d3fad92a73a94102bfa012520
Martin Yang <martin0327@gmail.com>
2023-10-25 10:51:24 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (28.86%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -3,7 +3,7 @@ public:
     int kthGrammar(int n, int k) {
         function<int(int,int)> f = [&] (int n, int k) {
             if (n==1) return 0;
-            if (k&1) return 1^f(n-1,k/2);
+            if (k&1) return f(n-1,k/2)^1;
             else return f(n-1,k/2);
         };
         return f(n,k-1);
