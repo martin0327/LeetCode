d023ac68d585193a712ead4181317393a1febfac
Martin Yang <martin0327@gmail.com>
2023-10-28 20:10:51 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (89.44%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -8,17 +8,8 @@ public:
         a=e=i=o=u=1;
         while (--n) {
             int aa,ee,ii,oo,uu;
-            aa = e;
-            ee = (a+i)%mod;
-            ii = (a+e+o+u)%mod;
-            oo = (i+u)%mod;
-            uu = a;
-            
-            a = aa;
-            e = ee;
-            i = ii;
-            o = oo;
-            u = uu;
+            aa = e, ee = (a+i)%mod, ii = (a+e+o+u)%mod, oo = (i+u)%mod, uu = a;
+            a = aa, e = ee, i = ii, o = oo, u = uu;
         }        
         ll ans = (a+e+i+o+u)%mod;
         return ans;
