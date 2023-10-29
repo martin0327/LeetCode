0317fb8ab3a06f57bf7f276ef550051a08c051e2
Martin Yang <martin0327@gmail.com>
2023-10-29 09:59:45 +0900

    Time: 0 ms (100.00%), Space: 6.7 MB (8.64%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int num_test = minutesToTest / minutesToDie;
+        int ret = 0;
+        while (pow(num_test+1,ret) < buckets) {
+            ret++;
+        }
+        return ret;
+    }
+};
