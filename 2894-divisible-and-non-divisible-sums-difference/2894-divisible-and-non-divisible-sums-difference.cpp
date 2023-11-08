1d7c9a71b829043a3c01bbaaee7ae262588fcdf6
Martin Yang <martin0327@gmail.com>
2023-11-08 15:31:00 +0900

    Time: 4 ms (31.18%), Space: 6.5 MB (18.12%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int x = 0, y = 0;
-        for (int i=1; i<=n; i++) {
-            if (i % m == 0) y += i;
-            else x += i;
-        }
-        return x-y;
+        int ans = 0;
+        for (int i=1; i<=n; i++) ans += (i%m) ? i : -i;
+        return ans;
     }
 };
