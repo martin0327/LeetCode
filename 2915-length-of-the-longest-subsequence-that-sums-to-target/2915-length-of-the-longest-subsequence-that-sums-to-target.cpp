22d19dc20ab2ff401131b5092271b6bff71009af
Martin Yang <martin0327@gmail.com>
2023-11-08 10:54:42 +0900

    Time: 181 ms (87.06%), Space: 28.9 MB (97.94%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> dp(t+1,-1);
         dp[0] = 0;
         for (auto x : a) {
-            for (int i=t; i>=0; i--) {
+            for (int i=t-x; i>=0; i--) {
                 if (dp[i] == -1) continue;
-                if (i+x <= t) dp[i+x] = max(dp[i+x], dp[i]+1);
+                dp[i+x] = max(dp[i+x], dp[i]+1);
             }
         }
         return dp[t];
