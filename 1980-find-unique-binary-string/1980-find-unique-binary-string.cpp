491dea313c22b4ab069255802791cab333d15e76
Martin Yang <martin0327@gmail.com>
2023-11-16 09:08:38 +0900

    Time: 6 ms (42.30%), Space: 10.4 MB (86.71%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     string findDifferentBinaryString(vector<string>& a) {
         int n = a.size();
-        set<string> s(a.begin(), a.end());
-        for (int mask=0; mask<(1<<n); mask++) {
-            string t;
-            for (int i=0; i<n; i++) {
-                if (mask>>i&1) t += '1';
-                else t += '0';
-            }
-            if (!s.count(t)) return t;
+        string ans;
+        for (int i=0; i<n; i++) {
+            ans += (a[i][i] == '0') ? '1' : '0';
         }
-        return "";        
+        return ans;
     }
 };
