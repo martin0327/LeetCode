d1a10f0cc15fb137cb8ac6e5dbed40d89a40e6ac
Martin Yang <martin0327@gmail.com>
2023-11-24 10:41:13 +0900

    Time: 488 ms (9.09%), Space: 52 MB (90.91%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun maxCoins(a: IntArray): Int {
+        return a.sortedArrayDescending()
+        .take(2*a.size/3)
+        .filterIndexed { idx, value -> (idx % 2) == 1 }
+        .sum()
+    }
+}
