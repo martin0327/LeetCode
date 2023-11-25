2ca0cea297660a82ca3db48ec0d541c7c77cdcd5
Martin Yang <martin0327@gmail.com>
2023-11-25 09:58:23 +0900

    Time: 674 ms (100.00%), Space: 55.3 MB (50.00%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.kt
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun getSumAbsoluteDifferences(a: IntArray): IntArray {
-        val pre = a.scan(0) { s,x -> s+x }.toIntArray()
-        val suf = a.reversed().scan(0) { s,x -> s+x }.reversed().toIntArray()
+        val pre = a.scan(0) { s,x -> s+x }
+        val suf = a.reversed().scan(0) { s,x -> s+x }.reversed()
         return a.mapIndexed { i, x -> x*(2*i+1-a.size) - pre[i] + suf[i+1] }.toIntArray()
     }
 }
