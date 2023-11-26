280b5888b8ade2ff9ab77f4f4e68c93bad106734
Martin Yang <martin0327@gmail.com>
2023-11-26 11:10:30 +0900

    Time: 712 ms (25.00%), Space: 83.8 MB (25.00%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.kt
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.kt
@@ -4,10 +4,10 @@ class Solution {
         val m = a[0].size
         val b = Array(n) { IntArray(m) }
 
-        (0 until n).reversed().forEach { i ->
+        (0 until n).forEach { i ->
             (0 until m).forEach { j ->
-                b[i][j] = if (a[i][j]== 1) {
-                    if (i == n - 1) 1 else b[i + 1][j]+ 1
+                b[i][j] = if (a[i][j] == 1) {
+                    if (i == 0) 1 else b[i-1][j] + 1
                 } else 0
             }
         }
