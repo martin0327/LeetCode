31920dbb9848772c3634e7a964b086cf356a28ff
Martin Yang <martin0327@gmail.com>
2023-11-26 09:57:14 +0900

    Time: 705 ms (25.00%), Space: 84.4 MB (25.00%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.kt
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.kt
@@ -2,20 +2,21 @@ class Solution {
     fun largestSubmatrix(a: Array<IntArray>): Int {
         val n = a.size
         val m = a[0].size
-        var ans = 0
         val b = Array(n) { IntArray(m) }
 
-        for (j in 0 until m) {
-            var cnt = 0
-            for (i in n-1 downTo 0) {
-                if (a[i][j]== 1) b[i][j] = if (i==n-1) 1 else b[i+1][j]+1
-                else b[i][j] = 0
+        (0 until n).reversed().forEach { i ->
+            (0 until m).forEach { j ->
+                b[i][j] = if (a[i][j]== 1) {
+                    if (i == n - 1) 1 else b[i + 1][j]+ 1
+                } else 0
             }
         }
         
-        return (0 until n).map {i -> b[i].toMutableList()
+        return (0 until n).map {i -> 
+            b[i].toMutableList()
                 .sortedDescending()
                 .mapIndexed {j,x -> (j+1)*x}
-                .max()}.max()        
+                .max()
+        }.max()        
     }
 }
