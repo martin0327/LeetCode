6f446949c3a0cef511e0f6fa141e7d32bc7d41b6
Martin Yang <martin0327@gmail.com>
2023-11-27 15:05:37 +0900

    Time: 309 ms (83.33%), Space: 40.8 MB (66.67%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.kt
+++ 0935-knight-dialer/0935-knight-dialer.kt
@@ -7,20 +7,6 @@ class Solution {
     private val dr = listOf(1, 1, 2, 2)
     private val dc = listOf(2, -2, 1, -1)
     
-    // private fun matMul(a: vvi, b: vvi): vvi {
-    //     val n = a.size
-    //     val ret = MutableList(n) { MutableList(n) { 0L } }
-    //     for (i in 0 until n) {
-    //         for (j in 0 until n) {
-    //             for (k in 0 until n) {
-    //                 ret[i][j]+= a[i][k]* b[k][j]
-    //                 ret[i][j]%= mod.toLong()
-    //             }
-    //         }
-    //     }
-    //     return ret
-    // }
-    
     private fun matMul(a: List<List<Long>>, b: List<List<Long>>): List<List<Long>> {
         val n = a.size
         return (0 until n).map { i ->
@@ -32,28 +18,28 @@ class Solution {
         }
     }
 
-
-
-
-    private fun matExp(a: vvi, e: Int): vvi {
+    private fun matExp(a: List<List<Long>>, e: Int): List<List<Long>> {
         val n = a.size
-        val ret = MutableList(n) { MutableList(n) { 0L } }
-        for (i in 0 until n) {
-            ret[i][i]= 1L
-        }
-        var exp = e
-        var base = a
-        while (exp > 0) {
-            if (exp and 1 == 1) {
-                val temp = matMul(ret, base)
-                ret.clear()
-                ret.addAll(temp.map { it.toMutableList() })
+        val ret = (0 until n).map { i ->
+            (0 until n).map { j ->
+                if (i == j) 1L else 0L
             }
-            exp = exp shr 1
-            base = matMul(base, base)
+        }.toMutableList()
+
+        tailrec fun matExpHelper(exp: Int, base: List<List<Long>>, result: List<List<Long>>): List<List<Long>> {
+            return when {
+                exp == 0 -> result
+                exp and 1 == 1 -> {
+                    val temp = matMul(result, base)
+                    matExpHelper(exp shr 1, matMul(base, base), temp)
+                }
+                else -> matExpHelper(exp shr 1, matMul(base, base), result)
+            }
         }
-        return ret
+
+        return matExpHelper(e, a, ret)
     }
+
 
     fun knightDialer(n: Int): Int {
         val R = 4
