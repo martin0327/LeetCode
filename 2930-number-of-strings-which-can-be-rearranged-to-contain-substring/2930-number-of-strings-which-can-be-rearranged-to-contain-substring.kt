e1f2547812f99dcdb1ce6fe4f6e123282ff45f8b
Martin Yang <martin0327@gmail.com>
2023-11-28 15:19:20 +0900

    Time: 325 ms (44.44%), Space: 40.4 MB (55.56%) - LeetHub

---
+++ 2930-number-of-strings-which-can-be-rearranged-to-contain-substring/2930-number-of-strings-which-can-be-rearranged-to-contain-substring.kt
@@ -0,0 +1,74 @@
+typealias ll = Long
+typealias ti3 = Triple<Int,Int,Int>
+typealias vi = List<Long>
+typealias vvi = List<vi>
+const val mod = 1e9.toLong() + 7
+
+    
+class Solution {
+    private fun matMul(a: vvi, b: vvi): vvi {
+        val n = a.size
+        return (0 until n).map { i ->
+            (0 until n).map { j ->
+                (0 until n).sumOf { k ->
+                    a[i][k]*b[k][j]
+                }.rem(mod)
+            }
+        }
+    }
+
+    private fun matExp(a: vvi, e: Int): vvi {
+        val n = a.size
+        val ret = (0 until n).map { i ->
+            (0 until n).map { j ->
+                if (i == j) 1L else 0L
+            }
+        }.toList()
+
+        tailrec fun matExpHelper(exp: Int, base: vvi, res: vvi): vvi {
+            return when {
+                exp == 0 -> res
+                exp and 1 == 1 -> matExpHelper(exp shr 1, matMul(base, base), matMul(res, base))
+                else -> matExpHelper(exp shr 1, matMul(base, base), res)
+            }
+        }
+
+        return matExpHelper(e, a, ret)
+    }
+
+
+    fun stringCount(n: Int): Int {
+        val mp = mutableMapOf<ti3, Int>()
+        var m : Int = 0
+        (0 until 2).forEach { i ->
+            (0 until 2).forEach { j ->
+                (0 until 3).forEach { k ->
+                    mp[Triple(i,j,k)] = m
+                    m++
+                }
+            }
+        }
+        
+        val a = MutableList(m) { MutableList(m) { 0L } }
+        
+        (0 until 2).forEach { i ->
+            (0 until 2).forEach { j ->
+                (0 until 3).forEach { k ->
+                    val ni = minOf(1, i + 1)
+                    val nj = minOf(1, j + 1)
+                    val nk = minOf(2, k + 1)
+                    val u = mp[Triple(i, j, k)]!!
+                    val v1 = mp[Triple(ni, j, k)]!!
+                    val v2 = mp[Triple(i, nj, k)]!!
+                    val v3 = mp[Triple(i, j, nk)]!!
+                    a[u][u]+= 23L
+                    a[u][v1]+= 1L
+                    a[u][v2]+= 1L
+                    a[u][v3]+= 1L
+                }
+            }
+        }
+        
+        return matExp(a, n)[0][m-1].toInt()
+    }
+}
