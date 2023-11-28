75b03264de9207afe14356d8248b5b79ad8a4822
Martin Yang <martin0327@gmail.com>
2023-11-28 13:59:40 +0900

    Time: 1282 ms (5.00%), Space: 6.1 MB (100.00%) - LeetHub

---
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
@@ -0,0 +1,47 @@
+using ll = long long;
+using vi = vector<ll>;
+
+void debug(vector<ll> &a) {
+    for (auto x : a) {
+        cout << x << " ";
+    }   cout << endl;
+}
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        ll ans = 0, lim = limit;
+        for (int i=0; i<=lim; i++) {
+            ll m = n - i;
+            if (m < 0) continue;
+            ll lj = -1, rj = -1;
+            {
+                ll lo = 0, hi = min(m,lim);
+                while (lo <= hi) {
+                    ll mid = (lo+hi)/2;
+                    ll k = m - mid;
+                    if (k <= lim) {
+                        lj = mid;
+                        hi = mid - 1;
+                    } 
+                    else lo = mid + 1;
+                }
+            }
+            {
+                ll lo = 0, hi = min(m,lim);
+                while (lo <= hi) {
+                    ll mid = (lo+hi)/2;
+                    ll k = m - mid;
+                    if (0 <= k) {
+                        rj = mid;
+                        lo = mid + 1;
+                    }
+                    else hi = mid - 1;
+                }
+            }
+            if (lj != -1 && rj != -1) {
+                ans += max(0ll, rj-lj+1);
+            }
+        }
+        return ans;
+    }
+};
