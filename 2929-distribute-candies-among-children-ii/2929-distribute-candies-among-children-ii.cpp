be20c22df0763772a0c6941761fdbcc9b6d187d3
Martin Yang <martin0327@gmail.com>
2023-11-28 14:05:29 +0900

    Time: 27 ms (34.82%), Space: 6.4 MB (42.16%) - LeetHub

--- 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
@@ -13,31 +13,32 @@ public:
         for (int i=0; i<=lim; i++) {
             ll m = n - i;
             if (m < 0) continue;
-            ll lj = -1, rj = -1;
-            {
-                ll lo = 0, hi = min(m,lim);
-                while (lo <= hi) {
-                    ll mid = (lo+hi)/2;
-                    ll k = m - mid;
-                    if (k <= lim) {
-                        lj = mid;
-                        hi = mid - 1;
-                    } 
-                    else lo = mid + 1;
-                }
-            }
-            {
-                ll lo = 0, hi = min(m,lim);
-                while (lo <= hi) {
-                    ll mid = (lo+hi)/2;
-                    ll k = m - mid;
-                    if (0 <= k) {
-                        rj = mid;
-                        lo = mid + 1;
-                    }
-                    else hi = mid - 1;
-                }
-            }
+            ll lj = max(0ll,m-lim), rj = min(m,lim);
+            // ll lj = -1, rj = -1;
+            // {
+            //     ll lo = 0, hi = min(m,lim);
+            //     while (lo <= hi) {
+            //         ll mid = (lo+hi)/2;
+            //         ll k = m - mid;
+            //         if (k <= lim) {
+            //             lj = mid;
+            //             hi = mid - 1;
+            //         } 
+            //         else lo = mid + 1;
+            //     }
+            // }
+            // {
+            //     ll lo = 0, hi = min(m,lim);
+            //     while (lo <= hi) {
+            //         ll mid = (lo+hi)/2;
+            //         ll k = m - mid;
+            //         if (0 <= k) {
+            //             rj = mid;
+            //             lo = mid + 1;
+            //         }
+            //         else hi = mid - 1;
+            //     }
+            // }
             if (lj != -1 && rj != -1) {
                 ans += max(0ll, rj-lj+1);
             }
