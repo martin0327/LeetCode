b23654c3ff8ecb4ae83af7c9572b0893aef6dc50
Martin Yang <martin0327@gmail.com>
2023-11-28 14:07:26 +0900

    Time: 26 ms (39.18%), Space: 6.2 MB (78.32%) - LeetHub

--- 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
@@ -1,47 +1,12 @@
 using ll = long long;
-using vi = vector<ll>;
 
-void debug(vector<ll> &a) {
-    for (auto x : a) {
-        cout << x << " ";
-    }   cout << endl;
-}
 class Solution {
 public:
     long long distributeCandies(int n, int limit) {
         ll ans = 0, lim = limit;
-        for (int i=0; i<=lim; i++) {
-            ll m = n - i;
-            if (m < 0) continue;
-            ll lj = max(0ll,m-lim), rj = min(m,lim);
-            // ll lj = -1, rj = -1;
-            // {
-            //     ll lo = 0, hi = min(m,lim);
-            //     while (lo <= hi) {
-            //         ll mid = (lo+hi)/2;
-            //         ll k = m - mid;
-            //         if (k <= lim) {
-            //             lj = mid;
-            //             hi = mid - 1;
-            //         } 
-            //         else lo = mid + 1;
-            //     }
-            // }
-            // {
-            //     ll lo = 0, hi = min(m,lim);
-            //     while (lo <= hi) {
-            //         ll mid = (lo+hi)/2;
-            //         ll k = m - mid;
-            //         if (0 <= k) {
-            //             rj = mid;
-            //             lo = mid + 1;
-            //         }
-            //         else hi = mid - 1;
-            //     }
-            // }
-            if (lj != -1 && rj != -1) {
-                ans += max(0ll, rj-lj+1);
-            }
+        for (int i=0; i<=min(n,limit); i++) {
+            ll m = n - i, lj = max(0ll,m-lim), rj = min(m,lim);
+            ans += max(0ll, rj-lj+1);
         }
         return ans;
     }
