71637656054c8c99aeec8d1bab2f7945935749a1
Martin Yang <martin0327@gmail.com>
2023-11-29 19:44:16 +0900

    Time: 265 ms (7.14%), Space: 45.1 MB (14.29%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.kt
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun areSimilar(a: Array<IntArray>, k: Int): Boolean {
-        // val m = a[0].size
-        // val d = k % m
-        // return a contentDeepEquals a.mapIndexed { i, b -> 
-        //     when (i % 2) {
-        //         0 -> b.sliceArray(m-d until m) + b.slice(0 until m-d)
-        //         else -> b.sliceArray(d until m) + b.sliceArray(0 until d) 
-        //     }
-        // }.toTypedArray()
         return a.mapIndexed { i, b ->
             val m = b.size
             val d = k % m
