2263352f9c89669dd0302feb079584589efcd354
Martin Yang <martin0327@gmail.com>
2023-11-29 18:36:29 +0900

    Time: 279 ms (17.65%), Space: 40.5 MB (29.41%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun findWordsContaining(a: Array<String>, x: Char): List<Int> {
+        return (0 until a.size).filter {
+            i -> x in a[i]
+        }.toList()
+    }
+}
