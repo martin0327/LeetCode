02c9fefc21f82539bd7c94aa9ba9b6b63afe3fbf
Martin Yang <martin0327@gmail.com>
2023-11-29 23:00:23 +0900

    Time: 11 ms (31.33%), Space: 28.4 MB (10.89%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -3,40 +3,23 @@ using ll = long long;
 class Solution {
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& a, vector<int>& b) {
-        ll mx1 = 1, mx2 = 1;
         
         sort(a.begin(), a.end());
         sort(b.begin(), b.end());
         
-        {
-            ll cnt = 0;
+        auto f = [&] (vector<int> a) {
+            ll cnt = 0, ret = 1;
             for (int i=0; i<a.size(); i++) {
-                if (cnt==0) {
-                    cnt++;
-                }
+                if (cnt==0) cnt++;
                 else {
                     if (a[i]-a[i-1]==1) cnt++;
                     else cnt = 1;
                 }
-                mx1 = max(mx1, cnt);
+                ret = max(ret, cnt);
             }
-        }
-        {
-            ll cnt = 0;
-            for (int i=0; i<b.size(); i++) {
-                if (cnt==0) {
-                    cnt++;
-                }
-                else {
-                    if (b[i]-b[i-1]==1) cnt++;
-                    else cnt = 1;
-                }
-                mx2 = max(mx2, cnt);
-            }
-        }
-        ll s = min(mx1,mx2);
-        s++;
-        return s*s;        
-        
+            return ret;
+        };        
+        ll s = min(f(a),f(b))+1;
+        return s*s;
     }
 };
