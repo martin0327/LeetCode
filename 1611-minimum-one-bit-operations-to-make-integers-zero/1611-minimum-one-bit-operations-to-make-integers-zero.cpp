ad2fe87ff12fe21c677742fa55e5254fa5f14c92
Martin Yang <martin0327@gmail.com>
2023-11-30 17:00:44 +0900

    Time: 9 ms (7.58%), Space: 10 MB (7.58%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    map<int,int> dp1, dp2;
+    
+    int msb(int x) {
+        int p = 63 - __builtin_clzll(x);
+        return 1<<p;
+    }
+
+    int f(int x) {
+        if (dp1.count(x)) return dp1[x];
+        if (x <= 1) return dp1[x] = x;
+        if (__builtin_popcount(x) == 1) return dp1[x] = 1 + g(x>>1) + f(x>>1);
+        int m = msb(x);
+        int y = x - m;
+
+        if (y >= m/2) {
+            return dp1[x] = 1 + f(m/2) + f(y-(m/2));
+        }
+        else {
+            int mm = msb(y);
+            return dp1[x] = 1 + f(y-mm) + g(m/2) - g(mm) + f(m/2);
+        }
+    }
+
+    int g(int x) {
+        assert((x&(x-1)) == 0);
+        if (x <= 1) return x;
+        return dp2[x] = g(x/2) + f(x/2) + 1;
+    }
+
+    int minimumOneBitOperations(int n) {
+        return f(n);
+    }
+};
