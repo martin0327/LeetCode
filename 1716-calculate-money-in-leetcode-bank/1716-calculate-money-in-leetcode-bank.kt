6f5ef714f9b39297de5c20e6498b0138a4b6f7a3
Martin Yang <martin0327@gmail.com>
2023-12-06 10:26:08 +0900

    Time: 128 ms (33.33%), Space: 33.1 MB (60.00%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.kt
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.kt
@@ -1,8 +1,7 @@
 class Solution {
     fun totalMoney(n: Int): Int {
-        val q = n / 7;
-        val r = n % 7;
-        return 7*q*(q+7)/2 + r*(2*q+r+1)/2
-        
+        val q = n / 7
+        val r = n % 7
+        return 7*q*(q+7)/2 + r*(2*q+r+1)/2        
     }
 }
