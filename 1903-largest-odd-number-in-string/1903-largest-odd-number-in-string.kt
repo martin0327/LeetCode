f60b45ba16e8de7c4f0701412c21d62f05c9d582
Martin Yang <martin0327@gmail.com>
2023-12-07 10:54:26 +0900

    Time: 235 ms (26.92%), Space: 39.2 MB (88.46%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.kt
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun largestOddNumber(s: String): String {
-        return s.substring(0, s.indexOfLast { it.toString().toInt() and 1 == 1} + 1)
+        return s.substring(0, s.indexOfLast { it.toString().toInt().rem(2) == 1} + 1)
         // return (s.length-1 downTo 0).
         // find { (s[it].toString().toInt() and 1) == 1 }
         // ?.let { s.substring(0,it+1) } ?: ""
