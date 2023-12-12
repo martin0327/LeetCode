f6e18a898ed77bab6115eabe4a71f41cc5ea9da8
Martin Yang <martin0327@gmail.com>
2023-12-12 14:22:43 +0900

    Time: 459 ms (37.24%), Space: 214.4 MB (21.31%) - LeetHub

--- 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.cpp
+++ 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.cpp
@@ -18,28 +18,18 @@ public:
         ll n = a.size();
         map<ll,vi> mp;
         for (int i=0; i<n; i++) mp[a[i]].push_back(i);
-        vector<pair<ll,ll>> b, st;
+        vector<pair<ll,ll>> b;
         for (auto &[k,v] : mp) {
             ll l = v.front(), r = v.back();
             b.push_back({l,r});
         }
         sort(b.begin(), b.end());
+        vi st;
         for (auto [l,r] : b) {
-            if (st.empty()) st.push_back({l,r});
-            else {
-                auto [l1,r1] = st.back();
-                if (l < r1) {
-                    st.pop_back();
-                    ll l2 = min(l,l1);
-                    ll r2 = max(r,r1);
-                    st.push_back({l2,r2});
-                }
-                else st.push_back({l,r});
-            }
+            if (st.empty() || st.back() < l) st.push_back(r);
+            else st.back() = max(st.back(), r);
         }
         ll sz = st.size();
-        cout << sz << endl;
         return mpow(2,sz-1);
-       
     }
-};
+};  
