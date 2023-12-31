7f6e7e97bccc303c7c558576c72a868a74d70547
Martin Yang <martin0327@gmail.com>
2023-12-31 16:24:10 +0900

    Time: 0 ms (100.00%), Space: 6.7 MB (75.31%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        int ans = -1;
-        int n = s.size();
+        int n = s.size(), ans = -1;
         vector<int> pr(26,-1);
         for (int i=0; i<n; i++) {
             int x = s[i] - 'a';
             if (pr[x] == -1) pr[x] = i;
-            else ans = max(ans, i - pr[x] - 1);
+            else ans = max(ans, i-pr[x]-1);
         }
         return ans;
     }
