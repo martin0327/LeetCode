2eb1ab513bedc3e9bdcc01a38e160e953294252b
Martin Yang <martin0327@gmail.com>
2024-01-02 20:35:20 +0900

    Time: 340 ms (30.69%), Space: 70.9 MB (32.32%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<int> s(a.begin(), a.end());
         int ans = 0;
         for (auto x : s) {
-            if (!s.count(x-1)) { // O(1)
+            if (!s.count(x-1)) {
                 int len = 1;
                 for (int i=x+1; s.count(i); i++) len++;
                 ans = max(ans, len);
