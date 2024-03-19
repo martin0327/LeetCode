df06131ee2f53ffe574e3c9b4050776dcd6e8dc6
Martin Yang <martin0327@gmail.com>
2024-03-19 14:23:58 +0900

    Time: 168 ms (5.04%), Space: 62.4 MB (5.93%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,34 @@
+using vi = vector<int>;
+using pii = pair<int,int>;
+using vp = vector<pii>;
+
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vi cnt(26);
+        for (auto x : tasks) cnt[x-'A']++;
+        set<pii> a;
+        for (int i=0; i<26; i++) {
+            if (cnt[i] > 0) a.insert({-cnt[i],i});
+        }
+        int cur = 0;
+        vi last(26,-100);
+        while (a.size()) {
+            bool flag = false;
+            for (auto [x,i] : a) {
+                if (cur > last[i] + n) {
+                    flag = true;
+                    last[i] = cur;
+                    cur++;
+                    a.erase({x,i});
+                    if (x+1 != 0) a.insert({x+1,i});
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) cur++;
+        }
+        return cur;
+    }
+};
