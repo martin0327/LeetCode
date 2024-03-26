4bb23c6dedd80efe4518aa23cd92c00bd4f2ceac
Martin Yang <martin0327@gmail.com>
2024-03-26 09:24:53 +0900

    Time: 43 ms (62.28%), Space: 43.6 MB (55.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    void debug(vector<int> &a) {
-        for (auto x : a) {
-            cout << x << " ";
-        }   cout << endl;
-    }
     int firstMissingPositive(vector<int>& a) {
         int n = a.size();
         for (int i=0; i<n; i++) {
             while (a[i] != i+1 && 1 <= a[i] && a[i] <= n) {
-                if (a[a[i]-1] != a[i]) swap(a[i], a[a[i]-1]);
+                int j = a[i] - 1;
+                if (a[j] != a[i]) swap(a[i], a[j]);
                 else break;
             }
-            // debug(a);
         }
         for (int i=0; i<n; i++) {
             if (a[i] != i+1) return i+1;
         }
         return n+1;
-        
     }
 };
