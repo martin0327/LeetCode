4f1ee9fe18e0880d4439f326f5a221d33bcf1485
Martin Yang <martin0327@gmail.com>
2024-03-26 13:29:29 +0900

    Time: 38 ms (83.67%), Space: 43.6 MB (55.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,7 +4,7 @@ public:
         int n = a.size();
         a.push_back(-1);
         for (int i=0; i<n; i++) {
-            while (a[i] != i && 1 <= a[i] && a[i] <= n) {
+            while (1 <= a[i] && a[i] <= n) {
                 int j = a[i];
                 if (a[j] != a[i]) swap(a[i], a[j]);
                 else break;
