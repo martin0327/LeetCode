f9da412ddba0c64f88dd6e469076cf503da06f04
Martin Yang <martin0327@gmail.com>
2024-03-26 20:26:13 +0900

    Time: 6 ms (27.97%), Space: 7.4 MB (44.16%) - LeetHub

---
+++ 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(int k) {
+        int ans = k - 1;
+        for (int i=1; i<k; i++) {
+            int x = 1 + i;
+            int q = (k + (x-1)) / x;
+            ans = min(ans, q-1+i);
+        }
+        return ans;
+    }
+};
