236debb9cae91173b2b5b76738d46ff3dddbcded
Martin Yang <martin0327@gmail.com>
2024-03-26 20:26:52 +0900

    Time: 5 ms (31.14%), Space: 7.4 MB (10.44%) - LeetHub

--- 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minOperations(int k) {
         int ans = k - 1;
-        for (int i=1; i<k; i++) {
+        for (int i=1; i<=k/2; i++) {
             int x = 1 + i;
             int q = (k + (x-1)) / x;
             ans = min(ans, q-1+i);
