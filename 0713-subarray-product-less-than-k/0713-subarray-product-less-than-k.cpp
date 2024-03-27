6c34ab2e4b46cb9df5b56c233074c40f42105ae5
Martin Yang <martin0327@gmail.com>
2024-03-27 13:57:01 +0900

    Time: 60 ms (44.94%), Space: 63.7 MB (13.98%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -5,13 +5,12 @@ public:
         if (k<=1) return 0;
         int n = a.size();
         int ans=0, i=0, j=0, p=1;
-        vector<bool> check(n+1);
         while (i < n) {
             if (j < n && p < k) p *= a[j++];
             else p /= a[i++];
-            if (p < k && !check[j]) {
+            if (p < k) {
                 ans += j - i;
-                check[j] = 1;
+                if (j==n) break;
             }
         }
         return ans;
