b76f4bfba97fba660d5b70f7f7150264dba53e69
Martin Yang <martin0327@gmail.com>
2024-03-30 11:40:57 +0900

    Time: 34 ms (96.07%), Space: 39.2 MB (92.41%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,25 +1,15 @@
-template<typename T>
-void chmax(T &x, T y) {x = max(x,y);}
-
-template<typename T>
-void chmin(T &x, T y) {x = min(x,y);}
-
-
 class Solution {
 public:
     using vi = vector<int>;
     int subarraysWithKDistinct(vector<int>& a, int k) {
-
         int n = a.size();
         vi c(n+1);
         int cnt = 0, ans = 0;
-        map<int,int> mx, mn;
+        vi max_left(n+1, -1);
+        vi min_left(n+1, -1);
         for (int i=0, j=0; i<n; ) {
             if (cnt == k) {
-                // if (!mx.count(j)) mx[j] = i;
-                // else chmax(mx[j],i);
-                if (!mn.count(j)) mn[j] = i;
-                else chmin(mn[j],i);
+                if (min_left[j] == -1) min_left[j] = i;
             }
             if (j < n && cnt <= k) {
                 if (++c[a[j++]]==1) cnt++;
@@ -39,25 +29,20 @@ public:
                 else break;
             }
             if (cnt == k) {
-                if (!mx.count(j)) mx[j] = i;
-                else chmax(mx[j],i);
+                max_left[j] = i;
             }
             if (j==n) break;
             if (j < n && cnt <= k) {
                 if (++c[a[j++]]==1) cnt++;
             }
-            // else {
-            //     if (--c[a[i++]]==0) cnt--;
-            // }
         }
         // debug(mx);
         // debug(mn);
-        set<int> rr;
-        for (auto [k,v] : mx) rr.insert(k);
-        for (auto [k,v] : mn) rr.insert(k);
-        for (auto j : rr) {
-            int l = mn[j];
-            int r = mx[j];
+        for (int i=0; i<=n; i++) {
+            if (min_left[i] == -1) continue;
+            if (max_left[i] == -1) continue;
+            int l = min_left[i];
+            int r = max_left[i];
             ans += r-l+1;
         }
 
