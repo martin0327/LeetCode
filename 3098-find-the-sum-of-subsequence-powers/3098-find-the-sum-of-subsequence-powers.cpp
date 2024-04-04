5ed46e52347cb5a762a11eba96dc677637cfbef7
Martin Yang <martin0327@gmail.com>
2024-04-04 14:28:44 +0900

    Time: 510 ms (67.39%), Space: 50.5 MB (73.14%) - LeetHub

--- 3098-find-the-sum-of-subsequence-powers/3098-find-the-sum-of-subsequence-powers.cpp
+++ 3098-find-the-sum-of-subsequence-powers/3098-find-the-sum-of-subsequence-powers.cpp
@@ -15,35 +15,81 @@ vector<T> get_unique(vector<T> a) {
     return a;
 }
 
+ll safe_mod(ll x, ll m) {
+    if (x >= m) x %= m;
+    if (x < 0) x += m;
+    return x;
+}
 
 class Solution {
 public:
     int sumOfPowers(vector<int>& nums, int k) {
         vi a(nums.begin(), nums.end());
         ll n = a.size();
+        a.push_back(-1e9);
         sort(a.begin(), a.end());
-        a.insert(a.begin(), -1e9);
         vi diffs;
         for (int i=1; i<=n; i++) {
             for (int j=i+1; j<=n; j++) {
-                diffs.push_back(abs(a[i]-a[j]));
+                int d = a[j] - a[i];
+                if (d > 0) diffs.push_back(d);
             }
         }
         diffs = get_unique(diffs);
+        int dsz = diffs.size();
+        vvi lower_idx(dsz,vi(n, n+1));
+        auto upper_idx = lower_idx;
+        for (int di=0; di<dsz; di++) {
+            int d = diffs[di];
+            for (int i=0, j=0; i<n; i++) {
+                while (j <= n && a[j]-a[i] < d) j++;
+                if (j <= n && a[j]-a[i] >= d) lower_idx[di][i] = j;
+            }
+            for (int i=0, j=0; i<n; i++) {
+                while (j<=n && a[j]-a[i] <= d) j++;
+                if (j <= n && a[j]-a[i] > d) upper_idx[di][i] = j;
+            }
+        }
+        vvvi dp(n+1, vvi(k+1, vi(2)));
+        vvvi pre(n+1, vvi(k+1, vi(2)));
+        
         ll ans = 0;
-        for (auto d : diffs) {
-            vvvi dp(n+1, vvi(k+1, vi(2)));
-            dp[0][0][0] = 1;
-            for (int i=0; i<n; i++) {
-                for (int j=0; j<k; j++) {
+        for (int di=0; di<dsz; di++) {
+            ll d = diffs[di];
+            for (int i=0; i<=n; i++) {
+                for (int j=0; j<=k; j++) {
                     for (int l=0; l<2; l++) {
+                        dp[i][j][l] = pre[i][j][l] = 0;
+                    }
+                }
+            }
+            dp[0][0][0] = 1;
+            for (int i=0; i<=n; i++) {
+                for (int j=0; j<=k; j++) {
+                    for (int l=0; l<2; l++) {
+                        if (i > 0) {
+                            if (pre[i-1][j][l] > 0) {
+                                pre[i][j][l] = safe_mod(pre[i][j][l]+pre[i-1][j][l], mod);
+                            }
+                            if (pre[i][j][l] > 0) {
+                                dp[i][j][l] = safe_mod(dp[i][j][l]+pre[i][j][l], mod);
+                            }
+                        }
                         if (dp[i][j][l] == 0) continue;
-                        for (int ni=i+1; ni<=n; ni++) {
-                            ll w = abs(a[i] - a[ni]);
-                            if (w >= d) {
-                                int nl = l | (w == d);
-                                dp[ni][j+1][nl] += dp[i][j][l];
-                                dp[ni][j+1][nl] %= mod;
+                        if (i < n && j < k) {
+                            int ni = lower_idx[di][i];
+                            if (ni > n) continue;
+                            ll w = a[ni] - a[i];
+                            if (l==1 || w > d) {
+                                pre[ni][j+1][l] = safe_mod(pre[ni][j+1][l]+dp[i][j][l], mod);
+                            }
+                            else {
+                                pre[ni][j+1][1] = safe_mod(pre[ni][j+1][1]+dp[i][j][l],mod);
+                                int ni2 = upper_idx[di][i];
+                                if (ni2 <= n) {
+                                    pre[ni2][j+1][1] = safe_mod(pre[ni2][j+1][1]-dp[i][j][l],mod);
+                                    pre[ni2][j+1][0] = safe_mod(pre[ni2][j+1][0]+dp[i][j][l],mod);
+                                }
                             }
                         }
                     }
@@ -54,7 +100,6 @@ public:
                 cnt += dp[i][k][1];
                 cnt %= mod;
             }
-            // debug(d,cnt);
             ans += d * cnt;
             ans %= mod;
         }
