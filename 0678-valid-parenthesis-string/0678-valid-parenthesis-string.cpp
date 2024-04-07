82ab0e7403385f9a1158e922d46cc26094d77912
Martin Yang <martin0327@gmail.com>
2024-04-08 01:38:45 +0900

    Time: 3 ms (45.50%), Space: 7.4 MB (53.16%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int l=0,r=0,w=0;
+        for (auto c : s) {
+            if (c == '*') w++;
+            else if (c == ')') r++;
+            else l++;
+        }
+
+        if (r < l) {
+            for (auto &c : s) {
+                if (c == ')') c = '(';
+                else if (c == '(') c = ')';
+            }
+            reverse(s.begin(), s.end());
+            swap(l,r);
+        }
+
+        int d = r - l;
+        if (d > w) return false;
+        for (auto &c : s) {
+            if (c == '*' && d > 0) {
+                c = '(';
+                d--;
+                w--;
+                l++;
+            }
+        }
+        int h = w / 2;
+        for (auto &c : s) {
+            if (c == '*' && h > 0) {
+                c = '(';
+                h--;
+            }
+        }
+        reverse(s.begin(), s.end());
+        h = w / 2;
+        for (auto &c : s) {
+            if (c == '*' && h > 0) {
+                c = ')';
+                h--;
+            }
+        }
+        reverse(s.begin(), s.end());
+        int x = 0;
+        for (auto c : s) {
+            if (c == '(') x++;
+            else if (c == ')') x--;
+            if (x < 0) return false;
+        }
+        return true;
+
+    }
+};
