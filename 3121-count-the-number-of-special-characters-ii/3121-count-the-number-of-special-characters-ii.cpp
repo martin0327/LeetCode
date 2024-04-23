48f7af0f79c07ba918cde4548d9394314f15f011
Martin Yang <martin0327@gmail.com>
2024-04-23 20:47:22 +0900

    Time: 190 ms (17.11%), Space: 66.2 MB (5.23%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,26 +1,19 @@
-using ll = long long;
-using vi = vector<ll>;
-using vvi = vector<vi>;
+using vi = vector<int>;
 
 class Solution {
 public:
     int numberOfSpecialChars(string s) {
-        vvi a(26), b(26);
         int n = s.size();
+        map<char,vi> mp;
         for (int i=0; i<n; i++) {
             char c = s[i];
-            int x = c - 'a';
-            int y = c - 'A';
-            auto f = [&] (int x) {
-                return 0 <= x && x < 26;
-            };
-            if (f(x)) a[x].push_back(i);
-            if (f(y)) b[y].push_back(i);
+            mp[c].push_back(i);
         }
         int ans = 0;
         for (int i=0; i<26; i++) {
-            if (a[i].size() && b[i].size()) {
-                if (a[i].back() < b[i].front()) ans++;
+            char x = 'a' + i, y = 'A' + i;
+            if (mp.count(x) && mp.count(y)) {
+                if (mp[x].back() < mp[y].front()) ans++;
             }
         }
         return ans;
