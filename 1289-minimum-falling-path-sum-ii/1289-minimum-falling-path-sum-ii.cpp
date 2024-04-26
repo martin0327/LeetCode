92e7617cde482b88fd9e843f81d93dc2570f323d
Martin Yang <martin0327@gmail.com>
2024-04-26 10:22:34 +0900

    Time: 19 ms (98.44%), Space: 18.2 MB (57.11%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -7,13 +7,17 @@ public:
     int minFallingPathSum(vector<vector<int>>& a) {
         int n = a.size();
         vvi dp(n, vi(n,inf));
+        vi pre(n), suf(n);
         for (int j=0; j<n; j++) dp[0][j] = a[0][j];
         for (int i=1; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                for (int l=0; l<n; l++) {
-                    if (j==l) continue;
-                    dp[i][j] = min(dp[i][j], dp[i-1][l] + a[i][j]);
-                }
+            pre[0] = dp[i-1][0];
+            for (int j=1; j<n; j++) pre[j] = min(pre[j-1], dp[i-1][j]);
+            suf[n-1] = dp[i-1][n-1];
+            for (int j=n-2; j>=0; j--) suf[j] = min(suf[j+1], dp[i-1][j]);
+            auto p = [&] (int j) { return (j>=0) ? pre[j] : inf; };
+            auto s = [&] (int j) { return (j<n) ? suf[j] : inf; };
+            for (int j=0; j<n; j++) {    
+                dp[i][j] = a[i][j] + min(p(j-1),s(j+1));
             }
         }
         int ans = inf;
