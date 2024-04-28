538d4b7da8c26f58c88d58857ce507486437d59d
Martin Yang <martin0327@gmail.com>
2024-04-28 10:51:46 +0900

    Time: 454 ms (100.00%), Space: 75.2 MB (83.33%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -8,34 +8,29 @@ ll safe_mod(ll x) {
     x %= mod;
     return (x<0) ? x+mod : x;
 }
-ll mod_sum(ll x, ll y) { return safe_mod(x+y); }
-ll acc_sum(vi &a) { 
-    ll ret = 0;
-    for (auto x : a) ret = mod_sum(ret, x);
-    return ret;
-}
+void mod_sum(ll &x, ll y) { x = safe_mod(x+y); }
+ll mod_acc(vi &a) { return safe_mod(accumulate(a.begin(), a.end(),0ll)); }
 
 vi dp[1001][1001];
+
 class Solution {
 public:
     
     int numberOfStableArrays(int n, int m, int lim) {
         for (int i=0; i<=n; i++) {
-            for (int j=0; j<=m; j++) {
-                dp[i][j] = {0,0};
-            }
+            for (int j=0; j<=m; j++) dp[i][j] = {0,0};
         }
-        // vvvi dp(n+1, vvi(m+1, vi(2)));
+
         for (int i=1; i<=min(n,lim); i++) dp[i][0][0] = 1;
         for (int j=1; j<=min(m,lim); j++) dp[0][j][1] = 1;
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=m; j++) {
-                if (i > 0) dp[i][j][0] = mod_sum(dp[i][j][0], acc_sum(dp[i-1][j]));
-                if (j > 0) dp[i][j][1] = mod_sum(dp[i][j][1], acc_sum(dp[i][j-1]));
-                if (i > lim) dp[i][j][0] = mod_sum(dp[i][j][0], -dp[i-lim-1][j][1]);
-                if (j > lim) dp[i][j][1] = mod_sum(dp[i][j][1], -dp[i][j-lim-1][0]);
+                mod_sum(dp[i][j][0], mod_acc(dp[i-1][j]));
+                mod_sum(dp[i][j][1], mod_acc(dp[i][j-1]));
+                if (i > lim) mod_sum(dp[i][j][0], -dp[i-lim-1][j][1]);
+                if (j > lim) mod_sum(dp[i][j][1], -dp[i][j-lim-1][0]);
             }
         }
-        return acc_sum(dp[n][m]);
+        return mod_acc(dp[n][m]);
     }
 };
