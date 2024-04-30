f3d8386d7f13c244cfcba229aa3649b548140599
Martin Yang <martin0327@gmail.com>
2024-04-30 13:47:11 +0900

    Time: 64 ms (65.27%), Space: 16.6 MB (64.67%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -2,11 +2,9 @@ using ll = long long;
 class Solution {
 public:
     long long wonderfulSubstrings(string s) {
-        ll ans = 0, p = 0;
-        vector<ll> b = {0}, cnt(1<<10);
-        for (int i=0; i<10; i++) {
-            b.push_back(1ll<<i);
-        }
+        ll ans = 0, p = 0, k = 10;
+        vector<ll> b = {0}, cnt(1<<k);
+        for (int i=0; i<k; i++) b.push_back(1ll<<i);
         cnt[0] = 1;
         for (auto c : s) {
             p ^= (1ll<<(c-'a'));
@@ -17,4 +15,4 @@ public:
         }
         return ans;
     }
-};
+}; 
