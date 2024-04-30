45b5ef6580827106a7e4313be8c94ebf1f2ff080
Martin Yang <martin0327@gmail.com>
2024-04-30 13:39:05 +0900

    Time: 67 ms (65.27%), Space: 16.5 MB (64.67%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -7,15 +7,15 @@ public:
         for (int i=0; i<10; i++) {
             b.push_back(1ll<<i);
         }
-        map<ll,ll> mp; mp[0]++;
+        vector<ll> cnt((1<<10));
+        cnt[0] = 1;
         for (auto c : s) {
             p ^= (1ll<<(c-'a'));
             for (auto x : b) {
                 ll t = x ^ p;
-                auto it = mp.find(t);
-                if (it != mp.end()) ans += it->second;
+                ans += cnt[t];
             }
-            mp[p]++;
+            cnt[p]++;
         }
         return ans;
     }
