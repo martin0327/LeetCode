4d903485abec1e2eb1ea344d4ce8d0340332b7b2
Martin Yang <martin0327@gmail.com>
2024-05-07 20:19:47 +0900

    Time: 0 ms (100.00%), Space: 8.7 MB (9.87%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -3,20 +3,17 @@ public:
     bool isValid(string s) {
         string sp = "@#$";
         string vow = "aeiouAEIOU";
-        set<char> sc1(sp.begin(), sp.end());
-        set<char> sc2(vow.begin(), vow.end());
-        bool ans1 = true;
-        ans1 &= (s.size() >= 3);
-        bool ans2 = false;
-        bool ans3 = false;
+        set<char> sc(sp.begin(), sp.end());
+        set<char> v(vow.begin(), vow.end());
+        bool b1 = (s.size() >= 3);
+        bool b2 = false;
+        bool b3 = false;
         for (auto c : s) {
-            if (sc1.count(c)) ans1 = false;
-            if (sc2.count(c)) ans2 = true;
-            int x = c - 'a';
-            int y = c - 'A';
-            if (0 <= x && x < 26 && !sc2.count(c)) ans3 = true;
-            if (0 <= y && y < 26 && !sc2.count(c)) ans3 = true;
+            if (sc.count(c)) b1 = false;
+            if (v.count(c)) b2 = true;
+            if ('a' <= c && c < 'z' && !v.count(c)) b3 = true;
+            if ('A' <= c && c < 'Z' && !v.count(c)) b3 = true;
         }
-        return ans1 && ans2 && ans3;
+        return b1 && b2 && b3;
     }
 };
