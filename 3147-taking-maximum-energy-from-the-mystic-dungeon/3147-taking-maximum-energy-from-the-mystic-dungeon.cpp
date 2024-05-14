070590a7925e250afd4f623d37a3ede719606510
Martin Yang <martin0327@gmail.com>
2024-05-14 21:13:08 +0900

    Time: 151 ms (37.37%), Space: 149.7 MB (29.00%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,18 +1,15 @@
-using vi = vector<int>;
-
 const int inf = 2e9;
 class Solution {
 public:
     int maximumEnergy(vector<int>& a, int k) {
         int n = a.size();
-        vi dp(n, -inf);
-        function<int(int)> f = [&] (int i) {
-            if (i >= n) return 0;
-            if (dp[i] != -inf) return dp[i];
-            return dp[i] = f(i+k) + a[i];
-        };
+        vector<int> dp(n);
         int ans = -inf;
-        for (int i=0; i<n; i++) ans = max(ans, f(i));
+        for (int i=n-1; i>=0; i--) {
+            if (i+k >= n) dp[i] = a[i];
+            else dp[i] = a[i] + dp[i+k];
+            ans = max(ans, dp[i]);
+        }
         return ans;
     }
 };
