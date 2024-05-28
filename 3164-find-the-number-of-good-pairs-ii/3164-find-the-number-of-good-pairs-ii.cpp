a1fa27291d308dc4687bc2efdf6d1c455ac574c3
Martin Yang <martin0327@gmail.com>
2024-05-28 21:17:55 +0900

    Time: 991 ms (71.87%), Space: 305.5 MB (8.56%) - LeetHub

---
+++ 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.cpp
@@ -0,0 +1,20 @@
+using ll = long long;
+const ll sz = 1e6;
+class Solution {
+public:
+    long long numberOfPairs(vector<int>& a, vector<int>& b, int k) {
+        vector<ll> cnt(sz+1);
+        for (auto x : b) cnt[x]++;
+        ll ans = 0;
+        for (auto x : a) {
+            for (int d=1; d*d <= x; d++) {
+                if (x % d == 0) {
+                    int y = d, z = x / d;
+                    if (y % k == 0) ans += cnt[y/k];
+                    if (z % k == 0 && z != y) ans += cnt[z/k];
+                }
+            }
+        }
+        return ans;        
+    }
+};
