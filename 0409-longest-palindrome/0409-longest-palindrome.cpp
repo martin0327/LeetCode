4eb2aae9758d199af80fef2b09b632b2734e06f2
Martin Yang <martin0327@gmail.com>
2024-06-04 09:42:53 +0900

    Time: 0 ms (100.00%), Space: 9 MB (26.04%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         map<char,int> mp;
-        for (auto c : s) {
-            mp[c]++;
-        }
+        for (auto c : s) mp[c]++;
+        
         bool odd = false;
         int ans = 0;
         for (auto [k,v] : mp) {
             odd |= (v&1);
-            ans += 2*(v/2);
+            ans += v-(v&1);
         }
         return ans + odd;        
     }
