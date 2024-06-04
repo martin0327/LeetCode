0001e3692026ac54964b1774dec2427f12c4f621
Martin Yang <martin0327@gmail.com>
2024-06-04 21:42:42 +0900

    Time: 281 ms (98.11%), Space: 129.1 MB (30.52%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -1,27 +1,45 @@
+using pii = pair<int,int>;
 class Solution {
 public:
     int countDays(int k, vector<vector<int>>& a) {
         int n = a.size();
-        vector<pair<int,int>> b(n);
+        vector<pii> b(n);
         for (int i=0; i<n; i++) {
             b[i] = {a[i][0],a[i][1]};
         }
         sort(b.begin(), b.end());
-        vector<pair<int,int>> c;
+        vector<pii> c; // merged intervals that has no overlaps
         for (auto [l,r] : b) {
-            if (c.empty() || l > c.back().second) c.push_back({l,r});
+            if (c.empty() || c.back().second < l) c.push_back({l,r});
             else c.back().second = max(c.back().second, r);
         }
         int ans = k;
-        for (auto [l,r] : c) {
-            ans -= (r-l+1);
-        }
+        for (auto [l,r] : c) ans -= (r-l+1);
         return ans;
+        
     }
 };
 
 // 1   2   3   4   5   6   7   8   9   10
 // [       ]       [       ]       [    ]
-    
-    
-    
+       // 3               3              2
+           
+// 10 - (3-1+1) - (7-5+1) - (10-9+1) = 2
+           
+           
+// if all intervals [l,r] are pairwise disjoint , then the "answer = days - sum(r-l+1)"
+// 
+           
+          // [1,4] [3,7] 
+// sort all intervals in ascending order of l 
+// [1,7]
+           
+           
+//  1   2   3   4   5
+           
+    // 1 2 3 4 -> they are pairwise different 
+           
+           
+           //   [3,6] [4,8] [4,5]
+           // [3,6] + [4,5] = [3,6]
+           // [3,6] + [4,8] = [3,8]
