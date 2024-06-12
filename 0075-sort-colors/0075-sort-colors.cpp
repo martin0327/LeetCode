6c6eb4a0b4a17385a17aca370a4f1bc924e5243e
Martin Yang <martin0327@gmail.com>
2024-06-12 09:46:08 +0900

    Time: 4 ms (31.04%), Space: 9.8 MB (71.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     void sortColors(vector<int>& a) {
-        int n = a.size();
         vector<int> cnt(3);
-        for (auto x : a) cnt[x]++;
-        for (int i=0,j=0; i<n; j++) {
+        for (int x : a) cnt[x]++;
+        for (int i=0,j=0; i<(int)a.size(); j++) {
             while (cnt[j]--) a[i++] = j;
         }        
     }
