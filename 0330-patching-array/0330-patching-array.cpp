57e0d0593e5ec12cfb5c3c3fdda9b5dcfd3ad110
Martin Yang <martin0327@gmail.com>
2024-06-16 10:31:23 +0900

    Time: 4 ms (67.05%), Space: 13.6 MB (82.46%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -2,10 +2,7 @@ using ll = long long;
 class Solution {
 public:
     int minPatches(vector<int>& a, int k) {
-        ll cur = 1, ans = 0;
-        if (a[0] == 1) a.erase(a.begin());
-        else ans++;
-        ll n = a.size();
+        ll cur = 0, ans = 0, n = a.size();
         for (int j=0; ;) {
             if (cur >= k) break;
             while (j<n && a[j]<=cur+1) {
