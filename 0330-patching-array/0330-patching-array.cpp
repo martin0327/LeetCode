c7f660858a3d9e46dc23c562f81f5c7788c4d8a1
Martin Yang <martin0327@gmail.com>
2024-06-16 10:33:18 +0900

    Time: 4 ms (67.05%), Space: 13.9 MB (12.30%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -4,10 +4,7 @@ public:
     int minPatches(vector<int>& a, int k) {
         ll cur = 0, ans = 0, n = a.size();
         for (int j=0; ;) {
-            if (cur >= k) break;
-            while (j<n && a[j]<=cur+1) {
-                cur += a[j++];
-            }
+            while (j<n && a[j]<=cur+1 && cur < k) cur += a[j++];
             if (cur >= k) break;
             if (j>=n || a[j] > cur+1) {
                 cur += cur+1;
