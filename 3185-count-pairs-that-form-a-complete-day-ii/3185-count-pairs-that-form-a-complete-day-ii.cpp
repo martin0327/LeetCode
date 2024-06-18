f9d33ae93bd99a35e901b29f750f88e81f23ab81
Martin Yang <martin0327@gmail.com>
2024-06-18 21:38:08 +0900

    Time: 192 ms (5.43%), Space: 138.4 MB (64.24%) - LeetHub

--- 3185-count-pairs-that-form-a-complete-day-ii/3185-count-pairs-that-form-a-complete-day-ii.cpp
+++ 3185-count-pairs-that-form-a-complete-day-ii/3185-count-pairs-that-form-a-complete-day-ii.cpp
@@ -6,11 +6,7 @@ public:
         map<ll,ll> mp;
         for (ll x : a) {
             x %= mod;
-            x += mod;
-            x %= mod;
             ll y = (mod-x) % mod;
-            assert(0 <= x && x < mod);
-            assert(0 <= y && y < mod);
             ans += mp[y];
             mp[x]++;
         }
