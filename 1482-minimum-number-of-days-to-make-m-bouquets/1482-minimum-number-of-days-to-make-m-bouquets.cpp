aeb2ece42e9c407ba66727e18271a11c891e11ed
Martin Yang <martin0327@gmail.com>
2024-06-20 08:17:16 +0900

    Time: 126 ms (13.00%), Space: 68.9 MB (92.98%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -6,13 +6,9 @@ public:
         ll lo = 1, hi = 1e9, ans = -1;
         while (lo <= hi) {
             ll mid = (lo+hi)/2;
-            vector<ll> b(n);
-            for (int i=0; i<n; i++) {
-                b[i] = (a[i] <= mid);
-            }
             ll cnt = 0, cur = 0;
             for (int i=0; i<n; i++) {
-                if (b[i]) cur++;
+                if (a[i] <= mid) cur++;
                 else cur = 0;
                 if (cur == k) {
                     cur = 0;
