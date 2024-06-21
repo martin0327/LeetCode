1ca8ed7b350e2e25e5db6068e3ee081eb6d72ff1
Martin Yang <martin0327@gmail.com>
2024-06-21 18:54:13 +0900

    Time: 23 ms (66.42%), Space: 35.5 MB (6.08%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& a, vector<int>& b, int k) {
-        int n = a.size();
-        vector<int> c(n), pre(n+1);
-        int ans = 0;
+        int n = a.size(), ans = 0, mx = 0;
+        vector<int> c(n), pre(n+1);        
         for (int i=0; i<n; i++) {
             if (b[i]) c[i] = a[i];
             else ans += a[i];
@@ -11,11 +10,9 @@ public:
         for (int i=1; i<=n; i++) {
             pre[i] = pre[i-1] + c[i-1];
         }
-        int mx = 0;
         for (int i=k; i<=n; i++) {
             mx = max(mx, pre[i]-pre[i-k]);
         }
-        ans += mx;
-        return ans;        
+        return ans + mx;   
     }
 };
