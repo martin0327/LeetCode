3072ae8fbb51cdeb944329d91f6f07bc650393b0
Martin Yang <martin0327@gmail.com>
2024-06-23 01:18:17 +0900

    Time: 33 ms (80.00%), Space: 40.7 MB (40.00%) - LeetHub

---
+++ 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.cpp
@@ -0,0 +1,41 @@
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+
+const ll mod = 1e9+7;
+
+
+class Solution {
+public:
+    int numberOfPermutations(int n, vector<vector<int>>& req) {
+        ll q = req.size(), m = 0;
+        vi a(n,-1);
+        for (auto v : req) m = max(m, a[v[0]] = v[1]);
+
+        vvi dp(n+1, vi(m+1));
+        dp[0][0] = 1;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<=m; j++) {
+                if (dp[i][j] == 0) continue;
+                for (int d=0; d<=i; d++) {
+                    int nj = j + d;
+                    if (nj > m) break;
+                    dp[i+1][nj] += dp[i][j];
+                    dp[i+1][nj] %= mod;
+                }
+            }
+
+            if (a[i] != -1) {
+                for (int j=0; j<=m; j++) {
+                    if (j != a[i]) dp[i+1][j] = 0;
+                }
+            }
+        }
+        ll ans = 0;
+        for (int j=0; j<=m; j++) {
+            ans += dp[n][j];
+            ans %= mod;
+        }
+        return ans;
+    }
+};
