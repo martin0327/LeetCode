2f48d184b3759f3a4e49e94a0b08999748b82769
Martin Yang <martin0327@gmail.com>
2024-06-23 11:16:39 +0900

    Time: 843 ms (5.06%), Space: 291.6 MB (5.23%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,4 +1,9 @@
 using ll = long long;
+using pii = pair<ll,ll>;
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
 
 class Solution {
 public:
@@ -6,15 +11,29 @@ public:
         ll n = a.size(), lo = 1, hi = n, ans = -1;
         while (lo <= hi) {
             ll mid = (lo+hi)/2;
-            multiset<ll> s;
+            min_pq<pii> pq1;
+            max_pq<pii> pq2;
+            
             for (int i=0; i<mid; i++) {
-                s.insert(a[i]);
+                pq1.emplace(a[i],i);
+                pq2.emplace(a[i],i);
             }
-            ll diff = *s.rbegin() - *s.begin();
+            ll diff = pq2.top().first - pq1.top().first;
             for (int i=mid; i<n; i++) {
-                s.insert(a[i]);
-                s.erase(s.find(a[i-mid]));
-                diff = min(diff, *s.rbegin() - *s.begin());
+                pq1.emplace(a[i],i);
+                pq2.emplace(a[i],i);
+                while (pq1.size()) {
+                    auto [x,j] = pq1.top();
+                    if (j <= i - mid) pq1.pop();
+                    else break;
+                }
+                while (pq2.size()) {
+                    auto [x,j] = pq2.top();
+                    if (j <= i - mid) pq2.pop();
+                    else break;
+                }
+                diff = min(diff, pq2.top().first - pq1.top().first);
+                if (diff <= lim) break;
             }
             if (diff <= lim) {
                 ans = mid;
