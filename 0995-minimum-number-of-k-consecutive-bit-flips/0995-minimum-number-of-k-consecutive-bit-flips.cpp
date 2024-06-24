d8efae2458dbd37c0e8e4e5323f8458b63662941
Martin Yang <martin0327@gmail.com>
2024-06-25 00:04:26 +0900

    Time: 76 ms (95.49%), Space: 113.4 MB (10.25%) - LeetHub

---
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& a, int k) {
+        int n = a.size(), ans = 0;
+        vector<int> b(n+1);
+        for (int i=0; i<n; i++) {
+            if (i > 0) b[i] ^= b[i-1];
+            a[i] ^= b[i];
+            if (i+k-1<n) {
+                if (!a[i]) {
+                    b[i] ^= 1;
+                    b[i+k] ^= 1;
+                    ans++;
+                }
+            }
+            else if (!a[i]) return -1;
+        }
+        return ans;
+    }
+};
