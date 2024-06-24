737ae1cedd65185b60b8e30aab4c5d5cc75a59ca
Martin Yang <martin0327@gmail.com>
2024-06-25 00:06:26 +0900

    Time: 78 ms (93.44%), Space: 113.2 MB (34.84%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -4,8 +4,7 @@ public:
         int n = a.size(), ans = 0;
         vector<int> b(n+1);
         for (int i=0; i<n; i++) {
-            if (i > 0) b[i] ^= b[i-1];
-            a[i] ^= b[i];
+            if (i > 0) a[i] ^= (b[i] ^= b[i-1]);
             if (i+k-1<n) {
                 if (!a[i]) {
                     b[i] ^= 1;
