b569e1a521d9d5a8e094c613c63ed5942bfc4d5e
Martin Yang <martin0327@gmail.com>
2024-06-30 13:43:12 +0900

    Time: 1821 ms (50.00%), Space: 351.9 MB (50.00%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -4,76 +4,42 @@ using vvi = vector<vi>;
 class Solution {
 public:
     int minimumDiameterAfterMerge(vector<vector<int>>& e1, vector<vector<int>>& e2) {
-
-        int n = e1.size() + 1;
-        int m = e2.size() + 1;
-
-        auto f = [&] (vector<vector<int>>& e) {
+        
+        function<signed(vvi&)> f = [&](vvi &e) {
             int n = e.size() + 1;
             vvi adj(n);
-            vi deg(n);
-            for (auto v : e) {
-                adj[v[0]].push_back(v[1]);
-                adj[v[1]].push_back(v[0]);
-                deg[v[0]]++;
-                deg[v[1]]++;
+            for (auto uv : e) {
+                int u = uv[0], v = uv[1];
+                adj[u].push_back(v);
+                adj[v].push_back(u);
             }
-            queue<int> q;
+
+            vi dep(n);
+            function<void(int,int,int)> g = [&] (int u, int p, int d) {
+                dep[u] = d;
+                for (auto v :adj[u]) {
+                    if (v==p) continue;
+                    g(v,u,d+1);
+                }
+            };
+            g(0,-1,0);
+            int mx = -1, root = -1;
             for (int i=0; i<n; i++) {
-                if (deg[i] == 1) q.push(i);
-            }
-            int ret = -1;
-            while (q.size()) {
-                auto u = q.front();
-                q.pop();
-                ret = u;
-                for (auto v : adj[u]) {
-                    if (--deg[v] == 1) {
-                        q.push(v);
-                    }
+                if (mx < dep[i]) {
+                    mx = dep[i];
+                    root = i;
                 }
             }
+            dep = vi(n);
+            g(root,-1,0);
+
+            int ret = *max_element(dep.begin(), dep.end());
             return ret;
         };
-        auto c1 = (e1.empty()) ? 0 : f(e1);
-        auto c2 = (e2.empty()) ? 0 : f(e2);
-
-        for (auto uv : e2) {
-            int u = uv[0], v = uv[1];
-            u += n, v += n;
-            e1.push_back({u,v});
-        }
-        e1.push_back({c1,c2+n});
-        
-        n += m;
-        vvi adj(n);
-        for (auto uv : e1) {
-            int u = uv[0], v = uv[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        vi dep(n);
-        function<void(int,int,int)> g = [&] (int u, int p, int d) {
-            dep[u] = d;
-            for (auto v : adj[u]) {
-                if (v == p) continue;
-                g(v,u,d+1);
-            }
-        };
-        g(0,-1,0);
-        int mx = 0, root = -1;
-        for (int i=0; i<n; i++) {
-            if (dep[i] > mx) {
-                mx = dep[i];
-                root = i;
-            }
-        }
-        dep.assign(n,0);
-        g(root,-1,0);
-        int ans = 0;
-        for (int i=0; i<n; i++) {
-            ans = max(ans, dep[i]);
-        }
+        int d1 = f(e1), d2 = f(e2);
+        int ans = (d1+1)/2 + (d2+1)/2 + 1;
+        ans = max({ans,d1,d2});
         return ans;
+
     }
 };
