2b4bf5f17ae1fc2e066c9c1747edcf899a89ddae
Martin Yang <martin0327@gmail.com>
2024-07-08 10:59:18 +0900

    Time: 3 ms (65.36%), Space: 7.2 MB (62.20%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> a;
-        for (int i=0; i<n; i++) a.push_back(i);
-        
-        for (int i=0;;) {
-            int m = a.size();
-            if (m==1) return a[0]+1;
-            i = (i+k-1) % m;
-            a.erase(a.begin() + i);
+        int ans = 0;
+        for (int i=1; i<=n; i++) {
+            ans = (ans+k) % i;
         }
-        return -1;
+        return ans+1;
     }
 };
