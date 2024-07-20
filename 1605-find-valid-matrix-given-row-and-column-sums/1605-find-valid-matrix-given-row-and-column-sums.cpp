7a7c2dc034212a71267fb0871425e334b35f4189
Martin Yang <martin0327@gmail.com>
2024-07-20 13:34:11 +0900

    Time: 32 ms (85.33%), Space: 36.1 MB (11.51%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,16 +1,23 @@
 using vi = vector<int>;
 using vvi = vector<vi>;
+
 class Solution {
 public:
-  vector<vector<int>> restoreMatrix(vector<int>& r, vector<int>& c) {
-    int n = r.size(), m = c.size(), i=0, j=0;
-    vvi ret(n, vi(m));
-    while (i<n && j<m) {
-      ret[i][j] = min(r[i],c[j]);
-      if (r[i] < c[j]) c[j] -= r[i++];
-      else r[i] -= c[j++];
+    vector<vector<int>> restoreMatrix(vector<int>& rs, vector<int>& cs) {
+        int n = rs.size(), m = cs.size();
+        vvi a(n, vi(m));
+        vi b(m);
+        for (int i=0; i<n; i++) {
+            int x = rs[i];
+            for (int j=0; j<m; j++) {
+                int d = min(x,cs[j]-b[j]);
+                if (d > 0) {
+                    x -= d;
+                    b[j] += d;
+                    a[i][j] = d;
+                }
+            }
+        }
+        return a;
     }
-    return ret;
-  }
 };
-
