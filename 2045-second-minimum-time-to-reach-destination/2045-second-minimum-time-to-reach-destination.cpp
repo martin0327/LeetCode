8681595c1d249dc2a106a1b26a319b7a87b65a95
Martin Yang <martin0327@gmail.com>
2024-07-29 08:46:36 +0900

    Update 2045-second-minimum-time-to-reach-destination.cpp

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
@@ -1,60 +1,61 @@
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using ti3 = tuple<ll,ll,ll>;
+const ll inf = 2e18;
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
+    return a;
+}
+
 class Solution {
 public:
-    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        // if the shortest path from 1 to n is of length L
-        // find whether there is a path of length L+1
-        // there is always a path of length L+2
-        
-        vector<vector<int>> adj(n);
-        for (auto& e : edges) {
-            int u = e[0]-1, v = e[1]-1;
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int ch) {
+        vvi adj(n);
+        for (auto e : edges) {
+            int u = e[0], v = e[1];
+            u--; v--;
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         
-        // bfs from goal
-        vector<int> d(n, 1e9);
-        d[n-1] = 0;
-        queue<int> q;
-        q.push(n-1);
-        while (!q.empty()) {
-            int cur = q.front(); q.pop();
-            for (auto nei : adj[cur]) {
-                if (d[nei] == 1e9) {
-                    d[nei] = d[cur] + 1;
-                    q.push(nei);
+        min_pq<ti3> pq;
+        pq.emplace(0,0,0);
+
+        vvi dist(n, vi(2,inf));
+        dist[0][0] = 0;
+
+        vi res;
+        while (pq.size()) {
+            auto [d,u,fs] = pq.top();
+            pq.pop();
+            if (d > dist[u][fs])  continue;
+
+            for (auto v : adj[u]) {
+                ll nd = d + time;
+                if (v == n-1) res.push_back(nd);
+                
+                ll q = nd / ch;
+                if (q&1) nd = (q+1)*ch;
+                if (dist[v][0] > nd) {
+                    pq.emplace(dist[v][0]=nd,v,0);
+                }
+                else if (dist[v][0] < nd && nd < dist[v][1]) {
+                    pq.emplace(dist[v][1]=nd,v,1);
                 }
             }
         }
+        res = get_unique(res);
+        ll ans = res[1];
+        return ans;
         
-        // check the existence of a path with length = d[0]+1
-        int len = d[0] + 2;
-        q.push(0);
-        bool done = false;
-        while (!q.empty()) {
-            int cur = q.front(); q.pop();
-            for (auto nei : adj[cur]) {
-                if (d[nei] == d[cur]) {
-                    len--;
-                    done = true;
-                    break;
-                } else if (d[nei] == d[cur] - 1) {
-                    q.push(nei);
-                }
-            }
-            if (done) break;
-        }
-        
-        // calculate the time needed
-        // light : green in [0, c),  [2c, 3c), ... 
-        //          red  in [c, 2c), [3c, 4c), ...
-        int currTime = 0;
-        //cout << len << '\n';
-        for (int i = 0; i < len; i++) {
-			if ((currTime / change) % 2 == 1)  // have to wait until the signal turns into green
-                currTime = ((currTime / change) + 1) * change;    
-            currTime += time;
-        }
-        return currTime;
     }
 };
