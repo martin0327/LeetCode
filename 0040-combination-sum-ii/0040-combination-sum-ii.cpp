a6b8f8ff62c898ee9ec518af1d4e07078d7762e9
Martin Yang <martin0327@gmail.com>
2024-08-13 09:56:17 +0900

    Time: 17 ms (11.43%), Space: 20 MB (7.57%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -5,31 +5,32 @@ class Solution {
 public:
     vvi dp[31][31];
     int check[31][31];
-    vector<vector<int>> combinationSum2(vector<int>& a, int tg) {
-        vector<int> cnt(tg+1);
+    vector<vector<int>> combinationSum2(vector<int>& a, int k) {
+        vector<int> cnt(k+1);
         for (auto x : a) {
-            if (x <= tg) cnt[x]++;
+            if (x <= k) cnt[x]++;
         }
+        
         function<vvi(int,int)> f = [&] (int i, int j) {
             vvi ret;
             if (j == 0) return dp[i][j] = ret = {{}};
-            if (i > tg || i > j) {
+            if (i > k || i > j) {
                 return ret;
             }
             for (int x=0; x<=cnt[i]; x++) {
                 auto vv = f(i+1,j-x*i);
-                for (auto p : vv) {
-                    int mul = x;
-                    while (mul--) {
-                        p.push_back(i);
+                for (auto v : vv) {
+                    int m = x;
+                    while (m--) {
+                        v.push_back(i);
                     }
-                    ret.push_back(p);
+                    ret.push_back(v);
                 }
             }
             return dp[i][j] = ret;
         };
-
-        vvi ret = f(0,tg);
+        
+        vvi ret = f(0,k);
         return ret;
     }
 };
