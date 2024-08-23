7ea1c91fd2a7bfb01c04b35850fc494fe1214d90
Martin Yang <martin0327@gmail.com>
2024-08-23 14:44:00 +0900

    Time: 34 ms (69.77%), Space: 16.6 MB (26.36%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -1,8 +1,7 @@
-import re
 from fractions import Fraction
 class Solution:
     def fractionAddition(self, s: str) -> str:
         s = s.replace('-', '+-').split('+')
-        s = [x.split('/') for x in s if len(x) > 1]
-        s = sum(Fraction(int(x),int(y)) for x,y in s)
+        s = [map(int,x.split('/')) for x in s if len(x) > 1]
+        s = sum(Fraction(x,y) for x,y in s)
         return "{}/{}".format(s.numerator,s.denominator)
