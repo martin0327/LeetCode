222de1696e4d7e145b7dfd0a05f0fe3bfdfe61c6
Martin Yang <martin0327@gmail.com>
2024-09-03 21:45:27 +0900

    Time: 666 ms (57.43%), Space: 115.6 MB (80.20%) - LeetHub

---
+++ 3275-k-th-nearest-obstacle-queries/3275-k-th-nearest-obstacle-queries.js
@@ -0,0 +1,92 @@
+class MaxPQ {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(val) {
+        this.heap.push(val);
+        this._heapifyUp();
+    }
+
+    pop() {
+        if (this.size() === 1) {
+            return this.heap.pop();
+        }
+        const top = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this._heapifyDown();
+        return top;
+    }
+
+    top() {
+        return this.heap[0];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    _heapifyUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index] <= this.heap[parentIndex]) break;
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            index = parentIndex;
+        }
+    }
+
+    _heapifyDown() {
+        let index = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) break;
+            [this.heap[index], this.heap[swap]] = [this.heap[swap], this.heap[index]];
+            index = swap;
+        }
+    }
+}
+
+/**
+ * @param {number[][]} queries
+ * @param {number} k
+ * @return {number[]}
+ */
+var resultsArray = function(qr, k) {
+    const pq = new MaxPQ();
+    const ans = [];
+
+    for (const v of qr) {
+        const d = Math.abs(v[0]) + Math.abs(v[1]);
+        pq.push(d);
+        while (pq.size() > k) pq.pop();
+        ans.push(pq.size() === k ? pq.top() : -1);
+    }
+
+    return ans;
+};
+
