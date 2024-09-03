5dd7878b65e020852a5566699c18981f460ebd00
Martin Yang <martin0327@gmail.com>
2024-09-03 21:14:26 +0900

    Time: 572 ms (64.82%), Space: 256.3 MB (83.67%) - LeetHub

--- 3275-k-th-nearest-obstacle-queries/3275-k-th-nearest-obstacle-queries.cpp
+++ 3275-k-th-nearest-obstacle-queries/3275-k-th-nearest-obstacle-queries.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> resultsArray(vector<vector<int>>& qr, int k) {
         priority_queue<int> pq;
         vector<int> ans;
-        for (auto v : qr) {
+        for (auto &v : qr) {
             int d = abs(v[0])+abs(v[1]);
             pq.push(d);
             while (pq.size() > k) pq.pop();
