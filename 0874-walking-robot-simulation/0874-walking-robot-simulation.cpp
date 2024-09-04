b645d81104a93d8135f233d66fe985f9c9cdc91b
Martin Yang <martin0327@gmail.com>
2024-09-05 01:32:07 +0900

    Time: 75 ms (61.60%), Space: 39.8 MB (53.87%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,35 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int,int>;
+
+vi dx = {0,1,0,-1};
+vi dy = {1,0,-1,0};
+
+class Solution {
+public:
+    int robotSim(vector<int>& comm, vector<vector<int>>& obs) {
+        int d = 0, x = 0, y = 0, ans = 0;
+        set<pii> s;
+        for (auto v : obs) {
+            s.insert({v[0],v[1]});
+        }
+        for (auto k : comm) {
+            if (k == -1) {
+                d = (d+1) % 4;
+            }
+            else if (k == -2) {
+                d = (d+3) % 4;
+            }
+            else {
+                while (k--) {
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+                    if (s.count({nx,ny})) break;
+                    x = nx, y = ny;
+                }
+                ans = max(ans, x*x+y*y);
+            }
+        }
+        return ans;
+    }
+};
