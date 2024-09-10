6674b44b4fee04872b5af34b89fd10adb6ef806f
Martin Yang <martin0327@gmail.com>
2024-09-10 21:10:39 +0900

    Time: 193 ms (82.90%), Space: 171.4 MB (71.76%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
@@ -1,13 +1,12 @@
 using ll = long long;
-
 class Solution {
 public:
     long long findMaximumScore(vector<int>& a) {
-        ll n = a.size();
-        vector<ll> b(n, a.front());
-        for (int i=1; i<n; i++) {
-            b[i] = max((ll) a[i], b[i-1]);
+        ll cur = 0, ans = 0;
+        for (ll x : a) {
+            cur = max(cur, x);
+            ans += cur;
         }
-        return accumulate(b.begin(), b.end()-1, 0ll);
+        return ans - cur;
     }
 };
