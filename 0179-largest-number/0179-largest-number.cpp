0612b14e7f91dba0382cfb99850eb19732cbc37e
Martin Yang <martin0327@gmail.com>
2024-09-18 09:35:55 +0900

    Time: 8 ms (48.02%), Space: 17.4 MB (36.02%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -2,27 +2,12 @@ class Solution {
 public:
     string largestNumber(vector<int>& a) {
         vector<string> b;
-        for (auto x : a) {
-            auto s = to_string(x);
-            int l = s.size();
-            b.push_back(s);
-        }
+        for (auto &x : a) b.push_back(to_string(x));
         int n = b.size();
-        // sort(b.rbegin(), b.rend());
-        sort(b.begin(), b.end(), [&] (string s, string t) {
-            auto st = s + t;
-            auto ts = t + s;
+        sort(b.begin(), b.end(), [&] (string &s, string &t) {
+            string st = s + t, ts = t + s;
             return st > ts;
         });
-        // for (int i=0; i<n; i++) {
-        //     for (int j=i+1; j<n; j++) {
-        //         auto s = b[i];
-        //         auto t = b[j];
-        //         string st = s + t;
-        //         string ts = t + s;
-        //         if (st < ts) swap(b[i],b[j]);
-        //     }
-        // }
         string ans;
         for (auto s : b) ans += s;
         set<char> sc(ans.begin(), ans.end());
