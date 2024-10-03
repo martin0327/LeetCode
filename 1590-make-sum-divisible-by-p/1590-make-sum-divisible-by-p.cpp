8cb7dec910c264e59ca70044d452752536a49c59
Martin Yang <martin0327@gmail.com>
2024-10-03 12:19:27 +0900

    Time: 198 ms (24.94%), Space: 75.6 MB (8.48%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,33 @@
+using ll = long long;
+using vi = vector<ll>;
+const ll inf = 1e9;
+
+class Solution {
+public:
+    int minSubarray(vector<int>& a, int p) {
+        ll n = a.size();
+        ll s = accumulate(a.begin(), a.end(), 0ll);
+        s %= p;
+        ll ret = 0;
+        if (s == 0) return ret;
+
+        vi pre(n+1);
+        for (ll i=1; i<=n; i++) {
+            pre[i] = pre[i-1] + a[i-1];
+            pre[i] %= p;
+        }
+        map<ll,ll> mp;
+        ret = inf;
+        for (ll i=0; i<=n; i++) {
+            ll t = pre[i] - s + p;
+            t %= p;
+            if (mp.count(t)) {
+                ll j = mp[t];
+                ret = min(ret, i-j);
+            }
+            mp[pre[i]] = i;
+        }
+        if (ret >= n) ret = -1;
+        return ret;
+    }
+};
