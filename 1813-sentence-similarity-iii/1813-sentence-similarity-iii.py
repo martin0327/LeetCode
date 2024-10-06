c4f0b37bb81acf7b7e7ea4540892a35d158c0b1c
Martin Yang <martin0327@gmail.com>
2024-10-06 16:55:46 +0900

    Time: 30 ms (86.96%), Space: 16.6 MB (47.21%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -5,13 +5,11 @@ class Solution:
         if len(a) < len(b):
             a, b = b, a
         n, m = len(a), len(b)
-        # print(a,b)
         for i in range(n+1):
             if i >= m: break
             c = a[:i] + a[-(m-i):]
             if c == b: return True
-            d = a[:(m-(i))] + (a[-(m-i):] if i > 0 else [])
-            # print(c,d)
+            d = a[:(m-i)] + (a[-(n-i):] if i > 0 else [])
             if d == b: return True
             
         return False
