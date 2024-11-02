94e7aa610acacdda43cd840b7f515fa495b3be9d
Martin Yang <martin0327@gmail.com>
2024-11-02 21:35:49 +0900

    Time: 1 ms (16.54%), Space: 16.6 MB (45.68%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.py
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -2,9 +2,7 @@ class Solution:
     def isCircularSentence(self, s: str) -> bool:
         a = s.split()
         n = len(a)
-        ok = True
         for i in range(n):
-            x = a[i]
-            y = a[(i+1)%n]
-            if x[-1] != y[0]: ok = False
-        return ok
+            x, y = a[i], a[(i+1)%n]
+            if x[-1] != y[0]: return False
+        return True
