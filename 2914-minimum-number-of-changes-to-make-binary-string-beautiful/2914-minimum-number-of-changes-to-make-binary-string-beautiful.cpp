8f12c54f20fc54beb538a51835f1a6cbfeabbd0e
Martin Yang <martin0327@gmail.com>
2024-11-05 09:20:32 +0900

    Time: 4 ms (30.85%), Space: 12.1 MB (87.26%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        int n = s.size();
+        int ans = 0;
+        for (int i=0; i<n; i+=2) {
+            ans += s[i] != s[i+1];
+        }
+        return ans;
+    }
+};
