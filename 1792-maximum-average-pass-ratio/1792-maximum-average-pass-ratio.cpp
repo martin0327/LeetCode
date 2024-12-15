6ee2a492c3f6a742e5f19e58da789823d0c054d6
Martin Yang <martin0327@gmail.com>
2024-12-16 01:21:34 +0900

    Time: 380 ms (44.44%), Space: 99.1 MB (39.10%) - LeetHub

---
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -0,0 +1,34 @@
+using ll = long long;
+using tdii = tuple<double,int,int>;
+
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& a, int ex) {
+        priority_queue<tdii,vector<tdii>, greater<tdii>> pq;
+        auto f = [&] (ll x, ll y) {
+            double w = (y+1)*x - y*(x+1);
+            w /= (y+1)*y;
+            return w;            
+        };
+        for (auto &pt : a) {
+            int x = pt[0], y = pt[1];
+            pq.push({f(x,y),x,y});
+        }
+        while (ex--) {
+            auto [w,x,y] = pq.top();
+            pq.pop();
+            pq.push({f(x+1,y+1),x+1,y+1});
+        }
+        double ans = 0;
+        while (pq.size()) {
+            auto [w,x,y] = pq.top();
+            pq.pop();
+            double t = x;
+            t /= y;
+            ans += t;
+        }
+        ans /= a.size();
+        return ans;
+        
+    }
+};
