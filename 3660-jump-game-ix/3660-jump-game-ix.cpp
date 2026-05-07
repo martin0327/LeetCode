26b6e74da41ad6c15a6d432c68e9dfc18057f11c
Martin Yang <martin0327@gmail.com>
2026-05-07 23:53:38 +0900

    Time: 620 ms (5.32%), Space: 306.4 MB (5.32%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -0,0 +1,205 @@
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() const { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+
+template<typename T1, typename T2>
+void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template<typename T1, typename T2>
+void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T>
+void asort(vector<T> &a) {sort(a.begin(), a.end());}
+template<typename T>
+void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
+template<typename T>
+void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
+
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
+    return a;
+}
+
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using pii = pair<ll,ll>;
+using vp = vector<pii>;
+using ti3 = tuple<ll,ll,ll>;
+using vti3 = vector<ti3>;
+using vs = vector<string>;
+
+const ll inf = 2e9;
+using S = ll;
+S op(S x, S y) { return min(x,y); }
+S e() { return inf; }
+
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& aa) {
+        vi a(aa.begin(), aa.end());
+        int n = a.size();
+        auto c = get_unique(a);
+        auto g = [&] (int x) {
+            auto it = lower_bound(c.begin(), c.end(), x);
+            return it - c.begin();
+        };
+
+        for (auto &x : a) x = g(x);
+        segtree<S,op,e> seg(a);
+        vp b(n);
+        for (int i=0; i<n; i++) {
+            b[i] = {a[i],i};
+        }
+        dsort(b);
+        vi ans(n);
+        int hi = inf, lo = inf;
+        int l = n;
+        // debug(a);
+        for (auto [x,i] : b) {
+            if (i > l || x > lo) {
+                ans[i] = hi;
+                if (i < l) {
+                    int t = seg.prod(i,n);
+                    if (t < x) {
+                        lo = t;
+                        l = i;
+                    }
+                }
+            }
+            else {
+                int t = seg.prod(i,n);
+                if (t == x) {
+                    ans[i] = x;
+                }
+                else {
+                    hi = x;
+                    lo = t;
+                    l = i;
+                    ans[i] = hi;
+                }
+            }
+            // debug(x,hi,lo,l);
+        }
+        // debug(ans);
+
+        vector<int> res(n);
+        for (int i=0; i<n; i++) {
+            res[i] = c[ans[i]];
+        }
+        return res;
+        
+    }
+};
