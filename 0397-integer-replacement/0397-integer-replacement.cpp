3410b2d1547c2618153973662ae47d9f35d9aa9b
Martin Yang <martin0327@gmail.com>
2026-05-09 13:51:50 +0900

    Time: 0 ms (100%), Space: 8.2 MB (28.14%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -6,23 +6,17 @@ public:
         queue<ll> q;
         q.push(n);
         vis[n] = 0;
+        vector<ll> nb; nb.reserve(2);
         while (q.size()) {
-            ll u = q.front();
-            q.pop();
+            ll u = q.front(); q.pop();
             if (u == 1) return vis[u];
-            if (u&1) {
-                for (ll v : {u-1,u+1}) {
-                    if (vis.count(v)) continue;
-                    vis[v] = vis[u] + 1;
-                    q.push(v);
-                }
-            }
-            else {
-                ll v = u / 2;
-                if (!vis.count(v)) {
-                    vis[v] = vis[u] + 1;
-                    q.push(v);
-                }
+            nb = {u/2};
+            if (u&1) nb = {u-1,u+1};
+            
+            for (auto v : nb) {
+                if (vis.count(v)) continue;
+                vis[v] = vis[u] + 1;
+                q.push(v);
             }
         }
         return -1;
