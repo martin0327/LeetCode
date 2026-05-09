854eb3f1463d86481fd181536f8e80220a7b7d8f
Martin Yang <martin0327@gmail.com>
2026-05-09 22:16:13 +0900

    Time: 90 ms (7.01%), Space: 50.1 MB (5.06%) - LeetHub

---
+++ 0391-perfect-rectangle/0391-perfect-rectangle.cpp
@@ -0,0 +1,347 @@
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+namespace atcoder {
+
+namespace internal {
+
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class S,
+          S (*op)(S, S),
+          S (*e)(),
+          class F,
+          S (*mapping)(F, S),
+          F (*composition)(F, F),
+          F (*id)()>
+struct lazy_segtree {
+  public:
+    lazy_segtree() : lazy_segtree(0) {}
+    lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
+    lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        lz = std::vector<F>(size, id());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return e();
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push(r >> i);
+        }
+
+        S sml = e(), smr = e();
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
+    }
+
+    S all_prod() { return d[1]; }
+
+    void apply(int p, F f) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = mapping(f, d[p]);
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return;
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        {
+            int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
+
+    template <bool (*g)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return g(x); });
+    }
+    template <class G> int max_right(int l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (int i = log; i >= 1; i--) push(l >> i);
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*g)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return g(x); });
+    }
+    template <class G> int min_left(int r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+    std::vector<F> lz;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, F f) {
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id();
+    }
+};
+
+}  // namespace atcoder
+
+using namespace atcoder;
+
+
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+
+template<typename T1, typename T2>
+void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template<typename T1, typename T2>
+void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T>
+void asort(vector<T> &a) {sort(a.begin(), a.end());}
+template<typename T>
+void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
+template<typename T>
+void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
+
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
+    return a;
+}
+
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using pii = pair<ll,ll>;
+using vp = vector<pii>;
+using vvp = vector<vp>;
+using ti3 = tuple<ll,ll,ll>;
+using vti3 = vector<ti3>;
+using vs = vector<string>;
+
+const ll inf = 2e9;
+using S = pii;
+using F = ll;
+
+S op(S a, S b) {
+    auto [x,u] = a;
+    auto [y,v] = b;
+    return {max(x,y),min(u,v)};
+}
+ 
+S e() {
+    return {-inf,inf};
+}
+ 
+S mapping(F f, S s) {
+    auto [a,b] = s;
+    return {a+f, b+f};
+}
+ 
+F composition(F f, F g) {
+    return f + g;
+}
+
+F id () {
+    return 0;
+}
+
+
+
+class Solution {
+public:
+    bool isRectangleCover(vector<vector<int>>& rt) {
+        vi xs, ys;
+        map<pii,vp> mp;
+        for (auto &v : rt) {
+            int x1 = v[0], y1 = v[1];
+            int x2 = v[2], y2 = v[3];
+            xs.push_back(x1);
+            xs.push_back(x2);
+            ys.push_back(y1);
+            ys.push_back(y2);
+            mp[{x1,1}].push_back({y1,y2});
+            mp[{x2,0}].push_back({y1,y2});
+        }
+        xs = get_unique(xs);
+        ys = get_unique(ys);
+        int xsz = xs.size();
+        int ysz = ys.size();
+        auto g = [&] (vi &a, int x) {
+            auto it = lower_bound(a.begin(), a.end(), x);
+            return it - a.begin();
+        };
+        
+        vvp xpush(xsz), xpop(xsz);
+        for (auto &[k,v] : mp) {
+            auto [x,pp] = k;
+            x = g(xs,x);
+            for (auto [lo,hi] : v) {
+                lo = g(ys,lo);
+                hi = g(ys,hi);
+                if (pp) xpush[x].push_back({lo,hi});
+                else xpop[x].push_back({lo,hi});
+            }
+        }
+        // debug(xpush);
+        // debug(xpop);
+        lazy_segtree<S, op, e, F, mapping, composition, id> seg(vp(ysz, {0,0}));
+        
+        // auto dbg = [&] () {
+        //     vp a;
+        //     for (int i=0; i<ysz; i++) {
+        //         a.push_back(seg.get(i));
+        //     }
+        //     debug(a);
+        // };
+        bool ans = true;
+        for (int i=0; i<xsz; i++) {
+            for (auto [lo,hi] : xpush[i]) {
+                seg.apply(lo,hi,1);
+            }
+            for (auto [lo,hi] : xpop[i]) {
+                seg.apply(lo,hi,-1);
+            }
+            auto [mx,mn] = seg.prod(0,ysz-1);
+            // debug(i,mx,mn);
+            // dbg();
+            if (i+1 == xsz) {
+                ans &= (mx == 0 && mn == 0);
+            }
+            else {
+                ans &= (mx == 1 && mn == 1);
+            }
+        }
+        return ans;
+    }
+};
