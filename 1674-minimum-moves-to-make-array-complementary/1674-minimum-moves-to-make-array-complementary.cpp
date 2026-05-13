9e32ac68720eec254ec854f9b30eccd405f022b0
Martin Yang <martin0327@gmail.com>
2026-05-14 00:19:44 +0900

    Time: 9 ms (57.52%), Space: 85.9 MB (97.35%) - LeetHub

---
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -0,0 +1,34 @@
+const int sz = 2e5+5;
+int b[sz];
+class Solution {
+public:
+    int minMoves(vector<int>& a, int lim) {
+        int n = a.size(), m = 2*lim+2;
+        memset(b,0,sizeof(b));
+        for (int i=0; i<n; i++) {
+            int j = n-1-i;
+            if (i>=j) break;
+            auto x = a[i], y = a[j];
+            int l = min(x,y)+1;
+            int r = max(x,y)+lim;
+            b[l]--;
+            b[r+1]++;
+            b[1] += 2;
+        }
+        for (int i=2; i<m; i++) {
+            b[i] += b[i-1];
+        }
+        for (int i=0; i<n; i++) {
+            int j = n-1-i;
+            if (i >= j) break;
+            auto x = a[i], y = a[j];
+            b[x+y]--;
+        }
+
+        int ans = 2e9;
+        for (int i=2; i<=2*lim; i++) 
+            ans = min(ans, b[i]);
+
+        return ans;
+    }
+};
