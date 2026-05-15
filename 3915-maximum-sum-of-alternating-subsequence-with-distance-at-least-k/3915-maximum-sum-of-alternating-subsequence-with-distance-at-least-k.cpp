7b1233ad998a2cd8ce441180fbb818356bdfef61
Martin Yang <martin0327@gmail.com>
2026-05-16 07:05:29 +0900

    Time: 2179 ms (22.55%), Space: 327.6 MB (34.14%) - LeetHub

---
+++ 3915-maximum-sum-of-alternating-subsequence-with-distance-at-least-k/3915-maximum-sum-of-alternating-subsequence-with-distance-at-least-k.cpp
@@ -0,0 +1,146 @@
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() const { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+template<typename T1, typename T2>
+void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template<typename T1, typename T2>
+void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+using ll = long long;
+using vi = vector<ll>;
+
+using S = ll;
+const ll inf = 2e18;
+S op(S x, S y) { return max(x,y); }
+S e() { return -inf; }
+using segt = segtree<S,op,e>;
+
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& aa, int k) {
+        vi a(aa.begin(), aa.end());
+        int n = a.size(), sz = *max_element(a.begin(), a.end()) + 1;
+        segt seg_lo(sz), seg_hi(sz);
+        vi dp_lo(a), dp_hi(a);
+        for (int i=0; i<n; i++) {
+            if (i >= k) {
+                auto y = a[i-k];
+                seg_lo.set(y, max(seg_lo.get(y), dp_lo[i-k]));
+                seg_hi.set(y, max(seg_hi.get(y), dp_hi[i-k]));
+            }
+
+            ll x = a[i];
+            chmax(dp_lo[i], seg_hi.prod(x+1,sz) + x);
+            chmax(dp_hi[i], seg_lo.prod(0,x) + x);
+        }
+        auto ans = *max_element(dp_lo.begin(), dp_lo.end());
+        chmax(ans,*max_element(dp_hi.begin(), dp_hi.end()));
+        return ans;
+    }
+};
