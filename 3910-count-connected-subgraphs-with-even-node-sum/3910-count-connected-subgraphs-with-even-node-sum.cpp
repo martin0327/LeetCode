9f230e57fa336f1d30d8163d961919dff1b3cb33
Martin Yang <martin0327@gmail.com>
2026-05-17 10:44:53 +0900

    Time: 83 ms (80.62%), Space: 64.9 MB (84.2%) - LeetHub

---
+++ 3910-count-connected-subgraphs-with-even-node-sum/3910-count-connected-subgraphs-with-even-node-sum.cpp
@@ -0,0 +1,83 @@
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    std::vector<int> parent_or_size;
+};
+
+class Solution {
+public:
+    int evenSumSubgraphs(vector<int>& a, vector<vector<int>>& edg) {
+        int n = a.size(), ans = 0;
+        for (int mask=1; mask<(1<<n); mask++) {
+            dsu uf(n);
+            int m = __builtin_popcount(mask);
+            for (auto &uv : edg) {
+                int u = uv[0], v = uv[1];
+                if ((mask>>u&1) && (mask>>v&1)) {
+                    uf.merge(u,v);
+                }
+            }
+            int s = 0,  src = -1;
+            for (int i=0; i<n; i++) {
+                if (mask>>i&1) {
+                    s += a[i];
+                    src = i;
+                }
+            }
+            if (uf.size(src) == m && s % 2 == 0) ans++;
+        }
+        return ans;
+    }
+};
