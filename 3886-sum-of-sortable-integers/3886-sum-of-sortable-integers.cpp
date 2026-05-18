d3a3e6120a58e28076ac8e63c024408d80b6e270
Martin Yang <martin0327@gmail.com>
2026-05-19 01:07:12 +0900

    Time: 85 ms (62.21%), Space: 88.4 MB (77.44%) - LeetHub

---
+++ 3886-sum-of-sortable-integers/3886-sum-of-sortable-integers.cpp
@@ -0,0 +1,60 @@
+using vi = vector<int>;
+class Solution {
+public:
+    int sortableIntegers(vector<int>& b) {
+        int n = b.size();
+        vi divs;
+        for (int i=1; i*i<=n; i++) {
+            if (n % i == 0) {
+                divs.push_back(i);
+                if (i*i != n) divs.push_back(n/i);
+            }
+        }
+        int ans = 0;
+        for (auto d : divs) {
+            bool ok = true;
+            int pmax = -1;
+            for (int i=0; i<n; i+=d) {
+                auto [it1,it2] = minmax_element(b.begin()+i, b.begin()+i+d);
+                int mn = *it1, mx = *it2;
+                if (mn < pmax) {
+                    ok = false;
+                    break;
+                }
+                if (mn != mx) {
+                    int idx = -1;
+                    if (b[i+d-1] == mx) {
+                        idx = d-1;
+                        for (int j=0; j<d; j++) {
+                            if (b[i+j] == mx) {
+                                idx = j;
+                            }
+                            else break;
+                        }
+                    }
+                    else {
+                        for (int j=d-1; j>=0; j--) {
+                            if (b[i+j] == mx) {
+                                idx = j;
+                                break;
+                            }
+                        }
+                    }
+                    assert(idx != -1);
+                    for (int j=0; j+1<d; j++) {
+                        int j2 = (idx-j+d) % d;
+                        int j1 = (idx-j-1+d) % d;
+                        if (b[i+j1] > b[i+j2]) {
+                            ok = false;
+                            break;
+                        }
+                    }
+                }
+                if (!ok) break;
+                pmax = mx;
+            }
+            if (ok) ans += d;
+        }
+        return ans;
+    }
+};
