30ea86d232c3eaec154a7bdec94f939dbaa8e313
Martin Yang <martin0327@gmail.com>
2026-05-19 01:16:20 +0900

    Time: 81 ms (64.36%), Space: 88.3 MB (93.01%) - LeetHub

--- 3886-sum-of-sortable-integers/3886-sum-of-sortable-integers.cpp
+++ 3886-sum-of-sortable-integers/3886-sum-of-sortable-integers.cpp
@@ -1,8 +1,8 @@
 using vi = vector<int>;
 class Solution {
 public:
-    int sortableIntegers(vector<int>& b) {
-        int n = b.size();
+    int sortableIntegers(vector<int>& a) {
+        int n = a.size(), ans = 0;
         vi divs;
         for (int i=1; i*i<=n; i++) {
             if (n % i == 0) {
@@ -10,12 +10,11 @@ public:
                 if (i*i != n) divs.push_back(n/i);
             }
         }
-        int ans = 0;
         for (auto d : divs) {
             bool ok = true;
             int pmax = -1;
             for (int i=0; i<n; i+=d) {
-                auto [it1,it2] = minmax_element(b.begin()+i, b.begin()+i+d);
+                auto [it1,it2] = minmax_element(a.begin()+i, a.begin()+i+d);
                 int mn = *it1, mx = *it2;
                 if (mn < pmax) {
                     ok = false;
@@ -23,10 +22,10 @@ public:
                 }
                 if (mn != mx) {
                     int idx = -1;
-                    if (b[i] == mn) {
+                    if (a[i] == mn) {
                         idx = 0;
                         for (int j=d-1; j>=0; j--) {
-                            if (b[i+j] == mn) {
+                            if (a[i+j] == mn) {
                                 idx = j;
                             }
                             else break;
@@ -34,7 +33,7 @@ public:
                     }
                     else {
                         for (int j=0; j<d; j++) {
-                            if (b[i+j] == mn) {
+                            if (a[i+j] == mn) {
                                 idx = j;
                                 break;
                             }
@@ -44,7 +43,7 @@ public:
                     for (int j=0; j+1<d; j++) {
                         int l = (idx+j) % d;
                         int r = (idx+j+1) % d;
-                        if (b[i+l] > b[i+r]) {
+                        if (a[i+l] > a[i+r]) {
                             ok = false;
                             break;
                         }
