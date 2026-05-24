c8a31edf3cd0fbbd316732d09cb26034db7b30e8
Martin Yang <martin0327@gmail.com>
2026-05-24 16:07:37 +0900

    Time: 1723 ms (56.78%), Space: 200.3 MB (81.02%) - LeetHub

--- 3943-number-of-pairs-after-increment/3943-number-of-pairs-after-increment.cpp
+++ 3943-number-of-pairs-after-increment/3943-number-of-pairs-after-increment.cpp
@@ -1,44 +1,15 @@
-
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
-
-template<typename T1, typename T2>
-void chmax(T1 &x, T2 y) { if (x < y) x = y; }
-template<typename T1, typename T2>
-void chmin(T1 &x, T2 y) { if (x > y) x = y; }
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
-template<typename T>
-void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
-
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    sort(a.begin(), a.end());
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
-
 using ll = long long;
-using vi = vector<ll>;
+using vi = vector<int>;
 using vvi = vector<vi>;
-using pii = pair<ll,ll>;
-using vp = vector<pii>;
-using vvp = vector<vp>;
-using ti3 = tuple<ll,ll,ll>;
-using vti3 = vector<ti3>;
-using vs = vector<string>;
-
 const int mx_sz = 250;
-unordered_map<int,int> frq[mx_sz];
-int offset[mx_sz];
 
 int ceil_div(int a, int b) {
-    return (a + b - 1) / b; // a / b + (a % b > 0)
+    return (a + b - 1) / b; // (a/b) + (a%b>0)
 }
+
+unordered_map<ll,ll> frq[mx_sz];
+ll offset[mx_sz];
+
 class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& a, vector<int>& b, vector<vector<int>>& qr) {
@@ -51,20 +22,20 @@ public:
         memset(offset, 0, sizeof(offset));
 
         for (int i=0; i<m; i++) {
-            ll bid = i / sz, x = b[i];
+            int bid = i / sz, x = b[i];
             frq[bid][x]++;
         }
 
         vector<int> ans;
         for (auto &t : qr) {
-            ll tp = t[0];
+            int tp = t[0];
             if (tp == 1) {
                 ll l = t[1], r = t[2], val = t[3];
                 auto l_id = l / sz;
                 auto r_id = r / sz;
                 if (l_id == r_id) {
                     for (int i=l; i<=r; i++) { 
-                        ll bid = l_id, x = b[i];
+                        int bid = l_id, x = b[i];
                         frq[bid][x]--;
                         frq[bid][x+val]++;
                         b[i] += val;
@@ -76,14 +47,14 @@ public:
                     }
                     for (int i=l_id*sz; i<(l_id+1)*sz; i++) {
                         if (i < l || i >= m) continue;
-                        ll bid = l_id, x = b[i];
+                        int bid = l_id, x = b[i];
                         frq[bid][x]--;
                         frq[bid][x+val]++;
                         b[i] += val;
                     }
                     for (int i=r_id*sz; i<(r_id+1)*sz; i++) {
                         if (i > r || i < 0) continue;
-                        ll bid = r_id, x = b[i];
+                        int bid = r_id, x = b[i];
                         frq[bid][x]--;
                         frq[bid][x+val]++;
                         b[i] += val;
@@ -105,7 +76,6 @@ public:
                 ans.push_back(cnt);
             }
         }
-
         return ans;
     }
 };
