e15e70774e898eba1a3a9baf3ccd5adbe182bbee
Martin Yang <martin0327@gmail.com>
2026-05-24 17:12:21 +0900

    Time: 1726 ms (56.78%), Space: 200.2 MB (81.02%) - LeetHub

--- 3943-number-of-pairs-after-increment/3943-number-of-pairs-after-increment.cpp
+++ 3943-number-of-pairs-after-increment/3943-number-of-pairs-after-increment.cpp
@@ -1,10 +1,9 @@
 using ll = long long;
 using vi = vector<int>;
-using vvi = vector<vi>;
 const int mx_sz = 250;
 
 int ceil_div(int a, int b) {
-    return (a + b - 1) / b; // (a/b) + (a%b>0)
+    return (a + b - 1) / b;
 }
 
 unordered_map<ll,ll> frq[mx_sz];
@@ -14,8 +13,8 @@ class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& a, vector<int>& b, vector<vector<int>>& qr) {
         int n = b.size(), sz = sqrt(n);
-        int num_b = ceil_div(n, sz);
-        for (int i=0; i<num_b; i++) {
+        int m = ceil_div(n, sz);
+        for (int i=0; i<m; i++) {
             frq[i].clear();
         }
         memset(offset, 0, sizeof(offset));
@@ -25,45 +24,42 @@ public:
             frq[bid][x]++;
         }
 
-        vector<int> ans;
+        vi ans;
         for (auto &t : qr) {
             int tp = t[0];
             if (tp == 1) {
                 ll l = t[1], r = t[2], v = t[3];
-                auto left = l / sz;
-                auto right = r / sz;
+                int left = l / sz, right = r / sz;
                 if (left == right) {
                     for (int i=l; i<=r; i++) { 
-                        int bid = left, x = b[i];
+                        int bid = left, &x = b[i];
                         frq[bid][x]--;
-                        frq[bid][x+v]++;
-                        b[i] += v;
+                        frq[bid][x+=v]++;
                     }
                 }
                 else {
                     for (int bid=left+1; bid<right; bid++) {
                         offset[bid] += v;
                     }
-                    for (int i=0; i<sz; i++) {
-                        int j = left*sz+i;
-                        if (j < l || j >= n) continue;
-                        int bid = left, &x = b[j];
+                    for (int j=0; j<sz; j++) {
+                        int i = left*sz+j;
+                        if (i < l || i >= n) continue;
+                        int bid = left, &x = b[i];
                         frq[bid][x]--;
-                        x += v;
-                        frq[bid][x]++;
+                        frq[bid][x+=v]++;
                     }
-                    for (int i=right*sz; i<(right+1)*sz; i++) {
-                        if (i > r) continue;
-                        int bid = right, x = b[i];
+                    for (int j=0; j<sz; j++) {
+                        int i = right*sz+j;
+                        if (i > r) break;
+                        int bid = right, &x = b[i];
                         frq[bid][x]--;
-                        frq[bid][x+v]++;
-                        b[i] += v;
+                        frq[bid][x+=v]++;
                     }
                 }
             }
             else {
                 ll tot = t[1], cnt = 0;
-                for (int bid=0; bid<num_b; bid++) {
+                for (int bid=0; bid<m; bid++) {
                     for (auto x : a) {
                         auto tg = tot - x - offset[bid];
                         if (frq[bid].count(tg)) {
