524fca494e3c1890b34152387b49fa4a77f6d99a
Martin Yang <martin0327@gmail.com>
2026-05-25 14:07:05 +0900

    Time: 27 ms (9.47%), Space: 27.5 MB (31.58%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,14 +1,11 @@
-using vi = vector<int>;
-const int sz = 1e5+1;
 class Solution {
 public:
     bool canReach(string s, int lo, int hi) {
         int n = s.size();
-        vi a = {0};
+        vector<int> a = {0};
         for (int i=1; i<n; i++) {
             if (s[i] == '1') continue;
-            int tg = i - hi;
-            auto it = lower_bound(a.begin(), a.end(), tg);
+            auto it = lower_bound(a.begin(), a.end(), i-hi);
             if (it != a.end() && *it <= i-lo) a.push_back(i);
         }
         return a.back() == n-1;
