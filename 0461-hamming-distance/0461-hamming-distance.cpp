5170ec2ffeef459e722607abfbbc2d7e60ea995b
Martin Yang <martin0327@gmail.com>
2026-05-26 18:09:26 +0900

    Time: 0 ms (100%), Space: 7.9 MB (67.55%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans = 0;
+        for (int i=0; i<32; i++) {
+            ans += (x>>i&1) ^ (y>>i&1);
+        }
+        return ans;
+    }
+};
