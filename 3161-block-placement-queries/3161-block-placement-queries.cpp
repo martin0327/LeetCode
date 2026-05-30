16f97956356505118dec67927c042aa12aa8de8f
Martin Yang <martin0327@gmail.com>
2026-05-30 22:42:28 +0900

    Time: 823 ms (5.94%), Space: 367.1 MB (27.72%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.cpp
+++ 3161-block-placement-queries/3161-block-placement-queries.cpp
@@ -150,32 +150,6 @@ using max_pq = priority_queue<T>;
 
 template<typename T1, typename T2>
 void chmax(T1 &x, T2 y) { if (x < y) x = y; }
-template<typename T1, typename T2>
-void chmin(T1 &x, T2 y) { if (x > y) x = y; }
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
-template<typename T>
-void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
-
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    sort(a.begin(), a.end());
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
-
-using ll = long long;
-using vi = vector<ll>;
-using vvi = vector<vi>;
-using pii = pair<ll,ll>;
-using vp = vector<pii>;
-using vvp = vector<vp>;
-using ti3 = tuple<ll,ll,ll>;
-using vti3 = vector<ti3>;
-using vs = vector<string>;
-
 const int inf = 2e9;
 using S = int;
 S op(S x, S y) { return max(x,y); }
@@ -202,8 +176,7 @@ public:
                 int x = v[1], len = v[2];
                 auto it = s.lower_bound(x);
                 int r = *prev(it);
-                int mx = x - r;
-                chmax(mx, seg.prod(0,r+1));
+                int mx = max(x-r, seg.prod(0,r+1));
                 ans.push_back(mx >= len);
             }
         }
