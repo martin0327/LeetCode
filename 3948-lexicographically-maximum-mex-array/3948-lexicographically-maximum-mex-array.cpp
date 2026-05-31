501cb6c61ead430986c0665ac5525f2497030da8
Martin Yang <martin0327@gmail.com>
2026-05-31 15:16:24 +0900

    Time: 1552 ms (5.05%), Space: 309.1 MB (5.06%) - LeetHub

---
+++ 3948-lexicographically-maximum-mex-array/3948-lexicographically-maximum-mex-array.cpp
@@ -0,0 +1,235 @@
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+namespace atcoder {
+
+namespace internal {
+
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+}  // namespace atcoder
+
+using namespace atcoder;
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+
+template<typename T1, typename T2>
+void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template<typename T1, typename T2>
+void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T>
+void asort(vector<T> &a) {sort(a.begin(), a.end());}
+template<typename T>
+void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
+template<typename T>
+void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
+
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
+    return a;
+}
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<ll,ll>;
+using vp = vector<pii>;
+using vvp = vector<vp>;
+using ti3 = tuple<ll,ll,ll>;
+using vti3 = vector<ti3>;
+using vs = vector<string>;
+
+const int inf = 2e9;
+using S = int;
+S op(S x, S y) { return min(x,y); }
+S e() { return inf; }
+
+class Solution {
+public:
+    vector<int> maximumMEX(vector<int>& a) {
+        int n = a.size(), sz = 1e5+1;
+        segtree<S,op,e> seg(vi(sz,0));
+        for (auto x : a) {
+            seg.set(x, seg.get(x)+1);
+        }
+
+        auto f = [&] () {
+            int ret = seg.max_right(0, [&] (int x) {
+                return x > 0;
+            });
+            return ret;
+        };
+
+        auto t = f();
+
+        vi ans;
+        reverse(a.begin(), a.end());
+        int cnt = 0;
+        while (a.size()) {
+            auto mex = f();
+            ans.push_back(mex);
+            if (mex == 0) {
+                auto x = a.back();
+                seg.set(x,seg.get(x)-1);
+                a.pop_back();
+            }
+            else {
+                int n = a.size(), idx = -1;
+                set<int> s;
+                for (int i=n-1; i>=0; i--) {
+                    if (a[i] >= mex) continue;
+                    else s.insert(a[i]);
+                    if (s.size() == mex) {
+                        idx = i;
+                        break;
+                    }
+                }
+                while (a.size() > idx) {
+                    auto x = a.back();
+                    seg.set(x, seg.get(x)-1);
+                    a.pop_back();
+                }
+            }
+            // cnt++;
+            // if (cnt == 3) break;
+        }
+        return ans;
+    }
+};
