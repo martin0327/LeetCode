c921d4a06e120d31950dd911e32871c0114e7822
Martin Yang <martin0327@gmail.com>
2026-06-02 08:47:51 +0900

    Time: 2 ms (71.73%), Space: 91.8 MB (85.65%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,12 +1,12 @@
-void chmin(int &x, int y) { x = min(x,y); }
+void chmin(short &x, short y) { x = min(x,y); }
 class Solution {
 public:
     int earliestFinishTime(vector<int>& at, vector<int>& ad, vector<int>& bt, vector<int>& bd) {
-        int n = at.size(), m = bt.size(), ans = 2e9;
-        for (int i=0; i<n; i++) {
-            int x = at[i] + ad[i];
-            for (int j=0; j<m; j++) {
-                int y = bt[j] + bd[j];
+        short n = at.size(), m = bt.size(), ans = 1<<14;
+        for (short i=0; i<n; i++) {
+            short x = at[i] + ad[i];
+            for (short j=0; j<m; j++) {
+                short y = bt[j] + bd[j];
                 chmin(ans, (x<=bt[j])?y:x+bd[j]);
                 chmin(ans, (y<=at[i])?x:y+ad[i]);
             }
