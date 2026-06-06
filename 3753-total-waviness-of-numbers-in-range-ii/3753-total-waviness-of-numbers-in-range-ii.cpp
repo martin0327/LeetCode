a9697fc6a98c936a8986341a5781b9d024fef941
Martin Yang <martin0327@gmail.com>
2026-06-06 11:51:12 +0900

    Time: 54 ms (41.14%), Space: 10.6 MB (34.18%) - LeetHub

--- 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
+++ 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
@@ -5,13 +5,11 @@ ll dp[sz][2][2][3][10][sz];
 class Solution {
 public:
     long long totalWaviness(long long lo, long long hi) {
-        function<ll(ll)> f = [&] (ll y) {
-            auto s = to_string(y);
+        function<ll(ll)> f = [&] (ll z) {
+            auto s = to_string(z);
             ll ret = 0, n = s.size();
             vi a(n);
-            for (int i=0; i<n; i++) {
-                a[i] = s[i] - '0';
-            }
+            for (int i=0; i<n; i++) a[i] = s[i] - '0';
             memset(dp,0,sizeof(dp));
             dp[0][0][0][0][0][0] = 1;
             for (int i=0; i<n; i++) {
@@ -19,18 +17,16 @@ public:
                     for (int lz=0; lz<2; lz++) {
                         for (int j=0; j<3; j++) {
                             for (int l=0; l<10; l++) {
-                                for (int x=0; x<sz; x++) {
-                                    if (dp[i][tb][lz][j][l][x] == 0) continue;
+                                for (int cnt=0; cnt<sz; cnt++) {
+                                    if (dp[i][tb][lz][j][l][cnt] == 0) continue;
                                     for (int d=0; d<=(tb?9:a[i]); d++) {
                                         int ntb = tb | (d < a[i]);
                                         int nlz = lz | (d != 0);
                                         int nj = 0;
-                                        if (d < l) nj = 1;
-                                        if (d > l) nj = 2;
-                                        if (lz == 0) nj = 0;
+                                        if (lz && d != l) nj = (d<l) ? 1 : 2;
                                         int nl = d;
-                                        int nx = x + (j+nj==3);
-                                        dp[i+1][ntb][nlz][nj][nl][nx] += dp[i][tb][lz][j][l][x];
+                                        int ncnt = cnt + (j+nj==3);
+                                        dp[i+1][ntb][nlz][nj][nl][ncnt] += dp[i][tb][lz][j][l][cnt];
                                     }
                                 }
                             }
@@ -42,8 +38,8 @@ public:
                 for (int lz=0; lz<2; lz++) {
                     for (int j=0; j<3; j++) {
                         for (int l=0; l<10; l++) {
-                            for (int x=1; x<sz; x++) {
-                                ret += dp[n][tb][lz][j][l][x] * x;
+                            for (int cnt=1; cnt<sz; cnt++) {
+                                ret += dp[n][tb][lz][j][l][cnt] * cnt;
                             }
                         }
                     }
