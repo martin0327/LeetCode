ab57eafe741ab026ce3f233aaaecf6713171c18d
Martin Yang <martin0327@gmail.com>
2026-06-06 14:37:57 +0900

    Time: 5 ms (33.32%), Space: 13.3 MB (9.08%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,49 @@
+using vi = vector<int>;
+using pnn = pair<Node*,Node*>;
+
+void debug(vector<int> a) {
+    for (auto x : a) cout << x << " ";
+    cout << endl;
+}
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        function<pnn(Node*)> f = [&] (Node* u) {
+            pnn ret;
+            if (!u) return ret;
+            
+            auto nb = u->next;
+            if (u->child) {
+                
+                auto v = u->child;
+                u->child = nullptr;
+                while (v->prev) v = v->prev;
+                auto [l,r] = f(v);
+                // vector<int> a = {u->val, l->val, r->val};
+                // debug(a);
+                u->next = l;
+                l->prev = u;
+                
+                
+                if (r && nb) {
+                    vi t = {1};
+                    debug(t);
+                    r->next = nb;
+                    nb->prev = r;
+                }
+                else if (r) {
+                    vi t = {2};
+                    debug(t);
+                    nb = r;
+                }
+            }
+            ret.first = u;
+            if (nb) ret.second = f(nb).second;
+            else ret.second = u;
+            
+            return ret;
+        };
+        auto [l,r] = f(head);
+        return l;
+    }
+};
