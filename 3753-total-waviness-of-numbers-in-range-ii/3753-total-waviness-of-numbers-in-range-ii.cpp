6230deec76da618a91937f06315235776edd3026
Martin Yang <martin0327@gmail.com>
2026-06-06 11:44:05 +0900

    Time: 44 ms (43.99%), Space: 10.3 MB (43.35%) - LeetHub

--- 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
+++ 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
@@ -1,50 +1,7 @@
-
-template<int D, typename T>
-struct Vec : public vector<Vec<D - 1, T>> {
-  static_assert(D >= 1, "Vector dimension must be greater than zero!");
-  template<typename... Args>
-  Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...)) {}
-};
-
-template<typename T>
-struct Vec<1, T> : public vector<T> {
-  Vec(int n = 0, const T& val = T()) : vector<T>(n, val) {}
-};
-
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
-
-template<typename T1, typename T2>
-void chmax(T1 &x, T2 y) { if (x < y) x = y; }
-template<typename T1, typename T2>
-void chmin(T1 &x, T2 y) { if (x > y) x = y; }
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
-template<typename T>
-void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
-
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    sort(a.begin(), a.end());
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
-
 using ll = long long;
 using vi = vector<ll>;
-using vvi = vector<vi>;
-using pii = pair<ll,ll>;
-using vp = vector<pii>;
-using vvp = vector<vp>;
-using ti3 = tuple<ll,ll,ll>;
-using vti3 = vector<ti3>;
-using vs = vector<string>;
-
-const int sz = 15;
+const int sz = 17;
+ll dp[sz][2][2][3][10][sz];
 class Solution {
 public:
     long long totalWaviness(long long A, long long B) {
@@ -55,7 +12,7 @@ public:
             for (int i=0; i<n; i++) {
                 a[i] = s[i] - '0';
             }
-            Vec<6,ll> dp(n+1,2,2,3,10,sz);
+            memset(dp,0,sizeof(dp));
             dp[0][0][0][0][0][0] = 1;
             for (int i=0; i<n; i++) {
                 for (int tb=0; tb<2; tb++) {
@@ -95,8 +52,6 @@ public:
             return ret;
         };
         auto ans = f(B) - f(A-1);
-        // debug(B,f(B));
-        // debug(A-1,f(A-1));
         return ans;
     }
 };
