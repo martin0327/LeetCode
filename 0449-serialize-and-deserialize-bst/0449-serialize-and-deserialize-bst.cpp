62280cae9099367b369b83e4a19b4a0a0cac5853
Martin Yang <martin0327@gmail.com>
2026-06-17 01:29:59 +0900

    Time: 26 ms (37.11%), Space: 34 MB (22.87%) - LeetHub

---
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -0,0 +1,73 @@
+using vs = vector<string>;
+string t = "!@#";
+
+vs g(string s) {
+    vector<string> ret;
+    set<char> sc(t.begin(), t.end());
+    for (auto c : s) {
+        if (ret.empty()) ret.push_back(string(1,c));
+        else {
+            if (sc.count(ret.back().back()) || sc.count(c)) ret.push_back(string(1,c));
+            else ret.back() += c;
+        }
+    }
+    return ret;
+}
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string s;
+        if (!root) return s;
+        function<void(TreeNode*)> f = [&] (TreeNode* v) {
+            s += to_string(v->val);
+            if (v->left) {
+                s += '!';
+                f(v->left);
+            }
+            if (v->right) {
+                s += '@';
+                f(v->right);
+            }
+            s += '#';
+            return;
+        };
+        f(root);
+        return s;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string s) {
+        if (s.empty()) return nullptr;
+        auto a = g(s);
+        auto ret = new TreeNode();
+        int n = a.size(), i = 0;
+        function<void(TreeNode*)> f = [&] (TreeNode* v) {
+            if (i >= n) return;
+            if (a[i] == "#") {
+                i++;
+                return;
+            }
+            else if (a[i] == "!") {
+                v->left = new TreeNode();
+                i++;
+                f(v->left);
+                f(v);
+            }
+            else if (a[i] == "@") {
+                v->right = new TreeNode();
+                i++;
+                f(v->right);
+                f(v);
+            }
+            else {
+                v->val = stoi(a[i]);
+                i++;
+                f(v);
+            }
+        };
+        f(ret);
+        return ret;
+    }
+};
