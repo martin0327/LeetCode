4a82e03a811ebfe44e156eb1e04810a2863959c4
Martin Yang <martin0327@gmail.com>
2026-06-17 22:27:53 +0900

    Time: 20 ms (89.66%), Space: 24 MB (67.82%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -1,5 +1,4 @@
 using ll = long long;
-using vi = vector<ll>;
 class Solution {
 public:
     char processStr(string s, long long k) {
@@ -12,18 +11,14 @@ public:
             else if (ch != '%') m++;
         }
         if (k < 0 || k >= m) return '.';
-        bool x = false;
         for (int i=n-1; i>=0; i--) {
             auto ch = s[i];
-            if (ch == '*') {
-                m++;
-            }
+            if (ch == '*') m++;
             else if (ch == '#') {
                 m >>= 1;
                 if (k >= m) k -= m;
             }
             else if (ch == '%') {
-                x ^= 1;
                 k = m-1-k;
             }
             else {
