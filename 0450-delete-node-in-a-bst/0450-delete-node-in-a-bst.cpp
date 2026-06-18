f48431b6a6429c5d0ad9633ddf7f15d984395151
Martin Yang <martin0327@gmail.com>
2026-06-19 08:42:03 +0900

    Time: 5 ms (0.45%), Space: 33.7 MB (99.62%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,36 @@
+using vi = vector<int>;
+void debug(vi &a) {
+    for (auto &x : a) cout << x << " ";
+    cout << endl;
+}
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        vi a;
+        function<void(TreeNode*)> f = [&] (TreeNode* v) {
+            if (!v) return;
+            f(v->left);
+            if (v->val != key) a.push_back(v->val);
+            f(v->right);
+        };
+        f(root);
+
+        int n = a.size();
+        if (n == 0) return nullptr;
+        function<void(TreeNode*,int,int)> g = [&] (TreeNode* v, int l, int r) {
+            int m = (l+r)/2;
+            v->val = a[m];
+            if (l <= m-1) {
+                v->left = new TreeNode();
+                g(v->left,l,m-1);
+            }
+            if (m+1 <= r) {
+                v->right = new TreeNode();
+                g(v->right,m+1,r);
+            }
+        };
+        auto ret = new TreeNode();
+        g(ret,0,n-1);
+        return ret;
+    }
+};
