7843edab232b6c916b46bb184978cc5d491291c8
Martin Yang <martin0327@gmail.com>
2026-06-24 16:49:14 +0900

    Time: 4 ms (18.62%), Space: 12.8 MB (6.3%) - LeetHub

--- 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.cpp
+++ 0467-unique-substrings-in-wraparound-string/0467-unique-substrings-in-wraparound-string.cpp
@@ -4,25 +4,17 @@ const int sz = 26;
 class Solution {
 public:
     int findSubstringInWraproundString(string s) {
-        int n = s.size(), cur = 0;
-        vi a(n,1);
-        for (int i=1; i<n; i++) {
-            int x = s[i] - 'a';
-            int y = s[i-1] - 'a';
-            if (x == (y+1) % sz) a[i] = a[i-1] + 1;
-        }
-        set<pii> sp;
+        int n = s.size();
+        vi a(n,1), b(sz);
         for (int i=0; i<n; i++) {
-            sp.insert({a[i], s[i] - 'a'});
-        }
-        set<pii> t;
-        for (auto [x,y] : sp) {
-            for (int i=x; i>=1; i--) {
-                if (t.count({i,y})) break;
-                else t.insert({i,y});
+            int x = s[i] - 'a';
+            if (i > 0) {
+                int y = s[i-1] - 'a';
+                if (x == (y+1) % sz) a[i] = a[i-1] + 1;
             }
+            b[x] = max(b[x], a[i]);
         }
-        int ans = t.size();
+        int ans = accumulate(b.begin(), b.end(), 0);
         return ans;
     }
 }; 
