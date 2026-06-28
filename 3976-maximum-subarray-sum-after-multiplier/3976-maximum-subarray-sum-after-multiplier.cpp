a507cb2f15dfb4a7b3ba75056a242dd2e1c75e97
Martin Yang <martin0327@gmail.com>
2026-06-28 16:45:31 +0900

    Time: 75 ms (14.27%), Space: 273.2 MB (6.8%) - LeetHub

--- 3976-maximum-subarray-sum-after-multiplier/3976-maximum-subarray-sum-after-multiplier.cpp
+++ 3976-maximum-subarray-sum-after-multiplier/3976-maximum-subarray-sum-after-multiplier.cpp
@@ -1,36 +1,9 @@
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
-
 template<typename T1, typename T2>
 void chmax(T1 &x, T2 y) { if (x < y) x = y; }
 template<typename T1, typename T2>
 void chmin(T1 &x, T2 y) { if (x > y) x = y; }
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
-template<typename T>
-void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
-
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    asort(a);
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
-
 using ll = long long;
 using vi = vector<ll>;
-using vvi = vector<vi>;
-using pii = pair<ll,ll>;
-using vp = vector<pii>;
-using vvp = vector<vp>;
-using ti3 = tuple<ll,ll,ll>;
-using vti3 = vector<ti3>;
-using vs = vector<string>;
-
 const ll inf = 2e18;
 
 class Solution {
@@ -44,38 +17,28 @@ public:
             b[i] = a[i] * k;
             c[i] = a[i] / k;
         }
-        vi pre(n+1), suf(n+2);
-        vi p1(n+1,-inf), s1(n+2,0);
-        p1[0] = 0, s1[n+1] = 0;
+        vi pre(n+1), suf(n+2), s(n+2,0);
         for (int i=1; i<=n; i++) {
             pre[i] = pre[i-1] + a[i];
-        }
-        for (int i=1, mn=0; i<=n; i++) {
-            chmax(p1[i], pre[i]-mn);
-            chmin(mn, pre[i]);
         }
         for (int i=n; i>=1; i--) {
             suf[i] = suf[i+1] + a[i];
         }
-        for (int i=n, mn=0; i>=1; i--) {
-            chmax(s1[i], suf[i]-mn);
+        for (ll i=n, mn=0; i>=1; i--) {
+            chmax(s[i], suf[i]-mn);
             chmin(mn, suf[i]);
         }
-        // debug(suf);
-        // debug(s1);
 
         auto f = [&] (vi b) {
             vi p2(n+1);
             for (int i=1; i<=n; i++) {
                 p2[i] = p2[i-1] + b[i];
             }
-            // debug(p2);
-            // debug(pre);
-            ll ret = pre[n], t = 0;
-            for (int i=1, mn = 0; i<=n; i++) {
-                chmax(ret, p2[i] + s1[i+1] + t);
+            ll ret = pre[n];
+            for (ll i=1, mn = 0, mx=0; i<=n; i++) {
+                chmax(ret, p2[i] + s[i+1] + mx);
                 chmin(mn, pre[i]);
-                chmax(t, pre[i] - p2[i] - mn);
+                chmax(mx, pre[i] - p2[i] - mn);
             }
             return ret;
         };
