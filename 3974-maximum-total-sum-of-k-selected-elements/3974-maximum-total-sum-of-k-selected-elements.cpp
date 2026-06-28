2be9f54b92006fd6120d7763c12a25255f59246c
Martin Yang <martin0327@gmail.com>
2026-06-28 15:18:42 +0900

    Time: 186 ms (7.41%), Space: 203.7 MB (6.37%) - LeetHub

---
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.cpp
@@ -0,0 +1,50 @@
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+
+template<typename T1, typename T2>
+void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template<typename T1, typename T2>
+void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T>
+void asort(vector<T> &a) {sort(a.begin(), a.end());}
+template<typename T>
+void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
+template<typename T>
+void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
+
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+    asort(a);
+    a.erase(unique(a.begin(), a.end()), a.end());
+    return a;
+}
+
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using pii = pair<ll,ll>;
+using vp = vector<pii>;
+using vvp = vector<vp>;
+using ti3 = tuple<ll,ll,ll>;
+using vti3 = vector<ti3>;
+using vs = vector<string>;
+
+
+class Solution {
+public:
+    long long maxSum(vector<int>& aa, int k, int mul) {
+        vi a(aa.begin(), aa.end());
+        int n = a.size();
+        dsort(a);
+        ll ans = 0;
+        for (int i=0; i<k; i++) {
+            auto x = a[i];
+            auto y = a[i] * mul;
+            ans += max(x,y);
+            mul--;
+        }
+        return ans;
+    }
+};
