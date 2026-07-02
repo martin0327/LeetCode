58906bc7f67fab9abd00b2cf98ad751cc9788799
Martin Yang <martin0327@gmail.com>
2026-07-03 08:20:51 +0900

    Time: 271 ms (60.87%), Space: 340.1 MB (62.45%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -11,15 +11,14 @@ class Solution {
 public:
     int findMaxPathScore(vector<vector<int>>& edg, vector<bool>& b, long long k) {
         ll n = b.size(), m = edg.size();
-        vvp adj1(n), adj2(n);
+        vvp adj(n);
         vi ws;
         for (int i=0; i<m; i++) {
             auto &t = edg[i];
             int u = t[0], v = t[1], w = t[2];
             if (!b[u] || !b[v]) continue;
             ws.push_back(w);
-            adj1[u].push_back({v,w});
-            adj2[v].push_back({u,w});
+            adj[u].push_back({v,w});
         }
         sort(ws.begin(), ws.end());
         ws.erase(unique(ws.begin(), ws.end()), ws.end());
@@ -28,19 +27,11 @@ public:
             return it - ws.begin();
         };
         int sz = ws.size();
-        vvp a(sz);
-        for (int i=0; i<n; i++) {
-            int u = i;
-            for (auto [v,w] : adj1[u]) {
-                auto j = g(w);
-                a[j].push_back({u,v});
-            }
-        }
-        auto f = [&] (ll src, vvp &adj, vi &dist, ll lim) {
-            dist = vi(n, inf);
-            dist[src] = 0;
+        auto f = [&] (ll lim) {
+            vi dist(n, inf);
+            dist[0] = 0;
             min_pq<pii> pq;
-            pq.push({0,src});
+            pq.push({0,0});
             while (pq.size()) {
                 auto [d,u] = pq.top();
                 pq.pop();
@@ -54,15 +45,13 @@ public:
                     }
                 }
             }
+            return dist[n-1] <= k;
         };
         ll lo = 0, hi = sz-1, ans = -1;
         while (lo <= hi) {
             ll mid = (lo+hi)/2;
             auto w = ws[mid];
-            vi dist1, dist2;
-            f(0,adj1,dist1,w);
-            bool ok = dist1[n-1] <= k;
-            if (ok) {
+            if (f(w)) {
                 ans = w;
                 lo = mid + 1;
             }
