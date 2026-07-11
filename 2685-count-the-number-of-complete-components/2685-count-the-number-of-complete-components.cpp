4bef4cdc563b4507134c50655a4e8b7abf7c83a3
Martin Yang <martin0327@gmail.com>
2026-07-11 11:55:37 +0900

    Time: 72 ms (24.8%), Space: 130.8 MB (57.98%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,83 @@
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    std::vector<int> parent_or_size;
+};
+
+
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& e) {
+        dsu d(n);
+        map<int,int> mp;
+        for (auto uv : e) {
+            int u = uv[0], v = uv[1];
+            d.merge(u,v);
+        }
+        for (auto uv : e) {
+            int u = uv[0], v = uv[1];
+            int l = d.leader(u);
+            mp[l]++;
+        }
+        int ans = 0, cnt = 0;
+        for (auto &[k,v] : mp) {
+            int sz = d.size(k);
+            cnt += sz;
+            if (sz*(sz-1) == 2*v) ans++;
+        }
+        ans += (n - cnt);
+        return ans;
+    }
+};
