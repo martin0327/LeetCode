bc95497b161477d7f8c4370f1f8eaaac2cb5e353
Martin Yang <martin0327@gmail.com>
2026-07-17 22:04:29 +0900

    Time: 48 ms (50.67%), Space: 122.9 MB (86.67%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -1,13 +1,11 @@
 using ll = long long;
-const int sz = 5e4+1;
-ll b[sz];
+using vi = vector<ll>;
 class Solution {
 public:
     vector<int> gcdValues(vector<int>& a, vector<long long>& qr) { 
-        memset(b,0,sizeof(b));
-        for (auto x : a) {
-            b[x]++;
-        }
+        int sz = *max_element(a.begin(), a.end()) + 1;
+        vi b(sz);
+        for (auto x : a) b[x]++;
         for (int i=1; i<sz; i++) {
             for (int j=2*i; j<sz; j+=i) {
                 b[i] += b[j];
@@ -19,24 +17,11 @@ public:
                 b[i] -= b[j];
             }
         }
-        for (int i=1; i<sz; i++) {
-            b[i] += b[i-1];
-        }
+        for (int i=1; i<sz; i++) b[i] += b[i-1];
         vector<int> ans;
-        for (auto q : qr) {
-            q++;
-            ll lo = 0, hi = sz-1, res = -1;
-            while (lo <= hi) {
-                ll mid = (lo+hi)/2;
-                if (b[mid] >= q) {
-                    res = mid;
-                    hi = mid - 1;
-                }
-                else {
-                    lo = mid + 1;
-                }
-            }
-            ans.push_back(res);
+        for (auto x : qr) {
+            auto it = lower_bound(b.begin(), b.end(), x+1);
+            ans.push_back(it-b.begin());
         }
         return ans;
     }
