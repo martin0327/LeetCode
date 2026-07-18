6c81cc4f8531dfaae0903de8ece9979aff1209be
Martin Yang <martin0327@gmail.com>
2026-07-19 02:00:35 +0900

    Time: 5 ms (5.5%), Space: 10.5 MB (9.91%) - LeetHub

--- 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
@@ -4,13 +4,13 @@ public:
         map<char,int> mp;
         for (auto ch : s) mp[ch]++;
         string ans;
-        int cnt1 = mp[y], cnt2 = mp[x];
-        while (cnt1--) ans += y;
+        int c1 = mp[y], c2 = mp[x];
+        while (c1--) ans += y;
         for (auto [k,v] : mp) {
             if (k == x || k == y) continue;
             while (v--) ans += k;
         }
-        while (cnt2--) ans += x;
+        while (c2--) ans += x;
         return ans;
     }
 };
