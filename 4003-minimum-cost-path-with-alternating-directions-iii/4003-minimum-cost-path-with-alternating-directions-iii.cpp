a12afbfde984a0775041ac7bca5db3d9c30a4033
Martin Yang <martin0327@gmail.com>
2026-07-26 15:41:39 +0900

    Time: 488 ms (83.94%), Space: 139.6 MB (77.95%) - LeetHub

--- 4003-minimum-cost-path-with-alternating-directions-iii/4003-minimum-cost-path-with-alternating-directions-iii.cpp
+++ 4003-minimum-cost-path-with-alternating-directions-iii/4003-minimum-cost-path-with-alternating-directions-iii.cpp
@@ -25,17 +25,23 @@ using ti4 = tuple<ll,ll,ll,ll>;
 const ll inf = 2e18;
 vi dr = {0,0,1,-1};
 vi dc = {1,-1,0,0};
+const int maxn = 1e5+1;
+vi dist[2][maxn];
 class Solution {
 public:
     long long minCost(int n, int m, vector<vector<int>>& pen) {
-        Vec<3,ll> dist(n,m,2,inf);
+        for (int i=0; i<2; i++) {
+            for (int j=0; j<n; j++) {
+                dist[i][j] = vi(m, inf);
+            }
+        }
         dist[0][0][0] = 1;
         min_pq<ti4> pq;
         pq.push({1,0,0,0});
         while (pq.size()) {
-            auto [d,r,c,j] = pq.top();
+            auto [d,j,r,c] = pq.top();
             pq.pop();
-            if (d > dist[r][c][j]) continue;
+            if (d > dist[j][r][c]) continue;
             for (int i=0; i<4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
@@ -43,25 +49,24 @@ public:
                 if (nr < 0 || nr >= n) continue;
                 if (nc < 0 || nc >= m) continue;
                 ll cost = (nr+1) * (nc+1);
-                if ((i%2) != (j&1)) cost += pen[r][c];
+                if ((i&1) != (j&1)) cost += pen[r][c];
                 ll nd = d + cost;
-                if (nd < dist[nr][nc][nj]) {
-                    dist[nr][nc][nj] = nd;
-                    pq.push({nd,nr,nc,nj});
+                if (nd < dist[nj][nr][nc]) {
+                    dist[nj][nr][nc] = nd;
+                    pq.push({nd,nj,nr,nc});
                 }
             }
             {
-                int nj = j^1;
-                ll cost = pen[r][c];
-                ll nd = d + cost;
-                if (nd < dist[r][c][nj]) {
-                    dist[r][c][nj] = nd;
-                    pq.push({nd,r,c,nj});
+                int nj = j^1, nr = r, nc = c;
+                ll nd = d + pen[r][c];
+                if (nd < dist[nj][nr][nc]) {
+                    dist[nj][nr][nc] = nd;
+                    pq.push({nd,nj,nr,nc});
                 }
             }
         }
-        auto ans = dist[n-1][m-1][0];
-        chmin(ans, dist[n-1][m-1][1]);
+        auto ans = dist[0][n-1][m-1];
+        chmin(ans, dist[1][n-1][m-1]);
         return ans;
     }
 };
