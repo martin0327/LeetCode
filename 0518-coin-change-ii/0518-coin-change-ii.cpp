9532a455be526911ce2c43594c64caeb54418192
Martin Yang <martin0327@gmail.com>
2026-07-27 07:40:06 +0900

    Time: 7 ms (87.18%), Space: 10.5 MB (81.53%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,12 +1,15 @@
+using ll = long long;
+const ll inf = 2e15;
 class Solution {
 public:
     int change(int k, vector<int>& c) {
         int n = c.size();
-        vector<int> dp(k+1);
+        vector<ll> dp(k+1);
         dp[0] = 1;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j+c[i]<=k; j++) {
+        for (ll i=0; i<n; i++) {
+            for (ll j=0; j+c[i]<=k; j++) {
                 dp[j+c[i]] += dp[j];
+                dp[j+c[i]] = min(dp[j+c[i]], inf);
             }
         }
         return dp[k];
