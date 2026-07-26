2e0731b94fd626854ea1a2f47959858e309b077a
Martin Yang <martin0327@gmail.com>
2026-07-26 15:30:47 +0900

    Time: 437 ms (8.77%), Space: 411.2 MB (10.78%) - LeetHub

---
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.cpp
@@ -0,0 +1,29 @@
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+    sort(a.begin(), a.end());
+    a.erase(unique(a.begin(), a.end()), a.end());
+    return a;
+}
+using vi = vector<int>;
+using vvi = vector<vi>;
+class Solution {
+public:
+    vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& a, vector<vector<int>>& b) {
+        vi c;
+        for (auto &v : a) c.push_back(v[0]);
+        for (auto &v : b) c.push_back(v[0]);
+        c = get_unique(c);
+        vvi ans;
+        for (auto x : c) {
+            vi tg = {x,0};
+            auto it1 = lower_bound(a.begin(), a.end(), tg);
+            auto it2 = lower_bound(b.begin(), b.end(), tg);
+            int s = 0;
+            if (it1 != a.end()) s += (*it1)[1];
+            if (it2 != b.end()) s += (*it2)[1];
+            vi t = {x,s};
+            ans.push_back(t);
+        }
+        return ans;
+    }
+};
