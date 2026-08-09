4fbfca7799f6d291c4978fd7b04fa0ed437d8b46
Martin Yang <martin0327@gmail.com>
2026-08-09 15:06:18 +0900

    Time: 136 ms (36.9%), Space: 136.5 MB (38.68%) - LeetHub

--- 4016-maximum-area-of-two-non-overlapping-square-submatrices/4016-maximum-area-of-two-non-overlapping-square-submatrices.cpp
+++ 4016-maximum-area-of-two-non-overlapping-square-submatrices/4016-maximum-area-of-two-non-overlapping-square-submatrices.cpp
@@ -1,35 +1,11 @@
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
-
 template<typename T1, typename T2>
 void chmax(T1 &x, T2 y) { if (x < y) x = y; }
 template<typename T1, typename T2>
 void chmin(T1 &x, T2 y) { if (x > y) x = y; }
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
-template<typename T>
-void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
-
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    asort(a);
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
 
 using ll = long long;
 using vi = vector<ll>;
 using vvi = vector<vi>;
-using pii = pair<ll,ll>;
-using vp = vector<pii>;
-using vvp = vector<vp>;
-using ti3 = tuple<ll,ll,ll>;
-using vti3 = vector<ti3>;
-using vs = vector<string>;
 const int inf = 2e9;
 class Solution {
 public:
@@ -61,14 +37,9 @@ public:
             ll rmax = -inf, cmax = -inf;
             bool ok = false;
             for (int i=len; i<=n; i++) {
-                if (ok) break;
                 for (int j=len; j<=m; j++) {
-                    if (ok) break; 
                     if (f(i,j,len) == len*len) {
-                        if (rmin+len <= i || cmin+len <= j || cmax-len >= j) {
-                            ok = true;
-                            break;
-                        } 
+                        if (rmin+len <= i || cmin+len <= j || cmax-len >= j) ok = true;
                         chmin(rmin, i);
                         chmin(cmin, j);
                         chmax(rmax, i);
