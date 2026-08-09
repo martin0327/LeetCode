56889bd2b6256b88c6ce7737f1b4938ede10d8b5
Martin Yang <martin0327@gmail.com>
2026-08-09 23:54:14 +0900

    Time: 133 ms (37.77%), Space: 124.3 MB (58.35%) - LeetHub

--- 4016-maximum-area-of-two-non-overlapping-square-submatrices/4016-maximum-area-of-two-non-overlapping-square-submatrices.cpp
+++ 4016-maximum-area-of-two-non-overlapping-square-submatrices/4016-maximum-area-of-two-non-overlapping-square-submatrices.cpp
@@ -3,8 +3,7 @@ void chmax(T1 &x, T2 y) { if (x < y) x = y; }
 template<typename T1, typename T2>
 void chmin(T1 &x, T2 y) { if (x > y) x = y; }
 
-using ll = long long;
-using vi = vector<ll>;
+using vi = vector<int>;
 using vvi = vector<vi>;
 const int inf = 2e9;
 class Solution {
@@ -12,34 +11,26 @@ public:
     int maxArea(vector<vector<int>>& a) {
         int n = a.size(), m = a[0].size();
         vvi pre(n+1, vi(m+1));
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                pre[i+1][j+1] = a[i][j];
-            }
-        }
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=m; j++) {
+                pre[i][j] = a[i-1][j-1] - pre[i-1][j-1];
                 pre[i][j] += pre[i-1][j] + pre[i][j-1];
-                pre[i][j] -= pre[i-1][j-1];
             }
         }
         auto f = [&] (int i, int j, int l) {
             assert(i >= l && j >= l);
-            auto ret = pre[i][j] - pre[i-l][j] - pre[i][j-l];
-            ret += pre[i-l][j-l];
+            auto ret = pre[i][j] + pre[i-l][j-l];
+            ret -= pre[i-l][j] + pre[i][j-l];
             return ret;
         };
-        ll lo = 1, hi = min(n,m), ans = 0;
+        int lo = 1, hi = min(n,m), ans = 0;
         while (lo <= hi) {
-            ll mid = (lo+hi)/2;
-            ll len = mid;
-            ll rmin = inf, cmin = inf;
-            ll rmax = -inf, cmax = -inf;
-            bool ok = false;
+            int mid = (lo+hi)/2, len = mid;
+            int rmin = inf, cmin = inf;
+            int rmax = -inf, cmax = -inf;
             for (int i=len; i<=n; i++) {
                 for (int j=len; j<=m; j++) {
                     if (f(i,j,len) == len*len) {
-                        if (rmin+len <= i || cmin+len <= j || cmax-len >= j) ok = true;
                         chmin(rmin, i);
                         chmin(cmin, j);
                         chmax(rmax, i);
@@ -47,7 +38,7 @@ public:
                     }
                 }
             }
-            if (ok) {
+            if (rmin+len <= rmax || cmin+len <= cmax) {
                 ans = mid;
                 lo = mid + 1;
             }
