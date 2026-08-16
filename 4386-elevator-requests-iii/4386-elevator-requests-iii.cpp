3dab685b2acbe49c9a8e423b7110218792dbce21
Martin Yang <martin0327@gmail.com>
2026-08-16 15:43:13 +0900

    Time: 690 ms (14.75%), Space: 226.9 MB (9.02%) - LeetHub

--- 4386-elevator-requests-iii/4386-elevator-requests-iii.cpp
+++ 4386-elevator-requests-iii/4386-elevator-requests-iii.cpp
@@ -1,28 +1,7 @@
-
-
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
-
 template<typename T1, typename T2>
 void chmax(T1 &x, T2 y) { if (x < y) x = y; }
 template<typename T1, typename T2>
 void chmin(T1 &x, T2 y) { if (x > y) x = y; }
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
-template<typename T>
-void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
-
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    sort(a.begin(), a.end());
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
-
 using ll = long long;
 using vi = vector<ll>;
 using vvi = vector<vi>;
@@ -30,10 +9,10 @@ const ll inf = 2e18;
 class Solution {
 public:
     long long elevatorRequests(int m, int start, vector<vector<int>>& a) {
-        a.push_back({0,start});
+        a.insert(a.begin(), {0,start});
         int n = a.size();
         vvi dp(1<<n, vi(n, inf));
-        dp[1<<(n-1)][n-1] = 0;
+        dp[1][0] = 0;
         for (int mask=0; mask<(1<<n); mask++) {
             for (int i=0; i<n; i++) {
                 if (dp[mask][i] == inf) continue;
