c529915f768e926ddf7857508d5b60503259e476
Martin Yang <martin0327@gmail.com>
2026-08-16 16:21:38 +0900

    Time: 31 ms (19.71%), Space: 23.9 MB (68.69%) - LeetHub

--- 0639-decode-ways-ii/0639-decode-ways-ii.cpp
+++ 0639-decode-ways-ii/0639-decode-ways-ii.cpp
@@ -507,41 +507,7 @@ using is_dynamic_modint_t = std::enable_if_t<is_dynamic_modint<T>::value>;
 using namespace atcoder;
 
 using mint = static_modint<1000000007>;
-
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
-
-template<typename T1, typename T2>
-void chmax(T1 &x, T2 y) { if (x < y) x = y; }
-template<typename T1, typename T2>
-void chmin(T1 &x, T2 y) { if (x > y) x = y; }
-template<typename T>
-void asort(vector<T> &a) {sort(a.begin(), a.end());}
-template<typename T>
-void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
-template<typename T>
-void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
-
-template<typename T>
-vector<T> get_unique(vector<T> a) {
-    asort(a);
-    a.erase(unique(a.begin(), a.end()), a.end());
-    return a;
-}
-
 using vm = vector<mint>;
-using ll = long long;
-using vi = vector<ll>;
-using vvi = vector<vi>;
-using pii = pair<ll,ll>;
-using vp = vector<pii>;
-using vvp = vector<vp>;
-using ti3 = tuple<ll,ll,ll>;
-using vti3 = vector<ti3>;
-using vs = vector<string>;
-
 class Solution {
 public:
     int numDecodings(string s) {
