d55c0b133a24987c01e4416a259b3f79e382039a
Martin Yang <martin0327@gmail.com>
2026-08-17 12:54:21 +0900

    Time: 1343 ms (5.03%), Space: 42.5 MB (5.08%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -0,0 +1,41 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+void chmax(int &x, int y) { x = max(x,y); }
+class Solution {
+public:
+    int stoneGameV(vector<int>& a) {
+        int n = a.size();
+        vi pre(n, a[0]);
+        for (int i=1; i<n; i++) pre[i] = pre[i-1] + a[i];
+        auto g = [&] (int l, int r) {
+            int ret = pre[r];
+            if (l > 0) ret -= pre[l-1];
+            return ret;
+        };
+        vvi dp(n, vi(n));
+        vvi vis(n, vi(n));
+        function<int(int,int)> f = [&] (int l, int r) {
+            int ret = 0;
+            if (vis[l][r]) return dp[l][r];
+            vis[l][r] = 1;
+            if (l == r) return dp[l][r] = ret;
+            for (int i=l; i<r; i++) {
+                auto v1 = g(l,i), v2 = g(i+1,r);
+                int t = 0;
+                if (v1 < v2) {
+                    t = v1 + f(l,i);
+                }
+                else if (v1 > v2) {
+                    t = v2 + f(i+1,r);
+                }
+                else {
+                    t = max(f(l,i), f(i+1,r)) + v1;
+                }
+                chmax(ret, t);
+            }
+            return dp[l][r] = ret;
+        };
+        auto ans = f(0,n-1);
+        return ans;
+    }
+};
