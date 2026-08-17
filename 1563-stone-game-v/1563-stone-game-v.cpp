b199bfa7914790e78b5908094e6acd3a66026404
Martin Yang <martin0327@gmail.com>
2026-08-17 13:02:13 +0900

    Time: 359 ms (79.37%), Space: 14.3 MB (97.18%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -4,6 +4,24 @@ const int sz = 505;
 int pre[sz];
 int dp[sz][sz];
 bool vis[sz][sz];
+
+int g(int l, int r) { return pre[r+1] - pre[l]; }
+int f(int l, int r) {
+    int ret = 0;
+    if (vis[l][r]) return dp[l][r];
+    vis[l][r] = 1;
+    if (l == r) return dp[l][r] = ret;
+    for (int i=l; i<r; i++) {
+        auto v1 = g(l,i), v2 = g(i+1,r);
+        int t = 0;
+        if (v1 < v2) t = v1 + f(l,i);
+        else if (v1 > v2) t = v2 + f(i+1,r);
+        else t = max(f(l,i), f(i+1,r)) + v1;
+        chmax(ret, t);
+    }
+    return dp[l][r] = ret;
+}
+
 class Solution {
 public:
     int stoneGameV(vector<int>& a) {
@@ -12,23 +30,6 @@ public:
         memset(dp,0,sizeof(dp));
         memset(vis,0,sizeof(vis));
         for (int i=1; i<=n; i++) pre[i] = pre[i-1] + a[i-1];
-        auto g = [&] (int l, int r) { return pre[r+1] - pre[l]; };
-
-        function<int(int,int)> f = [&] (int l, int r) {
-            int ret = 0;
-            if (vis[l][r]) return dp[l][r];
-            vis[l][r] = 1;
-            if (l == r) return dp[l][r] = ret;
-            for (int i=l; i<r; i++) {
-                auto v1 = g(l,i), v2 = g(i+1,r);
-                int t = 0;
-                if (v1 < v2) t = v1 + f(l,i);
-                else if (v1 > v2) t = v2 + f(i+1,r);
-                else t = max(f(l,i), f(i+1,r)) + v1;
-                chmax(ret, t);
-            }
-            return dp[l][r] = ret;
-        };
         auto ans = f(0,n-1);
         return ans;
     }
