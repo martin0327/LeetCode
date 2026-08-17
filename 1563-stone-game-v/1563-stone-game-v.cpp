59680c8fbe52538cf0a8dc8b227f74ab2f0dfff2
Martin Yang <martin0327@gmail.com>
2026-08-17 12:58:55 +0900

    Time: 871 ms (18.25%), Space: 15.5 MB (78.38%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -9,15 +9,11 @@ public:
     int stoneGameV(vector<int>& a) {
         int n = a.size();
         memset(pre,0,sizeof(pre));
-        pre[0] = a[0];
-        for (int i=1; i<n; i++) pre[i] = pre[i-1] + a[i];
-        auto g = [&] (int l, int r) {
-            int ret = pre[r];
-            if (l > 0) ret -= pre[l-1];
-            return ret;
-        };
         memset(dp,0,sizeof(dp));
         memset(vis,0,sizeof(vis));
+        for (int i=1; i<=n; i++) pre[i] = pre[i-1] + a[i-1];
+        auto g = [&] (int l, int r) { return pre[r+1] - pre[l]; };
+
         function<int(int,int)> f = [&] (int l, int r) {
             int ret = 0;
             if (vis[l][r]) return dp[l][r];
@@ -26,15 +22,9 @@ public:
             for (int i=l; i<r; i++) {
                 auto v1 = g(l,i), v2 = g(i+1,r);
                 int t = 0;
-                if (v1 < v2) {
-                    t = v1 + f(l,i);
-                }
-                else if (v1 > v2) {
-                    t = v2 + f(i+1,r);
-                }
-                else {
-                    t = max(f(l,i), f(i+1,r)) + v1;
-                }
+                if (v1 < v2) t = v1 + f(l,i);
+                else if (v1 > v2) t = v2 + f(i+1,r);
+                else t = max(f(l,i), f(i+1,r)) + v1;
                 chmax(ret, t);
             }
             return dp[l][r] = ret;
