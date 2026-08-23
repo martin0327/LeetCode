65a37abfd496e5961ec6ce51c64e782abf561346
Martin Yang <martin0327@gmail.com>
2026-08-24 08:49:44 +0900

    Time: 0 ms (100%), Space: 13.9 MB (67.7%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool sumGame(string s) {
+        int n=s.size(), m = n/2;
+        int x=0, y=0;
+        int c1=0, c2=0;
+        for (int i=0; i<n; i++) {
+            if (s[i] == '?') {
+                if (i<m) c1++;
+                else c2++;
+            }
+            else {
+                if (i<m) x += s[i] - '0';
+                else y += s[i] - '0';
+            }
+        }
+        if ((c1+c2)%2) return true;
+        auto f = [&] (int x, int y, int d) {
+            if (x+9*d > y) return true;
+            if (x+9*d < y) return true;
+            return false;
+        };
+        if (c1 < c2) {
+            swap(c1,c2);
+            swap(x,y);
+        }
+        int d = c1-c2;
+        if (d == 0) return x != y;
+        d /= 2;
+        if (f(x,y,d)) return true;
+        return false;
+    }
+};
