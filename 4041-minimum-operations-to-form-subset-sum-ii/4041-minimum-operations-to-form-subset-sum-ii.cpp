1a6481f979011b2af598e21dfa43095a07a6aaca
Martin Yang <martin0327@gmail.com>
2026-08-30 18:50:08 +0900

    Time: 431 ms (96.74%), Space: 41.4 MB (90.59%) - LeetHub

--- 4041-minimum-operations-to-form-subset-sum-ii/4041-minimum-operations-to-form-subset-sum-ii.cpp
+++ 4041-minimum-operations-to-form-subset-sum-ii/4041-minimum-operations-to-form-subset-sum-ii.cpp
@@ -1,19 +1,11 @@
 using pii = pair<int,int>;
 using vp = vector<pii>;
-using vi = vector<int>;
-using vvi = vector<vi>;
-const int sz = 501;
-const int m = 5001;
+const int sz = 501, m = 5001;
 const int inf = 2e9;
 int weight[sz][m];
+int dp[m];
 vp tr[sz];
 void chmin(int &x, int y) { x = min(x,y); }
-
-void debug(vp a) {
-    for (auto [x,y] : a) {
-        cout << x << " " << y << endl;
-    }
-}
 
 class Solution {
 public:
@@ -39,8 +31,7 @@ public:
             }
         }
         int n = a.size();
-        // vvi dp(n+1, vi(k+1, inf));
-        vi dp(k+1, inf);
+        for (int j=1; j<=k; j++) dp[j] = inf;
         dp[0] = 0;
         for (int i=0; i<n; i++) {
             for (int j=k; j>=0; j--) {
