1ecc4c2123efbd4a7d9c4e1f977dfd1aed4b11e7
Martin Yang <martin0327@gmail.com>
2026-09-05 14:50:06 +0900

    Time: 0 ms (100%), Space: 16.3 MB (21.41%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.cpp
@@ -0,0 +1,64 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int,int>;
+using vp = vector<pii>;
+vi dr,dc;
+class Solution {
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& a, vector<int>& src) {
+        int n = a.size(), m = a[0].size();
+        vvi vis(n, vi(m));
+        if (dr.empty()) {
+            for (int r=-1; r<=1; r++) {
+                for (int c=-1; c<=1; c++) {
+                    if (r==0 && c==0) continue;
+                    dr.push_back(r);
+                    dc.push_back(c);
+                }
+            }
+        }
+        auto sr = src[0], sc = src[1];
+        if (a[sr][sc] == 'M') {
+            a[sr][sc] = 'X';
+            return a;
+        }
+
+        auto oob = [&] (int r, int c) {
+            if (r < 0 || r >= n) return true;
+            if (c < 0 || c >= m) return true;
+            return false;
+        };
+
+        queue<pii> q;
+        q.push({sr,sc});
+        vis[sr][sc] = 1;
+        while (q.size()) {
+            auto [r,c] = q.front();
+            q.pop();
+            auto cnt = 0;
+            vp nb;
+            for (int i=0; i<8; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (oob(nr,nc)) continue; 
+                if (a[nr][nc] == 'M') cnt++;
+                else if (a[nr][nc] == 'E') {
+                    if (!vis[nr][nc]) {
+                        nb.push_back({nr,nc});
+                    }
+                }
+            }
+            if (cnt == 0) {
+                a[r][c] = 'B';
+                for (auto [nr,nc] : nb) {
+                    vis[nr][nc] = 1;
+                    q.push({nr,nc});
+                }
+            }
+            else {
+                a[r][c] = '0' + cnt;
+            }
+        }
+        return a;
+    }
+};
