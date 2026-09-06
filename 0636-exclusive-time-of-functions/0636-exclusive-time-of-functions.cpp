cb239ff780887ae03442cc80ba42fd201e18cca6
Martin Yang <martin0327@gmail.com>
2026-09-06 23:59:42 +0900

    Time: 26 ms (9.93%), Space: 28.3 MB (5.23%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -16,35 +16,33 @@ public:
         vector<ti3> b;
         for (auto s : a) {
             auto t = split_str(s,':');
-            auto key = stoi(t[0]);
+            int key = stoi(t[0]);
             int tp = (t[1] == "start");
-            auto pos = stoi(t[2]);
+            int pos = stoi(t[2]);
             if (tp == 0) pos++;
             b.push_back({pos,tp,key});
         }
         sort(b.begin(), b.end());
-        vi ans(n);
-        vector<ti3> lst;
-        vi st;
-        for (auto [pos,tp,key] : b) {
-            if (lst.size()) {
-                auto [lpos,ltp,lkey] = lst.back();
+        vi ans(n), st;
+        for (int i=0; i<int(b.size()); i++) {
+            auto [pos,tp,key] = b[i];
+            if (i > 0) {
+                auto [ppos,ptp,pkey] = b[i-1];
                 if (tp) {
-                    if (ltp) ans[lkey] += pos - lpos;
+                    if (ptp) ans[pkey] += pos - ppos;
                     else {
                         if (st.size()) {
-                            ans[st.back()] += pos - lpos;
+                            ans[st.back()] += pos - ppos;
                         }
                     }
                 }
                 else {
-                    ans[key] += pos - lpos;
+                    ans[key] += pos - ppos;
                     assert(key == st.back());
                     st.pop_back();
                 }
             }
             if (tp) st.push_back(key);
-            lst.push_back({pos,tp,key});
         }
         return ans;
     }
