ee4c8beaa67f3627003558e942453c7af0b5a080
Martin Yang <martin0327@gmail.com>
2026-09-06 16:24:30 +0900

    Time: 1480 ms (7.73%), Space: 68.9 MB (79.06%) - LeetHub

--- 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
+++ 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
@@ -13,27 +13,28 @@ using ti5 = tuple<ll,ll,ll,ll,ll>;
 
 vi dr = {0,1,0,-1};
 vi dc = {1,0,-1,0};
-const ll inf = 0x3f3f3f3f3f3f3f3f;
+const ll inf = 0x3f3f3f3f3f3f3f3f; 
 const int sz = 80;
-ll dist[sz][sz][5][sz];
+ll dist[sz][sz][sz][5];
+
 class Solution {
 public:
     int minCost(vector<vector<int>>& a, int k) {
         int n = a.size(), m = a[0].size();
         memset(dist, 0x3f, sizeof(dist));
-        dist[0][0][0][0] = dist[0][0][1][0] = a[0][0];
+        dist[0][0][0][0] = dist[0][0][0][1] = a[0][0];
         min_pq<ti5> pq;
         pq.push({a[0][0],0,0,0,0});
-        pq.push({a[0][0],0,0,1,0});
+        pq.push({a[0][0],0,0,0,1});
         auto oob = [&] (int r, int c) {
             if (r < 0 || r >= n) return true;
             if (c < 0 || c >= m) return true;
             return false;
         };
         while (pq.size()) {
-            auto [d,r,c,dir,t] = pq.top();
+            auto [d,r,c,t,dir] = pq.top();
             pq.pop();
-            if (d > dist[r][c][dir][t]) continue;
+            if (d > dist[r][c][t][dir]) continue;
             for (int i=0; i<4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
@@ -42,15 +43,15 @@ public:
                 int ndir = i;
                 int nt = t + (ndir != dir);
                 if (nt > k) continue;
-                if (nd < dist[nr][nc][ndir][nt]) {
-                    dist[nr][nc][ndir][nt] = nd;
-                    pq.push({nd,nr,nc,ndir,nt});
+                if (nd < dist[nr][nc][nt][ndir]) {
+                    dist[nr][nc][nt][ndir] = nd;
+                    pq.push({nd,nr,nc,nt,ndir});
                 }
             }
         }
         auto ans = inf;
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<=k; j++) {
+        for (int i=0; i<=k; i++) {
+            for (int j=0; j<4; j++) {
                 chmin(ans, dist[n-1][m-1][i][j]);
             }
         }
