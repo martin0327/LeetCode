13f04a9bd27d0809286d61f59edf673a4c6a4b0d
Martin Yang <martin0327@gmail.com>
2026-09-06 23:53:34 +0900

    Time: 13 ms (21.3%), Space: 29.1 MB (5.23%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -0,0 +1,57 @@
+vector<string> split_str(string s, const char delim = ' ') {
+    vector<string> ret;
+    stringstream ss(s);
+    string t;
+    while (getline(ss, t, delim)) {
+        ret.push_back(t);
+    }
+    return ret;
+}
+using vi = vector<int>;
+using vvi = vector<vi>;
+using ti3 = tuple<int,int,int>;
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& a) {
+        vector<ti3> A;
+        for (auto s : a) {
+            auto b = split_str(s,':');
+            auto key = stoi(b[0]);
+            int tp = (b[1] == "start");
+            auto pos = stoi(b[2]);
+            if (tp == 0) pos++;
+            A.push_back({pos,tp,key});
+        }
+        sort(A.begin(), A.end());
+        // debug(A);
+        vi ans(n);
+        vector<ti3> lst;
+        vi st;
+        for (auto [pos,tp,key] : A) {
+            if (lst.size()) {
+                auto [lpos,ltp,lkey] = lst.back();
+                if (tp) {
+                    if (ltp) ans[lkey] += pos - lpos;
+                    else {
+                        if (st.size()) {
+                            ans[st.back()] += pos - lpos;
+                        }
+                    }
+                }
+                else {
+                    ans[key] += pos - lpos;
+                    // debug(key,st);
+                    assert(key == st.back());
+                    st.pop_back();
+                }
+            }
+            if (tp) st.push_back(key);
+            lst.push_back({pos,tp,key});
+            // debug(pos,tp,key);
+            // debug(lst);
+            // debug(st);
+            // debug(ans);
+        }
+        return ans;
+    }
+};
