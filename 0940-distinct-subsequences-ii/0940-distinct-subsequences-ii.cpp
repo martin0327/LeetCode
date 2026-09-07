acd1f931379868767651d4656fc7674732aed65d
Martin Yang <martin0327@gmail.com>
2026-09-08 00:19:34 +0900

    Time: 3 ms (37.51%), Space: 8.8 MB (78.81%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,14 +1,12 @@
-using ll = long long;
-using vi = vector<ll>;
-const ll mod = 1e9+7, sz = 26;
-ll dp[sz];
+const int mod = 1e9+7, sz = 26;
+int dp[sz];
 class Solution {
 public:
     int distinctSubseqII(string s) {
         fill(dp,dp+sz,1);
-        ll ans = 0;
+        int ans = 0;
         for (auto &ch : s) {
-            ll x = ch - 'a';
+            int x = ch - 'a';
             ans += dp[x];
             ans %= mod;
             for (int y=0; y<sz; y++) {
