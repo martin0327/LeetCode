3d6d45a7cd3cbc3cab37b176a9d8feb27f30a106
Martin Yang <martin0327@gmail.com>
2026-09-09 13:01:38 +0900

    Time: 0 ms (100%), Space: 8 MB (24.04%) - LeetHub

--- 0672-bulb-switcher-ii/0672-bulb-switcher-ii.cpp
+++ 0672-bulb-switcher-ii/0672-bulb-switcher-ii.cpp
@@ -1,46 +1,40 @@
 using vi = vector<int>;
 using vvi = vector<vi>;
+const int mxk = 1000;
+bool dp[5][mxk+1][17];
+bool done = false;
 class Solution {
 public:
     int flipLights(int n, int k) {
-        auto f = [&] (int sz, vi tr) {
-            vi vis(sz,sz);
-            vvi dp(k+1, vi(sz));
-            dp[0][0] = 1;
-            for (int i=0; i<k; i++) {
-                for (int j=0; j<sz; j++) {
-                    if (!dp[i][j]) continue;
-                    for (auto t : tr) {
-                        int nj = j ^ t;
-                        dp[i+1][nj] = 1;
+        if (!done) {
+            done = true;
+            for (int l=1; l<=4; l++) {
+                int sz = 1<<l;
+                vi tr;
+                if (l == 1) tr = {0,1};
+                else if (l == 2) tr = {1,2,3};
+                else if (l == 3) tr = {1,2,5,7};
+                else tr = {3,6,9,15};
+                dp[l][0][0] = 1;
+                for (int i=0; i<mxk; i++) {
+                    for (int j=0; j<sz; j++) {
+                        if (!dp[l][i][j]) continue;
+                        for (auto t : tr) {
+                            int nj = j ^ t;
+                            dp[l][i+1][nj] = 1;
+                        }
                     }
                 }
             }
-            int ans = 0;
+        }
+        auto f = [&] (int n) {
+            n = min(n, 4);
+            int ans = 0, sz = 1<<n;
             for (int j=0; j<sz; j++) {
-                ans += dp[k][j];
+                ans += dp[n][k][j];
             }
             return ans;
         };
-        if (n == 1) {
-            vi tr = {0,1};
-            auto ans = f(2,tr);
-            return ans;
-        }
-        else if (n == 2) {
-            vi tr = {1,2,3};
-            auto ans = f(4,tr);
-            return ans;
-        }
-        else if (n == 3) {
-            vi tr = {1,2,5,7};
-            auto ans = f(8,tr);
-            return ans;
-        }
-        else {
-            vi tr = {3,6,9,15};
-            auto ans = f(16,tr);
-            return ans;
-        }
+        return f(n);
     }
 };
