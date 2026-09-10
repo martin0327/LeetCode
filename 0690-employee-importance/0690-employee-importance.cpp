cd4910dd80411be3d1ade502b03dca3622118d67
Martin Yang <martin0327@gmail.com>
2026-09-10 15:08:29 +0900

    Time: 28 ms (37.46%), Space: 23.2 MB (15.07%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+using vi = vector<int>;
+class Solution {
+public:
+    int getImportance(vector<Employee*> emp, int id) {
+        int sz = 2001;
+        vector<Employee*> a(sz);
+        vi vis(sz);
+        queue<int> q;
+        for (auto e : emp) {
+            int i = e->id;
+            a[i] = e;
+            if (id == i) {
+                q.push(i);
+                vis[i] = 1;
+            }
+        }
+        while (q.size()) {
+            auto u = q.front();
+            q.pop();
+            for (auto nb : a[u]->subordinates) {
+                int v = a[nb]->id;
+                if (vis[v]) continue;
+                vis[v] = 1;
+                q.push(v);
+            }
+        }
+        int ans = 0;
+        for (int i=0; i<sz; i++) {
+            if (vis[i]) {
+                ans += a[i]->importance;
+            }
+        }
+        return ans;
+    }
+};
