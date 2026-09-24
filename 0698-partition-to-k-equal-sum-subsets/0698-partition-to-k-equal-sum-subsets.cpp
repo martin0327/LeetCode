f7908405b2da4d87bf8ccad2e1c336e92b3a2e94
Martin Yang <martin0327@gmail.com>
2026-09-24 15:31:27 +0900

    Time: 284 ms (32.3%), Space: 12.5 MB (39.72%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,4 +1,4 @@
-bool dp[17][1<<17];
+bool dp[1<<17];
 int s[1<<17];
 class Solution {
 public:
@@ -17,20 +17,18 @@ public:
         tot /= k;
         for (int i=0; i<n; i++) {
             if (a[i] > tot) return false;
-            dp[i][1<<i] = 1;
+            dp[1<<i] = 1;
         }
         for (int mask=0; mask<sz; mask++) {
-            for (int i=0; i<n; i++) {
-                if (!dp[i][mask]) continue;
-                if (mask == sz-1) return true;
-                for (int j=0; j<n; j++) {
-                    if (mask>>j&1) continue;
-                    int nmask = mask | (1<<j);
-                    int q1 = s[mask] / tot;
-                    int q2 = s[nmask] / tot;
-                    if (q1 == q2 || (s[mask] % tot == 0) || (s[nmask] % tot == 0)) {
-                        dp[j][nmask] = 1;
-                    }
+            if (!dp[mask]) continue;
+            if (mask == sz-1) return true;
+            for (int j=0; j<n; j++) {
+                if (mask>>j&1) continue;
+                int nmask = mask | (1<<j);
+                int q1 = s[mask] / tot;
+                int q2 = s[nmask] / tot;
+                if (q1 == q2 || (s[mask] % tot == 0) || (s[nmask] % tot == 0)) {
+                    dp[nmask] = 1;
                 }
             }
         }
