ae11c6ecfe4d937c6cba139d1421656cdec63888
Martin Yang <martin0327@gmail.com>
2026-09-24 15:08:46 +0900

    Time: 627 ms (24.43%), Space: 213.1 MB (5.28%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,51 @@
+bool dp[17][1<<17];
+int s[1<<17];
+using vi = vector<int>;
+using vvi = vector<vi>;
+void debug(vi a) {
+    for (auto x : a) { 
+        cout << x << " ";
+    }   cout << endl;
+}
+void debug(vvi a) {
+    for (auto &v : a) {
+        debug(v);
+    }
+}
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& a, int k) {
+        int n = a.size(), sz = 1<<n;
+        memset(s,0,sizeof(s));
+        for (int mask=0; mask<sz; mask++) {
+            for (int i=0; i<n; i++) {
+                if (mask>>i&1) s[mask] += a[i];
+            }
+        }
+        auto tot = s[sz-1];
+        if (tot % k != 0) return false;
+
+        tot /= k;
+        // memset(dp,0,sizeof(dp));
+        vvi dp(n, vi(1<<n));
+        for (int i=0; i<n; i++) {
+            if (a[i] > tot) return false;
+            dp[i][1<<i] = 1;
+        }
+        for (int mask=0; mask<sz; mask++) {
+            for (int i=0; i<n; i++) {
+                if (!dp[i][mask]) continue;
+                if (mask == sz-1) return true;
+                for (int j=0; j<n; j++) {
+                    if (mask>>j&1) continue;
+                    int nmask = mask | (1<<j);
+                    int q1 = s[mask] / tot;
+                    int q2 = s[nmask] / tot;
+                    if (q1 == q2 || (s[mask] % tot == 0) || (s[nmask] % tot == 0)) dp[j][nmask] = 1;
+                }
+            }
+        }
+        // debug(dp);
+        return false;
+    }
+};
